18e439ac85f4e1158082a42a5fc973d2dd14327f
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-01-11 15:25:56 -0500

    Create main.swift

---
+++ Sichengthebest/003 猜数字/main.swift
@@ -0,0 +1,16 @@
+let number = Int.random(in: 0...999)
+var numberOfTries = 0
+var answer = 0
+
+while answer != number {
+    numberOfTries += 1
+    let answerAsk = askForNumber()
+    answer = answerAsk
+    if answer < number {
+        show("The number you are trying to guess is superior to your guess. Please try again. This is your \(numberOfTries)th try.")
+    }
+    if answer > number {
+        show("The number you are trying to guess is inferior to your guess. Please try again. This is your \(numberOfTries)th try.")
+    }
+}
+show("Good job! You have found out the number in \(numberOfTries) tries!")
