6cf3146363f97dd846a4e4097f583de2b9edddfa
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-01-11 17:01:13 -0500

    Create main.swift

---
+++ Sichengthebest/011 有一天有一天有一条鱼，游啊游啊游啊游在水里游啊！/main.swift
@@ -0,0 +1,14 @@
+let death = """
+更上一层楼
+欲穷千里目
+黄河入海流
+白日依山尽
+"""
+var final = ""
+var value = 0
+for i in 0...3 {
+    final += Array(death)[22-(value+4)...22-(value)]；
+    final += "\n"
+    value += 6
+}
+show(final)
