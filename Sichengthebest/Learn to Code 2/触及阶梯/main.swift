924742f47155de7cd17ea1f0bea71a9b2cdfce13
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-01-11 22:19:53 -0500

    Create main.swift

---
+++ Sichengthebest/Learn to Code 2/触及阶梯/main.swift
@@ -0,0 +1,21 @@
+let cyprien91 = Character()
+world.place(cyprien91, facing: north, atColumn: 0, row: 4)
+for i in 1 ... 2 {
+    cyprien91.toggleSwitch()
+    cyprien91.moveForward()
+}
+cyprien91.toggleSwitch()
+let koloWhatever86 = Character()
+world.place(koloWhatever86, facing: north, atColumn: 6, row: 4)
+for i in 1 ... 2 {
+    koloWhatever86.toggleSwitch()
+    koloWhatever86.moveForward()
+}
+koloWhatever86.toggleSwitch()
+let hwangInBeom85 = Character()
+world.place(hwangInBeom85, facing: west, atColumn: 4, row: 8)
+for i in 1 ... 2 {
+    hwangInBeom85.toggleSwitch()
+    hwangInBeom85.moveForward()
+}
+hwangInBeom85.toggleSwitch()
