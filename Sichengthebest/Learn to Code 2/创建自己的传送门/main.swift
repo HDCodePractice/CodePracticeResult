c7241465ca1278c11dcf8eee4e08c3c5ef58ae40
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-01-11 21:04:59 -0500

    Update main.swift

--- Sichengthebest/Learn to Code 2/创建自己的传送门/main.swift
+++ Sichengthebest/Learn to Code 2/创建自己的传送门/main.swift
@@ -1,5 +1,9 @@
 let q = Portal(color: #colorLiteral(red: 0.9764705882352941, green: 0.8509803921568627, blue: 0.5490196078431373, alpha: 1.0))
 world.place(q, atStartColumn: 0, startRow: 5, atEndColumn: 6, endRow: 1)
+func turnAround() {
+    turnLeft()
+    turnLeft()
+}
 func collectGems() {
     q.isActive = false
     collectGem()
@@ -7,19 +11,16 @@ func collectGems() {
     turnLeft()
     moveForward()
     collectGem()
-    turnLeft()
-    turnLeft()
+    turnAround()
     move(distance: 2)
     collectGem()
-    turnLeft()
-    turnLeft()
+    turnAround()
     moveForward()
     turnRight()
     moveForward()
     collectGem()
     q.isActive = true
-    turnLeft()
-    turnLeft()
+    turnAround()
     move(distance: 2)
 }
 
