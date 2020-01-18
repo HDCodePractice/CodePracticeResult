5a29d1157edfabdea56bb71614789631ba64a3d0
老黄 <huangdong@gmail.com>
2020-01-18 11:16:40 -0500

    Update main.swift

--- hd/003猜数字/main.swift
+++ hd/003猜数字/main.swift
@@ -6,7 +6,7 @@ while inputNumber != randomNumber {
     if inputNumber > randomNumber {
         show("太大大大了，你已经用了\(inputCount)次了～再来个小点儿的试试？")
     }else{
-        show("太小小小了，你已经用了\(inputCount)次了～再来个小点儿的试试？")
+        show("太小小小了，你已经用了\(inputCount)次了～再来个在点儿的试试？")
     }
     inputNumber = askForNumber()
     inputCount += 1
