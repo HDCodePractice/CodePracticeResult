33160056b3225aaac12de71ba1e0e5cc435f8baf
mengmengisthebest <59508960+mengmengisthebest@users.noreply.github.com>
2020-01-25 11:11:11 -0500

    Create Using the NOT Operator.swift

---
+++ mengmeng/LearnCode1/Using the NOT Operator.swift
@@ -0,0 +1,15 @@
+
+for i in 1 ... 4 {
+    moveForward()
+    if isOnGem {
+        collectGem()        
+    }   
+}
+turnRight()
+turnRight()
+moveForward()
+moveForward()
+turnRight()
+moveForward()
+moveForward()
+collectGem()
