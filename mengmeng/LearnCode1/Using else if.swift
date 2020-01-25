0a37d663cc23e159201329350028b254ec8de966
mengmengisthebest <59508960+mengmengisthebest@users.noreply.github.com>
2020-01-25 10:22:52 -0500

    Create Using else if.swift

---
+++ mengmeng/LearnCode1/Using else if.swift
@@ -0,0 +1,10 @@
+moveForward()
+collectGem()
+moveForward()
+if isOnClosedSwitch {
+    toggleSwitch()    
+} else if isOnOpenSwitch {
+    moveForward()    
+} else if isOnGem {
+    collectGem()    
+}
