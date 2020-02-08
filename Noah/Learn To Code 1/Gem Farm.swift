88381c8375a5932f667ff8389288f02992d65178
TypicalNoahVGSuperSniproXDBestPro <59535872+TypicalNoahVGSuperSniproXDBestPro@users.noreply.github.com>
2020-02-08 11:13:12 -0500

    Create Gem Farm.swift

---
+++ Noah/Learn To Code 1/Gem Farm.swift
@@ -0,0 +1,22 @@
+for i in 1 ... 3 {
+    turnRight()
+    moveForward()
+    collectGem()
+    moveForward()
+    collectGem()
+    turnRight()
+    turnRight()
+    moveForward()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    
+}
