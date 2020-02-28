8c284bb84718dc64ea29b8b69af8338df8b0c6da
TypicalNoahVGSuperSniproXDBestPro <59535872+TypicalNoahVGSuperSniproXDBestPro@users.noreply.github.com>
2020-02-28 16:02:09 -0500

    Create fix.Roll Right, Roll Left.swift

---
+++ Noah/Learn To Code 1/fix.Roll Right, Roll Left.swift
@@ -0,0 +1,35 @@
+func rollRight() {
+    turnRight()
+    while !isOnClosedSwitch && !isOnOpenSwitch {
+        if isOnGem {
+            collectGem()
+        }
+        if isBlocked {
+            turnLeft()
+        }
+        moveForward()
+    }
+}
+
+func rollLeft() {
+    turnLeft()
+    while !isOnGem {
+        if isBlocked {
+            turnRight()
+        }
+        if isOnClosedSwitch {
+            toggleSwitch()
+        }
+        moveForward()
+    }
+}
+
+moveForward()
+for i in 1 ... 3 {
+    rollRight()
+    rollLeft()
+}
+rollRight()
+
+
+
