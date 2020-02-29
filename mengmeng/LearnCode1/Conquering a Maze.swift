e712584a7b8eec2da0515add1fb0c2018968837d
mengmengisthebest <59508960+mengmengisthebest@users.noreply.github.com>
2020-02-29 10:39:58 -0500

    Create Conquering a Maze.swift

---
+++ mengmeng/LearnCode1/Conquering a Maze.swift
@@ -0,0 +1,12 @@
+while !isOnGem {
+    moveForward()
+    if isOnClosedSwitch {
+        toggleSwitch()
+        if isBlocked {
+            turnLeft()
+        } else {
+            turnRight()
+        }
+    }   
+}
+collectGem()
