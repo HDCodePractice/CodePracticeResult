4399220350bcce0bf7f7f731bafc0147bfb7ee4a
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-03-04 10:07:53 -0500

    Update main.swift

--- Sichengthebest/Learn to Code 2/寻找七颗宝石/main.swift
+++ Sichengthebest/Learn to Code 2/寻找七颗宝石/main.swift
@@ -1,13 +1,12 @@
 var gems = 0
-
 while gems < 7 {
-    while !isBlocked {
-        moveForward()
-        if isOnGem {
-            collectGem()
-            gems += 1
-        }
+    moveForward()
+    if isOnGem {
+        collectGem()
+        gems += 1
     }
-    turnLeft()
-    turnLeft()
+    if isBlocked {
+        turnLeft()
+        turnLeft()
+    }
 }
