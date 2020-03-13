231879f05b62c57cc1bef9279202afa97734cdfa
mengmengisthebest <59508960+mengmengisthebest@users.noreply.github.com>
2020-03-13 16:45:45 -0400

    Create Deactivating a Portal.swift

---
+++ mengmeng/LearnCode2/Deactivating a Portal.swift
@@ -0,0 +1,16 @@
+greenPortal.isActive = false
+func moveThree() {
+    moveForward()
+    moveForward()
+    moveForward()
+}
+
+for i in 1 ... 3 {
+    moveThree()
+    turnRight()
+    moveThree()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+}
+
