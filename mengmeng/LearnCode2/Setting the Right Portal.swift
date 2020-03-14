2159ce663ff65bba44103cec66a7483674bd48b0
mengmengisthebest <59508960+mengmengisthebest@users.noreply.github.com>
2020-03-14 09:58:07 -0400

    Create Setting the Right Portal.swift

---
+++ mengmeng/LearnCode2/Setting the Right Portal.swift
@@ -0,0 +1,27 @@
+func turnAround() {
+    turnRight()
+    turnRight()
+}
+bluePortal.isActive = false
+pinkPortal.isActive = false
+moveForward()
+moveForward()
+moveForward()
+collectGem()
+turnAround()
+pinkPortal.isActive = true
+moveForward()
+turnAround()
+moveForward()
+collectGem()
+turnAround()
+moveForward()
+bluePortal.isActive = true
+moveForward()
+moveForward()
+collectGem()
+turnAround()
+bluePortal.isActive = false
+moveForward()
+moveForward()
+collectGem()
