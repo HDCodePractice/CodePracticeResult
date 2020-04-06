46ba25edf91f5eb64121d2ba439d62233f6bda22
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-05 20:11:53 -0400

    “尽收囊中”完成
    若符号不同请谅解

---
+++ Qiqi/学习编程1/尽收囊中/main.swift
@@ -0,0 +1,30 @@
+func /*#-editable-code funcName*/ getG1 /*#-end-editable-code*/() {
+    //#-editable-code
+    
+    collectGem()
+    //#-end-editable-code
+}
+//#-editable-code
+getG1()
+moveForward()
+getG1()
+moveForward()
+getG1()
+turnRight()
+moveForward()
+getG1()
+moveForward()
+getG1()
+turnRight()
+moveForward()
+getG1()
+moveForward()
+getG1()
+turnRight()
+moveForward()
+getG1()
+turnRight()
+moveForward()
+getG1()
+
+//#-end-editable-code
