ccce4182e427544b0cdcc21c3fb5dba5fdf385d6
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-05 20:16:19 -0400

    “寻宝”完成

---
+++ Qiqi/学习编程1/寻宝/main.swift
@@ -0,0 +1,28 @@
+func moveThenToggle() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnAround()
+    moveForward()
+    moveForward()
+    
+}
+func turnAround() {
+    turnLeft()
+    turnLeft()
+    
+}
+moveThenToggle()
+moveThenToggle()
+turnLeft()
+moveThenToggle()
+moveThenToggle()
+moveForward()
+moveForward()
+moveThenToggle()
+moveForward()
+moveForward()
+moveForward()
+moveForward()
+moveForward()
+moveThenToggle()
