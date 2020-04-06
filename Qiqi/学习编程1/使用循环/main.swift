7bc59f49ae360096050b7f473b5be149602bcf82
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-05 20:17:31 -0400

    “使用循环”完成
    若符号不同请谅解

---
+++ Qiqi/学习编程1/使用循环/main.swift
@@ -0,0 +1,9 @@
+for i in 1 ... /*#-editable-code*/5/*#-end-editable-code*/ {
+    //#-editable-code
+    moveForward()
+    moveForward()
+    collectGem()
+    moveForward()
+    
+    //#-end-editable-code
+}
