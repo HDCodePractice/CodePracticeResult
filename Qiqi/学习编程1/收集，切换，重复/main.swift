60cd2ea14c73044bc5f83669a0025cb6f6d26fcc
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-05 20:05:04 -0400

    “收集，切换，重复”完成
    若符号不同请谅解

---
+++ Qiqi/学习编程1/收集，切换，重复/main.swift
@@ -0,0 +1,29 @@
+func /*#-editable-code yourFuncName*/getGS/*#-end-editable-code*/() {
+    //#-editable-code Add commands to your function
+    collectGem()
+    moveForward()
+    toggleSwitch()
+    
+    //#-end-editable-code   
+}
+//#-editable-code
+moveForward()
+getGS()
+moveForward()
+turnLeft()
+moveForward()
+getGS()
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+getGS()
+moveForward()
+turnLeft()
+moveForward()
+getGS()
+//#-end-editable-code
+//#-hidden-code
+playgroundEpilogue()
+//#-end-hidden-code
+
