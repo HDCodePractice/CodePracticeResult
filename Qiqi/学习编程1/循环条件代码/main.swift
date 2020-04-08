b29c10b10162ec8870e578a4db2ea079153c3ad2
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-07 20:40:40 -0400

    “循环条件代码”完成
    若符号不同请谅解

---
+++ Qiqi/学习编程1/循环条件代码/main.swift
@@ -0,0 +1,13 @@
+for i in 1 ... /*#-editable-code*/12/*#-end-editable-code*/ {
+    //#-editable-code
+    moveForward()
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }else if isOnGem{
+        collectGem()
+        
+    }
+    
+    //#-end-editable-code
+}
