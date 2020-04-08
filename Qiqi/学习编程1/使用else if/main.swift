3f4bbf94c0f567603ce53da09d3baaf2ec652db1
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-07 20:32:17 -0400

    “使用else if”完成

---
+++ Qiqi/学习编程1/使用else if/main.swift
@@ -0,0 +1,16 @@
+moveForward()
+if isOnClosedSwitch {
+    toggleSwitch()
+    
+}else if isOnGem{
+    collectGem()
+    
+}
+moveForward()
+if isOnClosedSwitch {
+    toggleSwitch()
+    
+}else if isOnGem{
+    collectGem()
+    
+}
