60d97e2a45393317fc9e2b6b775097cc71109187
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-07 20:30:37 -0400

    “检查开关”完成

---
+++ Qiqi/学习编程1/检查开关/main.swift
@@ -0,0 +1,18 @@
+moveForward()
+moveForward()
+if isOnClosedSwitch {
+    toggleSwitch()
+    
+}
+moveForward()
+if isOnClosedSwitch {
+    toggleSwitch()
+    
+}
+moveForward()
+if isOnClosedSwitch {
+    toggleSwitch()
+    
+}
+
+
