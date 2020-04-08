2f12657670f1cfa5993caab5db9cfd1a75d605d5
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-07 20:43:54 -0400

    “定义更巧妙的函数”完成
    若符号不同请谅解

---
+++ Qiqi/学习编程1/定义更巧妙的函数/main.swift
@@ -0,0 +1,40 @@
+func collectOrToggle() {
+    //#-editable-code
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }else if isOnGem{
+        collectGem()
+        
+    }
+    
+    //#-end-editable-code
+}
+//#-editable-code
+moveForward()
+moveForward()
+collectOrToggle()
+moveForward()
+moveForward()
+collectOrToggle()
+turnLeft()
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+collectOrToggle()
+moveForward()
+moveForward()
+collectOrToggle()
+turnRight()
+moveForward()
+turnRight()
+moveForward()
+moveForward()
+collectOrToggle()
+moveForward()
+moveForward()
+collectOrToggle()
+
+//#-end-editable-code
