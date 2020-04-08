3bc22320395d9e4d3910e7bd90377b3c4d90535a
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-07 20:26:24 -0400

    “拓展技能”完成

---
+++ Qiqi/学习编程1/拓展技能/main.swift
@@ -0,0 +1,28 @@
+func turnAround() {
+    turnLeft()
+    turnLeft()
+}
+for i in 1 ... 3 {
+    moveForward()
+    moveForward()
+    turnRight()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnAround()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnRight()
+    
+}
