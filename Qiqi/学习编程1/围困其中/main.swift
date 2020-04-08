5bc35bcd443d4f09dda4cd28bde9566e88fa6ca0
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-04-07 20:45:26 -0400

    “围困其中”完成

---
+++ Qiqi/学习编程1/围困其中/main.swift
@@ -0,0 +1,30 @@
+func checkTitle() {
+    if isOnClosedSwitch {
+        toggleSwitch()
+    }else if isOnGem{
+        collectGem()
+    }
+}
+for i in 1 ... 1 {
+    moveForward()
+    checkTitle()
+    turnLeft()
+    moveForward()
+    checkTitle()
+    turnLeft()
+    moveForward()
+    checkTitle()
+    moveForward()
+    checkTitle()
+    turnLeft()
+    moveForward()
+    checkTitle()
+    moveForward()
+    checkTitle()
+    turnLeft()
+    moveForward()
+    checkTitle()
+    moveForward()
+    checkTitle()
+    
+}
