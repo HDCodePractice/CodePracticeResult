df1cc4843b9608c89ae1bc4d844786c39b1c9c10
mengmengisthebest <59508960+mengmengisthebest@users.noreply.github.com>
2020-04-11 10:59:53 -0400

    Update 011Sub Strings.swift

--- mengmeng/LearnCode1-string/011Sub Strings.swift
+++ mengmeng/LearnCode1-string/011Sub Strings.swift
@@ -6,9 +6,11 @@ var s = """
 """
 var final = ""
 var value = 0
-let sa = Array(s)
-for i in 0...3 {
+for i in 0...2 {
     final += Array(s)[22-(value+4)...22-(value)]
     final += "\n"
     value += 6
 }
+final += Array(s)[22-(value+4)...22-(value)]
+
+show(final)
