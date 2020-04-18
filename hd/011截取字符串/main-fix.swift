03cb95d58ff99f73d489dc576bd61ad7d0d82c05
老黄 <huangdong@gmail.com>
2020-04-18 11:21:46 -0400

    prefix&suffix的连续使用版本

--- hd/011截取字符串/main-fix.swift
+++ hd/011截取字符串/main-fix.swift
@@ -1,23 +1,15 @@
-let death = """
+var s = """
 更上一层楼
 欲穷千里目
 黄河入海流
 白日依山尽
 """
 var final = ""
-var value = 0
 for i in 1 ... 4 {
-    var cut = 0
-    if i > 1{
-        cut = (5-i)*6-1
-    }else{
-        cut = (5-i)*6
-    }
-    let cutString = String(death.prefix(cut))
-    final += String( cutString.suffix(5) ) 
+    let a = (5 - i) * 6 - 1 
+    final += String(s.prefix(a).suffix(5))
     if i < 4 {
         final += "\n"
     }
 }
-
-show(final)
+show (final)
