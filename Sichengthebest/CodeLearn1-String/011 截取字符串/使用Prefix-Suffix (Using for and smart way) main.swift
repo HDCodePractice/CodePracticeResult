7fc1bf93880c4fde5e90af0ccb620def222969ec
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-04-18 10:19:09 -0400

    Create 使用Prefix-Suffix (Using for and smart way) main.swift

---
+++ Sichengthebest/CodeLearn1-String/011 截取字符串/使用Prefix-Suffix (Using for and smart way) main.swift
@@ -0,0 +1,18 @@
+var s = """
+
+更上一层楼
+欲穷千里目
+黄河入海流
+白日依山尽
+"""
+var final = ""
+var semi = ""
+let sa = Array(s)
+for i in 1...4 {
+    semi = String(s.suffix(i * 6))
+    if i == 1 {
+        semi = String(s.suffix(i * 5))
+    }
+    final += String(semi.prefix(6))
+}
+show(final)
