e70fe290949dd0a1a2b5c84128d6b4e1bfa2a128
mengmengisthebest <59508960+mengmengisthebest@users.noreply.github.com>
2020-04-18 10:00:43 -0400

    Create 011SubString(prefix and suffix).swift

---
+++ mengmeng/LearnCode1-string/011SubString(prefix and suffix).swift
@@ -0,0 +1,22 @@
+var s = """
+更上一层楼
+欲穷千里目
+黄河入海流
+白日依山尽
+"""
+var final = ""
+var value = 0
+var a = String(s.prefix(11))
+var thirdLine = String(a.suffix(5))
+var lastLine = String(a.prefix(5))
+var b = String(s.suffix(11))
+let secondLine = String(b.prefix(5))
+let firstLine = String(b.suffix(5))
+final.append(firstLine)
+final.append("\n")
+final.append(secondLine)
+final.append("\n")
+final.append(thirdLine)
+final.append("\n")
+final.append(lastLine)
+show(final)
