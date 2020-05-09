4dd97ee6821018bd5a85b6f1e192aeda5456c592
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-05-09 11:47:15 -0400

    Update main-recursive.swift

--- Sichengthebest/000 随机座位/main-recursive.swift
+++ Sichengthebest/000 随机座位/main-recursive.swift
@@ -8,3 +8,12 @@ func getBit(bit: Int) -> Int {
     return a
 }
 show(getBit(bit: 3))
+
+func getCount(a: Int, divisor: Int) -> Int {
+    var b = 0
+    if a > 0 || divisor > 0 {
+        b = a/divisor
+    }
+    return b
+}
+show(getCount(a:-182, divisor:5))
