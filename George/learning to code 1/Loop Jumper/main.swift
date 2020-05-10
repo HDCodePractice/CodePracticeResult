6fb02039e02a196de34da238c65ce531f48d19f9
Prom666 <63210896+Prom666@users.noreply.github.com>
2020-05-10 18:22:35 -0400

    I completed loop Jumper

---
+++ George/learning to code 1/Loop Jumper/main.swift
@@ -0,0 +1,13 @@
+for i in 1 ... 4 {
+    moveForward()
+    turnLeft()
+    moveForward()
+    moveForward()
+    collectGem()
+    turnRight()
+}
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+collectGem()
