df3702d0de519c4b4441644ccfdac57daca96bf9
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-05-21 20:15:49 -0400

    Create main.swift

---
+++ Qiqi/学习编程1/使用“非” 逻辑符/main.swift
@@ -0,0 +1,16 @@
+for i in 1 ... 4 {
+    moveForward()
+    if !isOnGem {
+        turnLeft()
+        moveForward()
+        moveForward()
+        collectGem()
+        turnLeft()
+        turnLeft()
+        moveForward()
+        moveForward()
+        turnLeft()
+    } else {
+        collectGem()
+    }
+}
