3021e75154cd0a8ce99e126db0778ae371421eff
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-05-21 21:20:15 -0400

    Create main.swift

---
+++ Qiqi/学习编程1/转身/main.swift
@@ -0,0 +1,13 @@
+func oneSide() {
+    moveForward()
+    collectGem()
+    turnLeft()
+    moveForward()
+    collectGem()
+    turnLeft()
+    moveForward()
+    turnRight()
+}
+for i in 1 ... 4 {
+    oneSide()
+}
