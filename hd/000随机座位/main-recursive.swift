f2415be1def1a2b36b4255979e1260e6587b4aec
老黄 <huangdong@gmail.com>
2020-05-23 09:56:00 -0400

    加入一个字符串的getNumber方法

--- hd/000随机座位/main-recursive.swift
+++ hd/000随机座位/main-recursive.swift
@@ -27,5 +27,29 @@ func getNumber(n: Int,bit: Int) {
     }
 }
 
+// 获取数字，n为现有的数字，bit为当前的位数
+func getNumber2(n: Int,bit: Int) {
+    for i in numbers{
+        // 把i放到n的前面，如果n为0，哪么说明就只有i
+        var s = ""
+        if n == 0 {
+            s = "\(i)"
+        }else{
+            s = "\(i)\(n)"
+        }
+        
+        if bit < 2{
+            // 判断s里是不是有重复的数字
+            if Array(s).count == Set(Array(s)).count {
+                count += 1
+                show("第\(count)个是:\(s)")
+            }
+        }
+        if bit > 1 {
+            getNumber(n: Int(s) ?? 0 , bit: bit - 1)
+        }
+    }
+}
+
 show("需要\(numbersCount)位数")
-getNumber(n: 0, bit: numbersCount)
+getNumber2(n: 0, bit: numbersCount)
