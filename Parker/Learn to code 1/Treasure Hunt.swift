790743838e7130233d8c1675ae7ec93f6c6673d4
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-05-23 16:07:30 -0400

    Create Treasure Hunt.swift

---
+++ Parker/Learn to code 1/Treasure Hunt.swift
@@ -0,0 +1,23 @@
+func goBack() {
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+func moveAndToggleSwitch() {
+    moveForward()
+    moveForward()
+    toggleSwitch()   
+}
+moveAndToggleSwitch()
+goBack()
+moveAndToggleSwitch()
+goBack()
+turnLeft()
+moveAndToggleSwitch()
+moveAndToggleSwitch()
+goBack()
+moveAndToggleSwitch()
+moveAndToggleSwitch()
+moveAndToggleSwitch()
+
