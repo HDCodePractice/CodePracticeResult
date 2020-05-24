d59a813a4588accefd45122dfcbd74dbe5bda4e8
老黄 <huangdong@gmail.com>
2020-05-24 17:31:24 -0400

    Create main2.swift

---
+++ hd/学习编程1/宝石农场/main2.swift
@@ -0,0 +1,38 @@
+func getSwitch() {
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    toggleSwitch()
+}
+func getGem() {
+    moveForward()
+    collectGem()
+    moveForward()
+    collectGem()
+}
+
+func turnBack() {
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+
+turnLeft()
+for i in 1 ... 2 {
+    getSwitch()
+    turnBack()
+    turnLeft()
+    moveForward()
+    turnLeft()
+}
+getSwitch()
+turnBack()
+for i in 1 ... 2 {
+    getGem()
+    turnBack()
+    turnLeft()
+    moveForward()
+    turnLeft()
+}
+getGem()
