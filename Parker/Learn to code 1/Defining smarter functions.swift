d842af9aede5774b7175f287cabcb53295f404c0
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-05-24 11:39:42 -0400

    Create Defining smarter functions.swift

---
+++ Parker/Learn to code 1/Defining smarter functions.swift
@@ -0,0 +1,22 @@
+func collectOrToggle() {
+    for i in 1 ... 4 {
+        moveForward()
+        if isOnClosedSwitch {
+            toggleSwitch()
+        }
+        else if isOnGem {
+            collectGem()
+        }
+    }
+}
+
+collectOrToggle()
+turnLeft()
+moveForward()
+moveForward()
+turnLeft()
+collectOrToggle()
+turnRight()
+moveForward()
+turnRight()
+collectOrToggle()
