a3ddb7718ca05258637d1a80c9349a4f5b746b48
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-05-25 15:13:49 -0400

    Create Four stash sweep way 2.swift

---
+++ Parker/Learn to code 1/Four stash sweep way 2.swift
@@ -0,0 +1,22 @@
+func move2() {
+    moveForward()
+    collectGem()
+    moveForward()
+}
+
+
+for i in 1 ... 4 {
+    move2()    
+    collectGem()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    turnRight()
+    moveForward()
+    collectGem()
+    turnRight()
+    turnRight()
+    move2()
+    collectGem()
+    moveForward()
+}
