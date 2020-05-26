32aae5ce4288178586500c55bd013f00b47483d6
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-05-25 22:00:12 -0400

    Create Main solution.swift

---
+++ Parker/Learn to code 1/Logical labyrinth/Main solution.swift
@@ -0,0 +1,21 @@
+for i in 1 ... 8 {
+    moveForward()
+    if isOnGem && isOnClosedSwitch {
+        toggleSwitch()
+        collectGem()
+        turnRight()
+        moveForward()
+        moveForward()
+        collectGem()
+        turnLeft()
+        turnLeft()
+        moveForward()
+        moveForward()
+        turnRight()
+    } else if isOnClosedSwitch {
+        toggleSwitch()
+        turnLeft()
+    } else if isOnGem {
+        collectGem()
+       }
+    }
