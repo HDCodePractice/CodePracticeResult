bb5a9df9d1d0493b966e589a4489007a98366a61
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-05-25 21:25:50 -0400

    Update Logical Labyrinth.swift

--- Parker/Learn to code 1/Logical Labyrinth.swift
+++ Parker/Learn to code 1/Logical Labyrinth.swift
@@ -23,3 +23,26 @@ for i in 1 ... 8 {
 
 SOLUTION WHERE BYTE DOES NOT GO BACK AFTER HE GETS THE LAST GEM:
   
+for i in 1 ... 8 {
+    moveForward()
+    if isOnGem && isOnClosedSwitch {
+        toggleSwitch()
+        collectGem()
+        turnRight()
+        moveForward()
+        moveForward()
+        collectGem()
+        if i < 8 {
+            turnLeft()
+            turnLeft()
+            moveForward()
+            moveForward()
+            turnRight()
+        }
+    } else if isOnClosedSwitch {
+        toggleSwitch()
+        turnLeft()
+    } else if isOnGem {
+        collectGem()
+    }
+}
