78cdd543afe162bc27b12fde0f35e9bdae47ac65
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-05-28 16:37:09 -0400

    Update roll right roll left.swift

--- Parker/Learn to code 1/roll right roll left.swift
+++ Parker/Learn to code 1/roll right roll left.swift
@@ -1,1 +1,17 @@
-
+while !isBlocked || !isBlockedLeft || !isBlockedRight {
+    if isOnClosedSwitch {
+        toggleSwitch()
+        if !isBlockedRight && !isBlocked {
+            turnRight()
+            }
+        } else if !isBlocked {
+        moveForward()
+        if isOnGem {
+            collectGem()
+            }
+        } else if isBlocked && isBlockedLeft {
+        turnRight()
+        } else {
+        turnLeft()
+        }
+    }
