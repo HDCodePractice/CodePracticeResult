d6a11cc93d5d784a7872309a5d557bc421c2170c
老黄 <huangdong@gmail.com>
2020-05-30 10:28:26 -0400

    Update main01.swift

--- hd/012人狗大战/main01.swift
+++ hd/012人狗大战/main01.swift
@@ -4,13 +4,21 @@ class Animals {
     // 基础攻击力是在1到10之间自动生成
     var power = Int.random(in: 1 ... 10)
     // 攻击方式会有所有的攻击方式和对应的攻击力
-    var Attacks : [String : Int] = [:]
+    // var Attacks : [String : Int] = [:]
     // 名字，给你的角色起一个名字吧
     var name = ""
     
+    init(_ name : String){
+        self.name = name
+    }
 }
 
-// 让Animals支持==的protocol
+class Human : Animals{
+}
+
+class Dog : Animals {
+}
+
 extension Animals:Equatable{
     static func == (lhs: Animals, rhs: Animals) -> Bool {
         return lhs.name == rhs.name && 
@@ -19,23 +27,8 @@ extension Animals:Equatable{
     }
 }
 
-class Human : Animals{
-    init(_ name : String){
-        super.init()
-        self.name = name
-    }
-    
-}
-
-class Dog : Animals {
-    init(_ name : String){
-        super.init()
-        self.name = name
-    }
-}
-
 var humans = [Human("Sunny"),Human("MengMeng"),Human("Noah")]
-var dogs = [Dog("SiCheng"),Dog("hdcola"),Dog("Bell")]
+var dogs = [Dog("SiCheng"),Dog("hdcola"),Dog("Fizz")]
 
 for i in humans {
     show("\(i.name) hp:\(i.hp) power:\(i.power)")
@@ -43,12 +36,4 @@ for i in humans {
 
 for i in dogs {
     show("\(i.name) hp:\(i.hp) power:\(i.power)")
-}
-
-
-let rHuman = humans.randomElement()!
-let rDog = dogs.randomElement()!
-
-if let i = humans.firstIndex(of: rHuman) {
-    humans.remove(at: i)
 }
