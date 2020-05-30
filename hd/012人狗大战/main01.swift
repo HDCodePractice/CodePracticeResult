e323a440c488f8acfe3bba5203467e6bc485e914
老黄 <huangdong@gmail.com>
2020-05-30 10:54:53 -0400

    Update main01.swift

--- hd/012人狗大战/main01.swift
+++ hd/012人狗大战/main01.swift
@@ -31,9 +31,45 @@ var humans = [Human("Sunny"),Human("MengMeng"),Human("Noah")]
 var dogs = [Dog("SiCheng"),Dog("hdcola"),Dog("Fizz")]
 
 for i in humans {
-    show("\(i.name) hp:\(i.hp) power:\(i.power)")
+    show("人类\(i.name) 诞生！ hp:\(i.hp) power:\(i.power)")
+}
+for i in dogs {
+    show("狗类\(i.name) 诞生！ hp:\(i.hp) power:\(i.power)")
 }
 
+while !(humans.isEmpty || dogs.isEmpty) {
+    let rHuman = humans.randomElement()!
+    let rDog = dogs.randomElement()!
+    
+    show ("\(rHuman.name)(hp:\(rHuman.hp) power:\(rHuman.power)) VS \(rDog.name)(hp:\(rDog.hp) power:\(rDog.power))开战！")
+    
+    rHuman.hp = rHuman.hp - rDog.power
+    if rHuman.hp <= 0 {
+        show("\(rHuman.name)被\(rDog.name)一招致死！")
+        if let i = humans.firstIndex(of: rHuman) {
+            humans.remove(at: i)
+        }
+    }else{
+        rDog.hp = rDog.hp - rHuman.power
+        if rDog.hp <= 0 {
+            show("\(rDog.name)被\(rHuman.name)一招致死！")
+            if let i = dogs.firstIndex(of: rDog) {
+                dogs.remove(at: i)
+            }
+        }
+    }
+    show ("激战之后\(rHuman.name)(hp:\(rHuman.hp) power:\(rHuman.power)) VS \(rDog.name)(hp:\(rDog.hp) power:\(rDog.power))")
+}
+
+if dogs.isEmpty {
+    show("人类最终统治了地球！最后的人类有：")
+}else{
+    show("狗类最终统治了地球！最后的狗类有：")
+}
+
+for i in humans {
+    show("\(i.name) hp:\(i.hp) power:\(i.power)")
+}
 for i in dogs {
     show("\(i.name) hp:\(i.hp) power:\(i.power)")
 }
