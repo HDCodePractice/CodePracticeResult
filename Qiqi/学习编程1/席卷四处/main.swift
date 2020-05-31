6f37e9d8a1f7679087ff02da96a1272067b9cda5
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-05-31 17:16:21 -0400

    Update main.swift

--- Qiqi/学习编程1/席卷四处/main.swift
+++ Qiqi/学习编程1/席卷四处/main.swift
@@ -1,25 +1,23 @@
 func getT() {
     moveForward()
     collectGem()
-    moveForward()
-    collectGem()
-    turnAround()
-    moveForward()
-    turnRight()
-    moveForward()
-    collectGem()
-    turnAround()
-    moveForward()
+    for i in 1 ... 2 {
+        turnLeft()
+        moveForward()
+        collectGem()
+        turnAround()
+    }
+    turnLeft()
     moveForward()
     collectGem()
 }
 func turnAround() {
     turnLeft()
     turnLeft()
+    moveForward()
 }
 for i in 1 ... 3 {
     getT()
     moveForward()
 }
 getT()
-
