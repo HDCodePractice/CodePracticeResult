8bb9f9e14cd5df41d98303d44824aacdd72c8c1b
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-06-07 17:30:05 -0400

    Update main.swift

--- Qiqi/学习编程1/满足条件时向上爬/main.swift
+++ Qiqi/学习编程1/满足条件时向上爬/main.swift
@@ -1,8 +1,8 @@
-for i in 1 ... 16 {
+for i in 1 ... 12 {
     if isOnGem {
         collectGem()
         turnLeft()
-    } else {
-        moveForward()
     }
+        moveForward()
 }
+collectGem()
