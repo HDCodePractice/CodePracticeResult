3628cada97ff5ed1ead281e1932dd587e2a24d86
oh101yeh <63210750+oh101yeh@users.noreply.github.com>
2020-06-07 17:42:11 -0400

    修改else if

--- hanchen/学习编程1/使用else if/main.swift
+++ hanchen/学习编程1/使用else if/main.swift
@@ -1,8 +1,8 @@
-moveForward()
-collectGem()
-moveForward()
-if isOnClosedSwitch {
-    toggleSwitch()
-}else if isOnGem {
-    collectGem()
+for i in 1 ... 2 {
+    moveForward()
+    if isOnClosedSwitch {
+        toggleSwitch()
+    }else if isOnGem {
+        collectGem()
+    }
 }
