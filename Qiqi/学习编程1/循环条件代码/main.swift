4b96683eb2520d7770e25af229cab82d3b527dff
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-06-07 17:07:21 -0400

    Update main.swift

--- Qiqi/学习编程1/循环条件代码/main.swift
+++ Qiqi/学习编程1/循环条件代码/main.swift
@@ -1,13 +1,8 @@
-for i in 1 ... /*#-editable-code*/12/*#-end-editable-code*/ {
-    //#-editable-code
+for i in 1 ... 12 {
     moveForward()
     if isOnClosedSwitch {
         toggleSwitch()
-        
     }else if isOnGem{
         collectGem()
-        
     }
-    
-    //#-end-editable-code
 }
