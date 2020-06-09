165d582ff8abe77b4b5c91f8ff9881d32a4631ec
Cindy0304 <65630764+Cindy0304@users.noreply.github.com>
2020-06-09 16:23:43 -0400

    尽收囊中完成

---
+++ Cindy/学习编程1/尽收囊中/main.swift
@@ -0,0 +1,18 @@
+func collectDoubbleGem() {
+    collectGem()
+    moveForward()
+    collectGem()
+}
+collectGem()
+moveForward()
+collectDoubbleGem()
+turnRight()
+moveForward()
+collectDoubbleGem()
+turnRight()
+moveForward()
+collectDoubbleGem()
+turnRight()
+moveForward()
+turnRight()
+collectDoubbleGem()
