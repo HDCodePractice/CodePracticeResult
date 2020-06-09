83d28374899f66113318175a041f7c638bbd940e
Cindy0304 <65630764+Cindy0304@users.noreply.github.com>
2020-06-09 17:28:09 -0400

    寻宝完成

---
+++ Cindy/学习编程1/寻宝/main.swift
@@ -0,0 +1,25 @@
+func moveThenToggle() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    }
+func turnAndMove() {
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+}
+moveThenToggle()
+turnAndMove()
+moveThenToggle()
+turnAndMove()
+turnRight()
+moveThenToggle()
+moveThenToggle()
+turnRight()
+turnRight()
+moveForward()
+moveForward()
+moveThenToggle()
+moveThenToggle()
+moveThenToggle()
