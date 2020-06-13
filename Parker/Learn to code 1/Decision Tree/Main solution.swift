5c6fc832183ef9ca257d17901947b00a149bf7da
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-06-13 16:57:09 -0400

    Update Main solution.swift

--- Parker/Learn to code 1/Decision Tree/Main solution.swift
+++ Parker/Learn to code 1/Decision Tree/Main solution.swift
@@ -4,24 +4,20 @@ func theFunction() {
     collectGem()
     turnLeft()
 }
-
 func turnMove3() {
     turnRight()
     moveForward()
     moveForward()
     moveForward()
 }
-func solveLeftSide() {
-    theFunction()
-    turnLeft()
-    moveForward()
-    turnLeft()
-}
 for i in 1 ... 5 {
     moveForward()
     if isOnClosedSwitch {
         toggleSwitch()
-        solveLeftSide()
+        theFunction()
+        turnLeft()
+        moveForward()
+        turnLeft()
     }
     else if isOnGem {
         collectGem()
