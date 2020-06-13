2ee5eaa4553dde6aa2e0e677b8b32543cba1ec19
TypicalNoahVGSuperSniproXDBestPro <59535872+TypicalNoahVGSuperSniproXDBestPro@users.noreply.github.com>
2020-06-13 11:40:14 -0400

    Create main.swift

---
+++ Noah/015Triangle/main.swift
@@ -0,0 +1,17 @@
+func nb (n: Int) {
+    var s = ""
+    for i in 1 ... n {
+        let sn = n - i
+        for k in 0 ... sn {
+            s.append(" ")
+        }
+        for k in 1 ... 2 * i - 1 {
+            s.append("*")
+        }
+        if i < n {
+            s.append("\n")
+        }
+    }
+    show(s)
+}
+nb(n : 6)
