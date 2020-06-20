b71d1c86a0fd843218b3e6c14f4e51ee7b3f2185
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-06-20 11:35:00 -0400

    Update main.swift

--- Sichengthebest/008 乘法口诀/main.swift
+++ Sichengthebest/008 乘法口诀/main.swift
@@ -1,9 +1,9 @@
 var string = ""
-for i in 1...5 {
+for i in 1...9 {
     for j in 1...i {
         string.append("\(i)*\(j)=\(i*j) ")
     }
-    if i < 5 {
+    if i < 9 {
         string.append("\n")
     }
 }
