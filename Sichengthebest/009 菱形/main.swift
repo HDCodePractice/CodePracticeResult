1275145c7b98aa852afee5251fdaeb56ca890477
老黄 <huangdong@gmail.com>
2020-06-20 10:40:25 -0400

    Update main.swift

--- Sichengthebest/009 菱形/main.swift
+++ Sichengthebest/009 菱形/main.swift
@@ -1,26 +1,23 @@
-func printDiamond(n: Int) {
-    var string = ""
-    for i in 1...n {
-        let spaceNumber = n-i
-        for j in 0...spaceNumber {
-            string.append(" ")
-        }
-        for j in 1...2*i-1 {
-            string.append(".")
-        }
-        string.append("\n")
+func appendLind(start: Int,n:Int , s: String) -> String{
+    var str = ""
+    for i in start ... n {
+        str += s
     }
-    for i in 1...n-1 {
-        for j in 0...i {
-            string.append(" ")
-        }
-        for j in 1...(n-i)*2-1 {
-            string.append(".")
-        }
-        if i != n {
-            string.append("\n")
-        }
-    }
-    show(string)
+    return str
 }
-printDiamond(n: 4)
+
+func printing(number : Int) {
+    var d = ""
+    for i in 1 ... number{
+        d += appendLind(start:0,n: number - i, s: " ")
+        d += appendLind(start:1,n: i*2-1, s: ".")
+        d += "\n"
+    }
+    for i in 1 ... number - 1{
+        d += appendLind(start:0,n: i, s: " ")
+        d += appendLind(start:1,n: (number-i)*2-1, s: ".")
+        d += "\n"
+    }
+    show(String(d.prefix(d.count - 1)))
+}
+printing(number : 98)
