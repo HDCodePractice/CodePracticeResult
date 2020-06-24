b8aa3c3f654139f4a35b411a9bbfdeba0ea1de46
老黄 <huangdong@gmail.com>
2020-06-24 17:10:45 -0400

    Update main2.swift

--- hd/016 寻找素数/main2.swift
+++ hd/016 寻找素数/main2.swift
@@ -1,9 +1,5 @@
 func findPrime(n: Int,m: Int) -> [Int] {
     var primes : [Int] = []
-    if n > m {
-        return []
-    }
-    
     for i in n ... m {
         if i > 2 {
             for j in 2 ... i - 1 {
@@ -17,10 +13,8 @@ func findPrime(n: Int,m: Int) -> [Int] {
             primes.append(i)
         }
     }
-    
     return primes
 }
-
 
 for i in findPrime(n: 1, m: 100){
     show(i)
