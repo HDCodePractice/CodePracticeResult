2cbdb1f63f7ccb7d1e535ba364522dddc7778a88
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-06-27 18:29:31 -0400

    Create main.swift

---
+++ Qiqi/学习编程2/Collect The Total/main.swift
@@ -0,0 +1,21 @@
+let totalGems = randomNumberOfGems
+var gemCounter = 0
+while gemCounter != randomNumberOfGems {
+    if isOnGem {
+        collectGem()
+        gemCounter += 1
+    }
+    if isBlocked {
+        if isBlockedRight && isBlockedLeft{
+            turnLeft()
+            turnLeft()
+        }
+        if isBlockedRight {
+            turnLeft()
+        }
+        if isBlockedLeft {
+            turnRight()
+        }
+    }
+    moveForward()
+}
