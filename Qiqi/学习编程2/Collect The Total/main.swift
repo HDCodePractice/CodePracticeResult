0e88de4f126ecb9f47d97fbd54d447678d336c4d
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-06-27 18:32:15 -0400

    Update main.swift

--- Qiqi/学习编程2/Collect The Total/main.swift
+++ Qiqi/学习编程2/Collect The Total/main.swift
@@ -1,21 +1,17 @@
 let totalGems = randomNumberOfGems
 var gemCounter = 0
-while gemCounter != randomNumberOfGems {
-    if isOnGem {
-        collectGem()
-        gemCounter += 1
-    }
-    if isBlocked {
-        if isBlockedRight && isBlockedLeft{
-            turnLeft()
-            turnLeft()
-        }
-        if isBlockedRight {
-            turnLeft()
-        }
-        if isBlockedLeft {
-            turnRight()
-        }
-    }
-    moveForward()
-}
+while gemCounter < totalGems {
+     moveForward()
+     if isOnGem {
+         collectGem()
+         gemCounter = gemCounter + 1
+     }
+     if !isBlockedRight {
+         turnRight()
+     } else if !isBlockedLeft {
+         turnLeft()
+     } else if isBlocked {
+         turnLeft()
+         turnLeft()
+     }
+ }
