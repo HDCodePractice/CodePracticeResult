f94a923c21d4923d63da3378a1e0dc4c7620253f
老黄 <huangdong@gmail.com>
2020-07-04 10:04:39 -0400

    Create main.swift

---
+++ hd/022 Game2048/00生成随机的2/main.swift
@@ -0,0 +1,89 @@
+class Game2048 {
+    var gray : [[Int]] = []
+    var grid : Int = 0
+    /**
+     初始化一个grid x grid个的数组
+     */
+    init(grid : Int){
+        self.grid = grid
+        for i in 1 ... grid {
+            var row : [Int] = []
+            for j in 1 ... grid {
+                row.append(0)
+            }
+            self.gray.append(row)
+        }
+    }
+    
+    /**
+     显示出当前的格子
+     */
+    func showGrid() {
+        var s = ""
+        for i in self.gray{
+            s += "|"
+            for j in i {
+                s += "\(j)|"
+            }
+            s += "\n"
+        }
+        show(String(s.prefix(s.count - 1)))
+    }
+    
+    func up() {
+        appear()
+    }
+    
+    func left() {
+        appear()
+    }
+    
+    func down() {
+        appear()
+    }
+    
+    func right() {
+        appear()
+    }
+    
+    func appear()-> Bool {
+        var emptyGrid = [[Int]] ()
+        for i in 0 ... self.grid - 1 {
+            for j in 0 ... self.grid - 1 {
+                if self.gray[i][j] == 0 {
+                    emptyGrid.append([i,j])
+                }
+            }
+        }
+        if emptyGrid.isEmpty {  // 没有空格
+            return false
+        }else{
+            let r = emptyGrid.randomElement()!
+            self.gray[r[0]][r[1]] = [2,4].randomElement()!
+            return true
+        }
+    }
+}
+
+let myGame = Game2048(grid: 4)
+while true {
+    let input = ask()
+    switch input {
+    case "w":
+        myGame.up()
+        myGame.showGrid()
+    case "a":
+        myGame.left()
+        myGame.showGrid()
+    case "s":
+        myGame.down()
+        myGame.showGrid()
+    case "d":
+        myGame.right()
+        myGame.showGrid()
+    default: 
+        show("只接受 w上 a左 s下 d右 指令")
+    }
+}
+
+myGame.showGrid()
