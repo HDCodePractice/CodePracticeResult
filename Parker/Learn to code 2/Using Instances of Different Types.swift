0f03a56aa9fef2ce4e0312dc05b5e99b2cc67989
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-07-04 18:32:33 -0400

    Update Using Instances of Different Types.swift

--- Parker/Learn to code 2/Using Instances of Different Types.swift
+++ Parker/Learn to code 2/Using Instances of Different Types.swift
@@ -1,24 +1,14 @@
 let expert = Expert()
 let character = Character()
-
-var gemCounter = 0
-
-func characterNavigateAroundWall() {
-    while gemCounter != 2 {
-        if character.isOnGem {
-            character.collectGem()
-            gemCounter += 1 
-        }
-        if !character.isBlockedRight && !character.isBlocked {
-            character.turnRight()
-        }
-        if character.isBlocked && !character.isBlockedRight {
-            expert.turnLockDown()
-            expert.turnLockDown()
-        }
-        character.moveForward()
-    }
-}
 expert.moveForward()
 expert.turnLockUp()
-characterNavigateAroundWall()
+character.moveForward()
+character.collectGem()
+character.moveForward()
+character.turnRight()
+character.moveForward()
+character.moveForward()
+expert.turnLockDown()
+expert.turnLockDown()
+character.moveForward()
+character.collectGem()
