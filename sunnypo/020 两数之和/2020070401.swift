caa279fd4c3c7496e84c5c3bbc9b9ee6ce6381cc
sunnypo <46382526+sunnypo@users.noreply.github.com>
2020-07-04 10:44:16 -0400

    2020070401

--- sunnypo/020 两数之和/2020070401.swift
+++ sunnypo/020 两数之和/2020070401.swift
@@ -2,9 +2,11 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     var result : [Int] = []
     for i in 0...nums.count-1{
         for j in 0...nums.count-1{
-            if target == nums[i] + nums[j]{
-                result.append(i)
-                result.append(j)
+            if j != i{
+                if target == nums[i] + nums[j]{
+                    result.append(i)
+                    result.append(j)
+                }
             }
         }
     }
@@ -15,6 +17,6 @@ for i in twoSum([1,3,5,7,9], 10) {
     show(i)
 }
 
-for i in twoSum([3,3], 6) {
+for i in twoSum([4,1,4], 8) {
     show(i)
 }
