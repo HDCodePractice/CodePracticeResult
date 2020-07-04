a75aabced31960992855e812d7cb8e7969e8e0f3
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-07-04 10:50:44 -0400

    Update main.swift

--- Sichengthebest/020 两数之和/main.swift
+++ Sichengthebest/020 两数之和/main.swift
@@ -3,13 +3,14 @@ func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
     for i in 0...nums.count-1 {
         for j in 0...nums.count-1 {
             if nums[i] + nums[j] == target {
-                return [i,j]
+                if i != j {
+                    return [i,j]
+                }
             }
         }
     }
     return []
 }
-
 for i in twoSum([1,3,5,7,9], 10) {
     show(i)
 }
