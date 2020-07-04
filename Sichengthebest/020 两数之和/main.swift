a7d8c0b6d428ff4d890c29d5201bd49cfb6e0ce0
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-07-04 10:23:05 -0400

    Create main.swift

---
+++ Sichengthebest/020 两数之和/main.swift
@@ -0,0 +1,15 @@
+func twoSum(_ nums: [Int], _ target: Int) -> [Int] {
+    var a : [Int] = []
+    for i in 0...nums.count-1 {
+        for j in 0...nums.count-1 {
+            if nums[i] + nums[j] == target {
+                return [i,j]
+            }
+        }
+    }
+    return []
+}
+
+for i in twoSum([1,3,5,7,9], 10) {
+    show(i)
+}
