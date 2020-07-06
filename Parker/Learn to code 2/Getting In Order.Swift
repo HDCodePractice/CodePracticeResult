deaa508420b963164cf6548ab31f201db7d953d5
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-07-06 14:08:09 -0400

    Create Getting In Order.Swift

---
+++ Parker/Learn to code 2/Getting In Order.Swift
@@ -0,0 +1,14 @@
+characters = [
+    Character(name: .blu),
+    Portal(color: #colorLiteral(red: 0.9098039269, green: 0.4784313738, blue: 0.6431372762, alpha: 1)),
+    Character(name: .hopper),
+    Gem()
+]
+characters.remove(at: 1)
+characters.remove(at: 2)
+characters.insert(Expert(), at: 1)
+var rowPlacement = 0
+for character in characters {
+    world.place(character, at: Coordinate(column: 1, row: rowPlacement))
+    rowPlacement += 1
+}
