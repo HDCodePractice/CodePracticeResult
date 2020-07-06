41d401b986a20ab7921c67dece238007bf58845b
ParkerXChen <65827012+ParkerXChen@users.noreply.github.com>
2020-07-06 14:28:08 -0400

    Create Randomized Lands.swift

---
+++ Parker/Learn to code 2/Randomized Lands.swift
@@ -0,0 +1,20 @@
+    let allTheCoordinates = world.allPossibleCoordinates
+    var heights: [Int] = []
+    let island = randomInt(from: 1, to: 6)
+    heights.append(island)
+    var index = 0
+    for coordinate in allTheCoordinates {
+        if index == heights.count {
+            index = 0
+        }
+        var currentHeight = heights[0]
+        for i in 1 ... randomInt(from: 1, to: 12) {
+            world.place(Block(), at: coordinate)
+        }
+        if coordinate.column < 5 {
+            world.place(Character(name: .blu), at: coordinate)
+        } else if coordinate.column >=  5 && coordinate.column < 7 {
+            world.removeAllBlocks(at: coordinate)
+            world.place(Water(), at: coordinate)
+        }
+    }    
