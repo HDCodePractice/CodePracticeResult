4929ef3fbfb6aedf84f9ae25c276d77967d1152a
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-07-11 12:13:59 -0400

    Create main.swift

---
+++ Qiqi/Making Your Own Portals/main.swift
@@ -0,0 +1,23 @@
+let greenPortal = Portal(color: #colorLiteral(red: 0.4028071761, green: 0.7315050364, blue: 0.2071235478, alpha: 1))
+world.place(greenPortal, atStartColumn: 1, startRow: 5, atEndColumn: 5, endRow: 1)
+func collectOneGem() {
+    for i in 1 ... 3 {
+        moveForward()
+        collectGem()
+        turnRight()
+        turnRight()
+        moveForward()
+        turnRight()
+    }
+    moveForward()
+    collectGem()
+    turnRight()
+    turnRight()
+    greenPortal.isActive = true
+}
+greenPortal.isActive = false
+moveForward()
+collectOneGem()
+moveForward()
+greenPortal.isActive = false
+collectOneGem()
