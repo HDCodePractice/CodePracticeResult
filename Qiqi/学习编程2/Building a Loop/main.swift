0f9c4f2c5431d782180ab9188e7659f8ab99846e
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2020-07-11 12:37:46 -0400

    Create main.swift

---
+++ Qiqi/学习编程2/Building a Loop/main.swift
@@ -0,0 +1,22 @@
+world.place(Block(), atColumn: 6, row: 3)
+world.place(Block(), atColumn: 6, row: 0)
+world.place(Block(), atColumn: 1, row: 0)
+world.place(Block(), atColumn: 0, row: 2)
+world.place(Block(), atColumn: 1, row: 3)
+world.place(Block(), atColumn: 3, row: 3)
+bluePortal.isActive = false
+yellowPortal.isActive = false
+greenPortal.isActive = false
+let character = Character()
+world.place(character, facing: south, atColumn: 7, row: 3)
+var gemCounter = 0
+while gemCounter != totalGems {
+    if character.isBlocked {
+        character.turnRight()
+    }
+    if character.isOnGem {
+        character.collectGem()
+        gemCounter += 1
+    }
+    character.moveForward()
+}
