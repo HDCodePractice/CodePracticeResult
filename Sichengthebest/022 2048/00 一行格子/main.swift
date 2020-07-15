aac1968643e6046d7c37a4fe6915d11d269398e8
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-07-15 10:28:26 -0400

    GOOD VERSION

--- Sichengthebest/022 2048/00 一行格子/main.swift
+++ Sichengthebest/022 2048/00 一行格子/main.swift
@@ -1,6 +1,6 @@
-func gen24() -> Int {
+func gen24() -> [Int] {
     var nums = [2,4]
-    return nums.randomElement()!
+    return nums
 }
 
 func initRow(_ length : Int) -> [Int] {
@@ -11,10 +11,12 @@ func initRow(_ length : Int) -> [Int] {
     return ret
 }
 
-
+let length = 4
 var row : [Int] = initRow(4)
 show("\(row)")
-for i in 0...row.count-1 {
-    row[i] += gen24()
+for i in 1...length {
+    for i in 0...row.count-1 {
+        row[i] = gen24().randomElement()!
+    }
+    show("\(row)")
 }
-show("\(row)")
