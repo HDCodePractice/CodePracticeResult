cab1994d991408677420315ff5d44a9dcf7b6a3e
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-07-18 11:25:17 -0400

    Create 挑战 main.swift

---
+++ Sichengthebest/CodeLearn1-String/011 截取字符串/挑战 main.swift
@@ -0,0 +1,24 @@
+let s = """
+一层楼更上
+千里目欲穷
+入海流黄河
+依山尽白日
+"""
+var final: [String] = []
+var finale = ""
+let a = s.split(separator: "\n")
+let b = a.count-1
+for i in 0 ... b {
+    final.append(String(a[b-i]))
+}
+show("\(final)")
+for i in 0...final.count-1 {
+    if i < final.count-1 {
+        finale.append(String(final[i].suffix(2)))
+        finale.append(String(final[i].prefix(3)) + "\n")
+    } else {
+        finale.append(String(final[i].suffix(2)))
+        finale.append(String(final[i].prefix(3)))
+    }
+}
+show(finale)
