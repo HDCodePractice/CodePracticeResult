3b23788822cf1a5c3f593d4fee7441a8d31151ed
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2020-07-18 11:33:37 -0400

    Update 挑战 main.swift

--- Sichengthebest/CodeLearn1-String/011 截取字符串/挑战 main.swift
+++ Sichengthebest/CodeLearn1-String/011 截取字符串/挑战 main.swift
@@ -11,9 +11,7 @@ let a = s.split(separator: "\n")
 let b = a.count-1
 for i in 0 ... b {
     final.append(String(a[b-i]))
-}
-for i in 0...final.count-1 {
-    if i < final.count-1 {
+    if i < b {
         finale.append(String(final[i].suffix(2)))
         finale.append(String(final[i].prefix(3)) + "\n")
     } else {
