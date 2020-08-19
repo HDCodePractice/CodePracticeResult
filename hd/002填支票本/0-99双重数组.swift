71cb4521445256b3a756a247d8b88ab6a2f28234
老黄 <huangdong@gmail.com>
2020-08-19 10:58:19 -0400

    Create 0-99双重数组.swift

---
+++ hd/002填支票本/0-99双重数组.swift
@@ -0,0 +1,23 @@
+func trans(_ number: Int) -> String{
+    var tran0to100 = [
+        ["","One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen", "Twenty"],
+        ["","-One", "-Two", "-Three", "-Four", "-Five", "-Six", "-Seven", "-Eight", "-Nine", "-Ten", "-Eleven", "-Twelve", "-Thirteen", "-Fourteen", "-Fifteen", "-Sixteen", "-Seventeen", "-Eighteen", "-Nineteen", "-Twenty"],
+        ["Twenty"],
+        ["Thirty"],
+        ["Forty"],
+        ["Fifty"],
+        ["Sixty"],
+        ["Seventy"],
+        ["Eighty"],
+        ["Ninety"]]
+    
+    if number < 20{
+        return "\(tran0to100[0][number % 10]) Dollars"
+    }else{
+        return "\(tran0to100[Int(number / 10) ][0])\(tran0to100[1][number % 10]) Dollars"
+    }
+}
+
+for i in 0 ... 99 {
+    show("\(i) : \(trans(i))")
+}
