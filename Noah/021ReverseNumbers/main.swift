2674b146020d923aadca1d46b2eddf58b6a4f258
xxX☆Snipro☆Xxx ☆〜（ゝ。∂）#TheMythicNoahC <59535872+xxXSniproXxxNoahCx@users.noreply.github.com>
2020-08-22 11:44:41 -0400

    Update main.swift

--- Noah/021ReverseNumbers/main.swift
+++ Noah/021ReverseNumbers/main.swift
@@ -15,21 +15,18 @@ func reverse(_ x: Int) -> Int {
     var s = ""
     var n = abs(x)
     var b = Array(String(abs(x)))
-    var c = b.count
-    for i in 1 ... c {
-        var one = n / pom(10, c - i)
-        n = n - one * pom(10, c - i) 
+    for i in 1 ... b.count {
+        var one = n / pom(10, b.count - i)
+        n = n - one * pom(10, b.count - i) 
         show(one)
         s = "\(one)\(s)"
     }
-    if x >= 0 {
-        return Int(s)!
-    } else {
+    if x < 0 {
         return Int("-\(s)")!
     }
+    return Int(s)!
 }
 
 show(reverse(123))
 show(reverse(-123))
 show(reverse(98900))
-
