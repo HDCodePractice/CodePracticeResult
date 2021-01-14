43de4290375f2e136fb5ec4094b91faf3434ff6f
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-01-14 17:03:15 -0500

    Create 寻宝.swift

---
+++ runrun/学习编程1/寻宝.swift
@@ -0,0 +1,31 @@
+func goandtoggle() {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnRight()
+    turnRight()
+    moveForward()
+    moveForward()
+    moveForward()
+    moveForward()
+    }
+    
+    func gojustonceandtoggle(){
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnRight()
+    turnRight()
+    moveForward()
+    moveForward()
+    
+  }
+  turnRight()
+goandtoggle()
+goandtoggle()
+turnRight()
+gojustonceandtoggle()
+gojustonceandtoggle()
