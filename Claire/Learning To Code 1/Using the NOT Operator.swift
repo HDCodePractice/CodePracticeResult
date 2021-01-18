42af758c4f83810cfab3fea7e6045a646cf5bc1a
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-01-17 21:11:02 -0500

    Create Using the NOT Operator.swift

---
+++ Claire/Learning To Code 1/Using the NOT Operator.swift
@@ -0,0 +1,15 @@
+for i in 1 ... 4 {
+    moveForward()
+    if !isOnGem {
+        turnLeft()
+        moveForward()
+        moveForward()
+        collectGem()
+        turnRight()
+        turnRight()
+        moveForward()
+        moveForward()
+        turnLeft()
+    }
+    collectGem()
+}
