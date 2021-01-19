f6a6acb9c2e53d1174c2d02ee2d59c27f80fa502
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-01-18 19:41:21 -0500

    Create 宝石农场.swift

---
+++ runrun/学习编程1/宝石农场.swift
@@ -0,0 +1,24 @@
+func go() {
+    turnRight()
+    moveForward()
+    collectGem()
+    moveForward()
+    collectGem()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+}
+for i in 1 ... 3 {
+    go()
+}
