ff2a86628ac66e7506f9ee3466283db17d001b73
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-01-19 17:04:47 -0500

    Create 定义更巧妙的函数.swift

---
+++ runrun/学习编程1/定义更巧妙的函数.swift
@@ -0,0 +1,23 @@
+func collectOrToggle() {
+if isOnClosedSwitch {
+   toggleSwitch()
+}else if isOnGem {
+ collectGem()
+}
+
+func go() {
+    for i in 1 ... 4 {
+        moveForward()
+        collectOrToggle()
+    }
+}
+go()
+turnLeft()
+moveForward()
+moveForward()
+turnLeft()
+go()
+turnRight()
+moveForward()
+turnRight()
+go()
