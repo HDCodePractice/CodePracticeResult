012bee5514716003f4dc17390d3585a09c5df016
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-01-31 19:46:24 -0500

    Create 设定正确的传送门.swift

---
+++ runrun/学习编程2/设定正确的传送门.swift
@@ -0,0 +1,26 @@
+func moveCollect() {
+    moveForward()
+    collectGem()
+}
+func go() {
+    turnLeft()
+    turnLeft()
+}
+moveForward()
+moveCollect()
+go()
+bluePortal.isActive=false
+moveForward()
+moveCollect()
+go()
+bluePortal.isActive=true
+pinkPortal.isActive=false
+moveForward()
+moveForward()
+moveForward()
+collectGem()
+go()
+pinkPortal.isActive=true
+moveForward()
+go()
+moveCollect()
