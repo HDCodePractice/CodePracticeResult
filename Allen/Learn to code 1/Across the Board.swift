64810514e72fafef9e2406fe01fc200aacc0ab32
Allenyzh <49468724+Allenyzh@users.noreply.github.com>
2021-02-05 21:16:40 -0500

    Create Across the Board.swift

---
+++ Allen/Learn to code 1/Across the Board.swift
@@ -0,0 +1,25 @@
+func go() {
+    collectGem()
+    moveForward()
+}
+func turnleftandgo() {
+    collectGem()
+    turnLeft()
+    moveForward()
+    turnLeft()
+}
+func turnrightandgo() {
+    collectGem()
+    turnRight()
+    moveForward()
+    turnRight()
+}
+go()
+go()
+turnrightandgo()
+go()
+go()
+turnleftandgo()
+go()
+go()
+go()
