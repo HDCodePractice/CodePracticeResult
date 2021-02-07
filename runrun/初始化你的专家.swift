341e435ca9c99a0a4daf12c049e00320b1bca12a
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-02-06 20:20:57 -0500

    Create 初始化你的专家.swift

---
+++ runrun/初始化你的专家.swift
@@ -0,0 +1,20 @@
+let expert=Expert
+expert.moveForward()
+expert.moveForward()
+expert.moveForward()
+expert.turnLockUp()
+expert.turnRight()
+for i in 1 ... 3 {
+    expert.turnRight()
+    expert.moveForward()
+    expert.moveForward()
+    expert.moveForward()
+    expert.turnRight()
+    expert.moveForward()
+    expert.moveForward()
+    expert.moveForward()
+    expert.collectGem()
+    expert.turnRight()
+}
+
+
