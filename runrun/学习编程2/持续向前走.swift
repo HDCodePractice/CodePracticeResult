58435b80bf98b50bee6afc908c345afc240003df
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-02-18 18:39:23 -0500

    Create 持续向前走.swift

---
+++ runrun/学习编程2/持续向前走.swift
@@ -0,0 +1,22 @@
+let expert =Expert()
+func move (distance: Int) {
+for i in 1 ... distance {
+        expert.moveForward()
+    }
+}
+move(distance: 6)
+expert.turnRight()
+move(distance: 2)
+expert.turnRight()
+move(distance: 5)
+expert.turnLeft()
+move(distance: 5)
+expert.turnLeft()
+expert.turnLockUp()
+expert.turnLeft()
+move(distance: 3)
+expert.turnRight()
+move(distance: 3)
+expert.turnRight()
+move(distance: 4)
+expert.collectGem()
