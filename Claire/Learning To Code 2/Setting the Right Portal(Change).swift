7188886bf401d4ad40fd594bfcc3268ce438e84e
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-02-18 20:20:59 -0500

    Create Setting the Right Portal(Change).swift

---
+++ Claire/Learning To Code 2/Setting the Right Portal(Change).swift
@@ -0,0 +1,23 @@
+var gemCounter = 0
+while gemCounter != 4 {
+    if gemCounter == 0 {
+        bluePortal.isActive = false
+        pinkPortal.isActive = false
+    } else if gemCounter == 1 {
+        bluePortal.isActive = true
+        pinkPortal.isActive = true
+    } else if gemCounter == 3 {
+        bluePortal.isActive = false
+    }
+    moveForward()
+    if isOnGem {
+        collectGem()
+        gemCounter += 1
+    }
+    if isBlocked {
+        turnLeft()
+        turnLeft()
+    } else {
+        moveForward()
+    }
+}
