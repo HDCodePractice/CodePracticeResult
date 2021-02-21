ad9ea30fcd7cd53326c0ac3a82af3368d28419d3
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-02-21 11:02:47 -0500

    Update 003猜数字.swift

--- Claire/003猜数字.swift
+++ Claire/003猜数字.swift
@@ -1,16 +1,13 @@
-let rNumber = Int.random(in: 0 ... 999)
-var time = 0
+var rNumber = Int.random(in: 0 ... 99)
 var input = askForNumber("选一个三位数？")
-time += 1
-var number = rNumber
-while number != input {
-    if number < input {
+var time = 1
+while rNumber != input {
+    if rNumber < input {
         show("哦不！你猜的太大啦！重新猜一遍吧！")
-        time += 1
-    } else if number > input {
+    } else if rNumber > input {
         show("哦不！你猜的太小啦！重新猜一遍吧！")
-        time += 1
     }
     input = askForNumber("再来试试？")
+    time += 1
 }
-show("恭喜你答对啦！正确的数字是\(number)！你一共用了\(time)猜中了这个数字，重新来一次吧？")
+show("恭喜你答对啦！正确的数字是\(rNumber)！你一共用了\(time)猜中了这个数字，重新来一次吧？")
