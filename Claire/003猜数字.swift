72d2ce8c971dedb0c704a47b527363a08df41ab7
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-02-21 10:47:01 -0500

    Update 003猜数字.swift

--- Claire/003猜数字.swift
+++ Claire/003猜数字.swift
@@ -1,7 +1,8 @@
 let rNumber = Int.random(in: 0 ... 999)
-var input = askForNumber("选一个三位数？")
-var number = rNumber
 var time = 0
+var input = askForNumber("选一个三位数？")
+time += 1
+var number = rNumber
 while number != input {
     if number < input {
         show("哦不！你猜的太大啦！重新猜一遍吧！")
