2d906222f3b61650c13b0f7de1520882ed5a3665
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-03-07 10:53:30 -0500

    Update 上下转动.swift

--- runrun/上下转动.swift
+++ runrun/上下转动.swift
@@ -1,41 +1,40 @@
-let expert=Expert()
-let character=Character()
-func go() {
-    character.turnRight()
-    character.move(distance: 2)
-    character.turnRight()
-    character.turnRight()
-    character.collectGem()
-    character.move(distance: 2)
-}
+let expert = Expert()
+let characte = Character()
+var gemCount = 0
 for i in 1 ... 4 {
     expert.turnLock(up: true, numberOfTimes: 4)
     expert.turnRight()
 }
-character.move(distance: 2)
-character.collectGem()
+while gemCount<3{
+    if !character.isBlockedRight {
+        character.turnRight()
+    }
+    character.moveForward()
+    if character.isOnGem {
+        character.collectGem()
+        gemCount+=1
+    }
+}
 character.turnRight()
 character.move(distance: 2)
-character.collectGem()
-character.turnRight()
-character.move(distance: 4)
-character.collectGem()
-character.turnRight()
-character.moveForward()
 for i in 1 ... 4 {
-    expert.turnLock(up: false, numberOfTimes:3)
+    expert.turnLock(up: false, numberOfTimes: 3)
     expert.turnRight()
 }
-character.turnLeft()
-character.moveForward()
-character.turnRight()
-character.turnRight()
-character.collectGem()
-character.move(distance: 2)
-go()
-character.turnRight()
-character.move(distance: 2)
-go()
-character.turnRight()
-character.move(distance: 2)
-character.collectGem()
+while gemCount<7 {
+    if !character.isBlockedRight{
+        character.turnRight()
+    }else if character.isBlocked {
+        if character.isBlockedLeft {
+            character.turnLeft()
+            character.turnLeft()
+        }else {
+            character.turnLeft()
+        }
+    }
+    character.moveForward()
+    if character.isOnGem {
+        character.collectGem()
+        gemCount+=1
+    }
+}
