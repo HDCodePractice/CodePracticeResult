0051b81d1e6f45e6963341f24136ff209315b4e6
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-03-11 20:23:18 -0500

    Update 过河.swift

--- runrun/学习编程二/过河.swift
+++ runrun/学习编程二/过河.swift
@@ -1,15 +1,7 @@
-let expert = Expert()
-world.place(expert, atColumn: 1, row: 8)
-func moveDist(dist: Int ) {
-    for i in 1 ... dist {
-        expert.collectGem()
-        expert.moveForward()
+let hdcola = Expert()
+for i in 1 ... 5 {
+    for j in 1 ... 8 {
+        world.place(hdcola, atColumn: i, row: j)
+        hdcola.collectGem()
     }
 }
-moveDist(dist: 3)
-expert.turnLockDown()
-expert.turnLeft()
-moveDist(dist: 4)
-expert.turnLockUp()
-expert.turnRight()
-moveDist(dist: 5)
