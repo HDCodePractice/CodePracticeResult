3f9bea43b01baa8d588267361b73f1937ee63125
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-03-11 20:25:15 -0500

    Create Rivers to Cross (BUG).swift

---
+++ Claire/Learning To Code 2/Rivers to Cross (BUG).swift
@@ -0,0 +1,10 @@
+let hdcola = Expert()
+func collectLineGem(startRow: Int, endRow: Int, colum: Int) {
+    for i in startRow ... endRow {
+        world.place(hdcola, atColumn: colum, row: i)
+        hdcola.collectGem()
+    }
+}
+collectLineGem(startRow: 5, endRow: 8, colum: 1)
+collectLineGem(startRow: 1, endRow: 5, colum: 5)
+collectLineGem(startRow: 5, endRow: 5, colum: 4)
