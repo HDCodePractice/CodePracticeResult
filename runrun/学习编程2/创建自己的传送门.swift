fee2276a6ae27cd727afb8f68cb7c4966c2b2911
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-03-18 18:04:15 -0400

    Update 创建自己的传送门.swift

--- runrun/学习编程2/创建自己的传送门.swift
+++ runrun/学习编程2/创建自己的传送门.swift
@@ -1,23 +1,7 @@
-let greenPortal = Portal(color: #colorLiteral(red: 0.0, green: 0.9768045545, blue: 0.0, alpha: 1.0))
-world.place(greenPortal, atStartColumn: 1, startRow: 5, atEndColumn: 5, endRow: 1)
-greenPortal.isActive=false
-var m = 0
-while m < 24{
-    turnRight()
-    if isBlocked {
-        if isBlockedLeft {
-            turnLeft()
-            turnLeft()
-        }else {
-            turnLeft()
-        }
-    }
-    moveForward()
-    m+=1
-    collectGem()
-    if m == 12 {
-        greenPortal.isActive=true
-    }else if m == 15 {
-        greenPortal.isActive=false
+let hd = Character()
+for i in 0 ... 6 {
+    for j in 0 ... 8 {
+        world.place(hd, atColumn:i, row: j)
+        hd.collectGem()
     }
 }
