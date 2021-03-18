9b7153e1ddf8d29517a754a2b076aa9ec6fb13d7
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-03-18 17:59:07 -0400

    Update 连接并解决.swift

--- runrun/学习编程2/连接并解决.swift
+++ runrun/学习编程2/连接并解决.swift
@@ -1,27 +1,13 @@
-var m = 0
-let block = Block()
-let block1 = Block()
-let block2 = Block()
-let block3 = Block()
-world.place(block, atColumn: 2, row: 2)
-world.place(block1, atColumn: 2, row: 2)
-world.place(block, atColumn: 4, row: 2)
-world.place(block2, atColumn: 6, row: 2)
-world.place(block3, atColumn: 6, row: 2)
-while m < 28{
-    turnRight()
-        if isBlocked {
-        if isBlockedLeft {
-            turnLeft()
-            turnLeft()
-        }else {
-            turnLeft()
-        }
-        }
-    moveForward()
-    m+=1
-    collectGem()
-    if isOnClosedSwitch {
-        toggleSwitch()
-    }
-}
+let runrun = Expert()
+world.place(runrun, atColumn: 2, row: 4)
+runrun.toggleSwitch()
+world.place(runrun, atColumn: 2, row: 0)
+runrun.collectGem()
+world.place(runrun, atColumn: 4, row: 4)
+runrun.toggleSwitch()
+world.place(runrun, atColumn: 4, row: 0)
+runrun.collectGem()
+world.place(runrun, atColumn: 6, row: 4)
+runrun.toggleSwitch()
+world.place(runrun, atColumn: 6, row: 0)
+runrun.collectGem()
