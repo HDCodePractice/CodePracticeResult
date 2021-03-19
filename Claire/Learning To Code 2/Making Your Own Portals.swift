3f52b522093b805b8ebe307f949ec6d98241e825
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-03-18 20:19:51 -0400

    Update Making Your Own Portals.swift

--- Claire/Learning To Code 2/Making Your Own Portals.swift
+++ Claire/Learning To Code 2/Making Your Own Portals.swift
@@ -1,10 +1,8 @@
 let whitePortal = Portal(color: #colorLiteral(red: 0.9999960065, green: 1.0, blue: 1.0, alpha: 1.0))
 world.place(whitePortal, atStartColumn: 0, startRow: 5, atEndColumn: 6, endRow: 1)
-var gemCounter = 0
-while gemCounter < 8 {
+for i in 1 ... 21 {
     if isOnGem {
         collectGem()
-        gemCounter += 1
     } else if isBlocked {
         turnRight()
         turnRight()
