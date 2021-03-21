c21a83727299860c367a03b535926a4ed5d01370
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-03-21 16:48:33 -0400

    Create 按序排列.swift

---
+++ runrun/学习编程2/按序排列.swift
@@ -0,0 +1,17 @@
+characters = [
+Character(name: .blu),
+portal(color: pink),
+Character(name: .hopper),
+Gem()
+]
+var blockLocations = [
+    Coordinate(column: 0, row: 0),
+    Coordinate(column: 3, row: 3),
+    Coordinate(column: 3, row: 0),
+    Coordinate(column: 0, row: 3)
+]
+for coordinate in blockLocations {
+    for i in 1 ... 5 {
+        world.place(Block(), at: coordinate)
+    }
+}
