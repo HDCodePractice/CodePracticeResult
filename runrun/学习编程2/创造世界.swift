0d84d3a1f22fbb3e3716cb7d574652bc8ff993b6
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-03-22 14:12:28 -0400

    Update 创造世界.swift

--- runrun/学习编程2/创造世界.swift
+++ runrun/学习编程2/创造世界.swift
@@ -1,22 +1,40 @@
-let mengxiang = Character()
-let coco = Character()
-let yueqing = Character()
-let vincent = Character()
-let laurianne = Character()
-let pinkPortal = Portal(color: #colorLiteral(red: 0.9299970865, green: 0.4419257045, blue: 0.6210277081, alpha: 1.0))
-world.place(pinkPortal, atStartColumn: 0, startRow: 4, atEndColumn: 6, endRow: 1)
-world.place(mengxiang, atColumn: 0, row: 5)
-world.place(vincent, atColumn: 1, row: 5)
-world.place(yueqing, atColumn: 2, row: 5)
-world.place(coco, atColumn: 2, row: 4)
-world.place(laurianne, atColumn: 1, row: 4)
-world.place(Gem(), atColumn: 0, row:0 )
-mengxiang.moveForward()
-let yellowPortal = Portal(color: #colorLiteral(red: 0.9626445174, green: 0.9242319465, blue: 0.0, alpha: 1.0))
-world.place( yellowPortal, atStartColumn: 2, startRow: 3, atEndColumn: 6, endRow: 2)
-coco.moveForward()
-coco.turnRight()
-coco.turnRight()
-coco.move(distance: 9)
-world.place(Gem(), atColumn: 6, row:11 )
-coco.collectGem()
+var rowTwo = world.row(2)
+var character:[Coordinate] = []
+for i in 1...12 {
+    for coordinate in rowTwo {
+        world.place(Block(), at: coordinate)
+    }
+    character.append(rowTwo.remove(at: 0))
+}
+for character in character {
+    world.place(Character(), facing: .east, at: character)
+}
+var blockLocations = [
+    Coordinate(column: 0, row: 0),
+    Coordinate(column: 3, row: 3),
+    Coordinate(column: 3, row: 0),
+    Coordinate(column: 0, row: 3)
+]
+for coordinate in blockLocations {
+    for i in 1 ... 5 {
+        world.place(Block(), at: coordinate)
+    }
+}
+let allCoordinates = world.allPossibleCoordinates
+var isLandCoordinates:[Coordinate] = []
+var waterCoordinates:[Coordinate] = []
+for coordinate in allCoordinates {
+    if coordinate.column > 0 && coordinate.column < 11 && coordinate.row > 0 && coordinate.row < 11 {
+        isLandCoordinates.append(coordinate)
+    } else {
+        waterCoordinates.append(coordinate)
+    }
+}
+for coordinate in isLandCoordinates {
+    world.place(Block(), at: coordinate)
+}
+for coordinate in waterCoordinates {
+    world.removeAllBlocks(at: coordinate)
+    world.place(Water(), at: coordinate)
+}
+
