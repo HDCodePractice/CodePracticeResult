623b1ac255a0fb09774983ccf4af5b9082c13a8d
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-03-25 19:40:30 -0400

    Create Appending to and Array.swift

---
+++ Claire/Learning To Code 2/Appending to and Array.swift
@@ -0,0 +1,12 @@
+let allCoordinates = world.allPossibleCoordinates
+var blockSet: [Coordinate] = []
+for coordinate in allCoordinates{
+    if coordinate.column > 5 || coordinate.row < 4{
+        blockSet.append(coordinate)
+    }
+}
+for coordinate in blockSet {
+    for i in 1 ... 6 {
+        world.place(Block(), at: coordinate)
+    }
+}
