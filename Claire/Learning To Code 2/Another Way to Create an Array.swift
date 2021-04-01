b64ae4cb6d99bdc77d8c2041cc7349bbf99fe4ee
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-04-01 19:07:22 -0400

    Create Another Way to Create an Array.swift

---
+++ Claire/Learning To Code 2/Another Way to Create an Array.swift
@@ -0,0 +1,12 @@
+let allCoordinates = world.allPossibleCoordinates
+for coordinate in allCoordinates {
+    let height = coordinate.column + coordinate.row
+    for i in 0 ... height {
+        world.place(Block(), at: coordinate)
+    }
+    if height >= 8 && height < 10 {
+        world.place(Character(name: .blu), at: coordinate)
+    } else if height > 9 {
+        world.place(Character(name: .byte), at: coordinate)
+    }
+}
