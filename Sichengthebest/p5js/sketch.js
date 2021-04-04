f4083b3acbe283ea8ceb7980e38fb903bf8a15cd
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-04-04 11:06:20 -0400

    Draw

--- Sichengthebest/p5js/sketch.js
+++ Sichengthebest/p5js/sketch.js
@@ -1,13 +1,18 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(500,500);
 }
 
 function draw() {
   background(220);
+  fill(93,93,93);
   square(100,200,200);
-  fill(9,212,34);
   triangle(300,200,100,200,200,50);
-  fill(9,212,34);
-  rect(170,300,60,100);
-  fill(222,212,34);
+  fill(101,67,33);
+  rect(170,280,60,120);
+  fill(1,1,1);
+  circle(220,340,10);
+  fill(255,255,255);
+  circle(200,150,60);
+  line(200,120,200,180);
+  line(170,150,230,150);
 }
