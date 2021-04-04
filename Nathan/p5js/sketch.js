4afc41d9734511c7219351b41d080891a4da18dd
Silverfox66 <81840529+Silverfox66@users.noreply.github.com>
2021-04-04 10:55:19 -0400

    My micky mouse project

---
+++ Nathan/p5js/sketch.js
@@ -0,0 +1,17 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  fill(255,255,255);
+  circle(200,200,200);
+  fill(211,55,53);
+  circle(100,100,100);
+  circle(300,100,100);
+  fill(30,30,30)
+  circle(160,160,30)
+  circle(240,160,30)
+  
+  circle(200,230,10);
+}
