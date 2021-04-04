e5700eca7bed6914ee98bfc4d06164c3864da2da
NinjagoJaden1 <cheung.jaden@gmail.com>
2021-04-04 07:55:49 -0700

    A mouse

---
+++ Jaden/p5js/sketch.js
@@ -0,0 +1,18 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(320);
+  fill(255,255,255);
+  circle(200,200,200);
+  fill(216,61,99);
+  circle(100,100,100);
+  circle(300,100,100);
+  fill(30,30,30);
+  circle(160,160,30);
+  circle(240,160,30);
+  circle(200,230,10);
+  line(200,230,140,220);
+
+}
