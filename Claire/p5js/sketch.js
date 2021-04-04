14bad027adfa9b3fd30f670628ae9b69fbcbebd9
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-04-04 10:55:07 -0400

    Draw a Panda

---
+++ Claire/p5js/sketch.js
@@ -0,0 +1,19 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  circle(200,200,200);
+  fill(30,30,30)
+  circle(120,100,100);
+  circle(275,100,100);
+  circle(200,230,25);
+  fill(30,30,30)
+  circle(150,175,50);
+  fill(255,255,255)
+  circle(150,175,25);
+  circle(245,175,50);
+  circle(245,175,25);
+  
+}
