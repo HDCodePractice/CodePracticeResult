3dba1ec8bbc48450a9fc019965fd48b1d66977b9
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-04-04 10:54:53 -0400

    Drawing

---
+++ Sichengthebest/p5js/sketch.js
@@ -0,0 +1,13 @@
+function setup() {
+  createCanvas(400, 400);
+}
+
+function draw() {
+  background(220);
+  square(100,200,200);
+  fill(9,212,34);
+  triangle(300,200,100,200,200,50);
+  fill(9,212,34);
+  rect(170,300,60,100);
+  fill(222,212,34);
+}
