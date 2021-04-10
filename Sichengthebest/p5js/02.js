cfd42be97e14022e4b06b1d38a1d538f50d0c439
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-04-10 11:55:35 -0400

    Reorganize folder

---
+++ Sichengthebest/p5js/02.js
@@ -0,0 +1,12 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+   
+  function draw() {
+    background('blue');
+    circle(200,200,200)
+    circle(100,200,100)
+    circle(300,200,100)
+    square(100,200,10)
+    square(300,200,10)
+  }
