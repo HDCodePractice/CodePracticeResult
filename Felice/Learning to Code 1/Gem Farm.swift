7c9ee2e3b9ec5f2e2d2b3100815b6a75224a63cb
felicechu <81840596+felicechu@users.noreply.github.com>
2021-04-14 16:37:24 -0400

    Complete Gem Farm

---
+++ Felice/Learning to Code 1/Gem Farm.swift
@@ -0,0 +1,22 @@
+func solveSquare() {
+    moveForward()
+    collectGem()
+    toggleSwitch()
+}
+for i in 1 ... 3 {
+    turnLeft()
+    solveSquare()
+    solveSquare()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    solveSquare()
+    solveSquare()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnRight()
+    moveForward()
+}
