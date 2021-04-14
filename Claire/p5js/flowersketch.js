2016a536d59c0cd18ba0ecb62ab17aab73decbac
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-04-14 18:39:50 -0400

    added flower

---
+++ Claire/p5js/flowersketch.js
@@ -0,0 +1,15 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(90,195,98);
+    circle(75,99,150)
+    circle(75,250,150)
+    circle(250,99,150)
+    circle(250,250,150)
+    fill(234,50,35)
+    circle(163,185,150)
+    fill(239,133,51)
+  }
+  
