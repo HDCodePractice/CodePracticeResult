f22e0b02b936ae241d3a84215fe6befde011a926
felicechu <81840596+felicechu@users.noreply.github.com>
2021-04-14 16:31:13 -0400

    Complete Branch Out

---
+++ Felice/Learning to Code 1/Branch Out.swift
@@ -0,0 +1,15 @@
+for i in 1 ... 3 {
+    moveForward()
+    moveForward()
+    turnRight()
+    for j in 1 ... 7 {
+        moveForward()
+    }
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    for k in 1 ... 7 {
+        moveForward()
+    }
+    turnRight()
+}
