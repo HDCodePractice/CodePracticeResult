ab6caf34e80cda095d3df9154c80cccd7d3f30f2
ParkerXChen <parkerxchen@gmail.com>
2021-04-17 10:07:29 -0400

    Add My Adjustable Flower

--- Parker/P5JS/DrawingGallery/MyDrawings/cat/sketch.js
+++ Parker/P5JS/DrawingGallery/MyDrawings/cat/sketch.js
@@ -1,19 +1,9 @@
 function setup() {
-  createCanvas(500, 400);
+  createCanvas(500, 400);  
 }
 
 function draw() {
-  background("white");
   fill("lightgrey");
-  triangle(280, 280, 350, 150, 390, 170);
-  ellipse(190, 280, 40, 30);
-  line(180, 180, 160, 290);
-  ellipse(230, 210, 130, 150);
-  line(230, 190, 195, 300);
-  ellipse(145, 290, 30, 20);
-  ellipse(180, 300, 30, 20);
-  ellipse(270, 260, 70, 80);
-  ellipse(250, 290, 40, 30);
   triangle(160, 45, 200, 65, 175, 95);
   triangle(280, 45, 240, 65, 265, 95);
   ellipse(220, 110, 100, 100);
