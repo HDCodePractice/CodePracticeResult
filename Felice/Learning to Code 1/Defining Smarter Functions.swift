8633336783a4b5ef40776ddd1c65aa41f9df06eb
felicechu <81840596+felicechu@users.noreply.github.com>
2021-04-17 17:22:05 -0400

    Complete Defining Smarter Functions

--- Felice/Learning to Code 1/Defining Smarter Functions.swift
+++ Felice/Learning to Code 1/Defining Smarter Functions.swift
@@ -1,4 +1,12 @@
-    for i in 1 ... 15 {
+func collectOrToggle() {    
+    moveForward()
+        if isOnGem {
+            collectGem()
+        }else if isOnClosedSwitch {
+            toggleSwitch()
+        }
+}
+for i in 1 ... 15 {
         collectOrToggle()
         if isBlocked && isBlockedRight {
             turnLeft()
