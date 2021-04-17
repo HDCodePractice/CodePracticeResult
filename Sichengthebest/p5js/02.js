5a178935ead154ef602f6566fe9d778bd1a6f996
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-04-17 10:12:18 -0400

    Update 02.js

--- Sichengthebest/p5js/02.js
+++ Sichengthebest/p5js/02.js
@@ -1,4 +1,7 @@
 function setup() {
+    // 正常操作的时候 width = windowWidth 和 height = windowHeight
+    // 可以把 width 和 height 的值改成不同大小的屏幕测试
+    
     r = random(1,255)
     g = random(1,255)
     b = random(1,255)
@@ -6,7 +9,9 @@ function setup() {
     g2 = random(1,255)
     b2 = random(1,255)
 
-    createCanvas(800,600);
+    height = windowHeight
+    width = windowWidth
+    createCanvas(height,width);
 
     // 图片背景
     img = createImg('https://images.unsplash.com/photo-1490750967868-88aa4486c946?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1500&q=80')
