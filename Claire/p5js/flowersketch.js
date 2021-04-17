4120204ca9ed78727862fae5be1d1e5f256196ca
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-04-17 10:10:30 -0400

    changed Flower & Panda

--- Claire/p5js/flowersketch.js
+++ Claire/p5js/flowersketch.js
@@ -1,15 +1,14 @@
 function setup() {
-    createCanvas(400, 400);
+    createCanvas(windowWidth-100,windowHeight-100);
   }
   
   function draw() {
-    background(90,195,98);
-    circle(75,99,150)
-    circle(75,250,150)
-    circle(250,99,150)
-    circle(250,250,150)
-    fill(234,50,35)
-    circle(163,185,150)
-    fill(239,133,51)
+    background(220);
+    fill(255,0,0);
+    circle(width/4,height/4,width/2);
+    circle(width*3/4,height*3/4,width/2);
+    circle(width*3/4,height/4,width/2);
+    circle(width/4,height*3/4,width/2);
+    fill(255,255,0);
+    circle(width/2,height/2,width/2);
   }
-  
--- Claire/p5js/sketch.js
+++ Claire/p5js/sketch.js
@@ -4,27 +4,40 @@ function setup() {
 
 function draw() {
   background(220);
-  circle(245,175,15);
-  // right tiny eye (black)
-  circle(120,100,100);
+  circle(width/3.333,height/4,width/4);
   // left ear (black)
-  circle(275,100,100);
+  fill(30,30,30);
+  // fill black
+  circle(width*3/1.455,height*3/4,width/4);
   // right ear (black)
-  circle(200,230,25);
-  // nose (black)
-  circle(150,175,50);
+  fill(30,30,30);
+  // fill black
+  circle(width/2,height/1.739,width/16);
+  circle(width/2.667,height/2.268,width/8);
   // left big eye (black)
-  circle(150,175,15);
-  // left tiny eye (black)
-  circle(245,175,50);
+  fill(30,30,30);
+  // fill black
+  circle(width*3/1.633,height*3/2.268,width/8);
   // right big eye (black)
   fill(30,30,30);
   // fill black
-  circle(200,200,200);
+  circle(width*3/1.633,height*3/2.286,width/26.667);
+  // right tiny eye (black)
+  fill(30,30,30);
+  // fill black
+  circle(width/2.667,height/2.286,width/26.667);
+  // left tiny eye (black)
+  fill(30,30,30);
+  // fill black
+  circle(width/2,height/2,width/2);
   // face (white)
-  circle(150,175,25);
+  fill(250,250,250);
+  // fill white
+  circle(width/2.667,height/2.286,width/16);
   // left small eye (white)
-  circle(245,175,25);
+  fill(250,250,250);
+  // fill white
+  circle(width/1.633,height/2.286,width/16);
   // right small eye (white)
   fill(250,250,250);
   // fill white
