8723284afdca9c6c3dc24c290a32a97578a2380e
felicechu <81840596+felicechu@users.noreply.github.com>
2021-04-22 19:00:20 -0400

    Complete Land of Bounty

--- Felice/Learning to Code 1/Land of Bounty.swift
+++ Felice/Learning to Code 1/Land of Bounty.swift
@@ -1,1 +1,19 @@
-
+while !isBlocked || !isBlockedRight {
+    if isOnGem {
+        collectGem()
+    }
+    if isOnClosedSwitch {
+        toggleSwitch()
+    }
+    if isBlocked && isBlockedLeft {
+        turnRight()
+        moveForward()
+        turnRight()
+    }
+    if isBlocked && !isBlockedLeft {
+        turnLeft()
+        moveForward()
+        turnLeft()
+    }
+    moveForward()
+}
