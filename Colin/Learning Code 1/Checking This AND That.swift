51cecfa12441e795f71f385e9553dcaf857ec3b6
colinw0214 <82334488+colinw0214@users.noreply.github.com>
2021-04-22 18:32:58 -0400

    Checking This AND That Done

---
+++ Colin/Learning Code 1/Checking This AND That.swift
@@ -0,0 +1,17 @@
+for i in 1 ... 7 {
+    moveForward()
+    if isOnGem && isBlockedLeft {
+        collectGem()
+        turnRight()
+        moveForward()
+        moveForward()
+        toggleSwitch()
+        turnLeft()
+        turnLeft()
+        moveForward()
+        moveForward()
+        turnRight()
+    } else if isOnGem {
+        collectGem()
+    }
+}
