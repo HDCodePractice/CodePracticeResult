c1f3c349789f609a0d76e84068e25bdba8163ef8
Danny Huang <huangdong@gmail.com>
2021-04-21 21:53:30 -0400

    for个没完没了

--- hd/p5js/02.js
+++ hd/p5js/02.js
@@ -1,31 +1,31 @@
 function setup() {
-  createCanvas(windowWidth, windowHeight-60);
-  frameRate(5); 
+  createCanvas(windowHeight-100,windowHeight-100);
+  
 }
 
-function draw() {
+function draw() {   
+  let beeX = random(0,width)
+  let beeY = random(0,height)
+  let beeSize = random(25,100)
+  let beeDist = beeSize/2;
+  let beeColor =  random(0,255)
   background(220);
-  let flowerX = random(0,width);
-  let flowerY = random(0,height);
-  let flowerSize = random(25,150);
-  let flowerDistance = flowerSize / 2 ;
-
-  fill(255,0,0);
-  // 左上角的花瓣
-  circle(flowerX-flowerDistance,flowerY-flowerDistance,flowerSize);
-
-  // 左下角的花瓣
-  circle(flowerX-flowerDistance,flowerY+flowerDistance,flowerSize);
-
-  // 右下角的花瓣
-  circle(flowerX+flowerDistance,flowerY+flowerDistance,flowerSize);
-
-  // 右上角的花瓣
-  circle(flowerX+flowerDistance,flowerY-flowerDistance,flowerSize);
-  // 花芯
-  fill(255,255,0);
-  circle(flowerX,flowerY,flowerSize)
-  
+  fill(0,0,0);
+  triangle(beeX + beeSize,beeY + beeDist/2,beeX + beeSize + beeDist,beeY + beeDist,beeX + beeDist,beeY + beeDist)
+  fill(255,255,0)
+  ellipse(beeX + beeDist + beeDist/2,beeY + beeDist*7/8,beeSize+beeDist/4,beeSize - beeDist*2/3)
+  circle(beeX + beeDist/2,beeY + beeDist/2,beeSize - beeDist/4)
+  fill(0,0,0)
+  line(beeX + beeDist*3/2,beeY + beeDist*1/5,beeX + beeDist + beeDist/2,beeY + beeDist + beeDist/2)
+  line(beeX + beeDist*2 + beeDist/4,beeY + beeDist/3,beeX + beeDist*2 + beeDist/4,beeY + beeDist + beeDist/3)
+  circle(beeX + beeDist/5,beeY + beeDist/5,beeSize - beeDist*2/1.23)
+  circle(beeX + beeDist/1.2,beeY + beeDist/5,beeSize - beeDist*2/1.23)
+  line(beeX + beeDist/1.1,beeY - beeDist/4,beeX + beeDist/1.1,beeY - beeDist/2)
+  line(beeX + beeDist/5,beeY - beeDist/4,beeX + beeDist/5,beeY - beeDist/2)
+  circle(beeX + beeDist/5,beeY - beeDist/1.5,beeSize - beeDist - beeDist/1.5);
+  circle(beeX + beeDist/1.1,beeY -  beeDist/1.5,beeSize - beeDist - beeDist/1.5);
+  fill(255,255,255);
+  arc(beeX + beeDist/2,beeY + beeDist/1.1,beeSize - beeDist - beeDist/4,beeSize - beeDist - beeDist/4,PI,QUARTER_PI,CHORD);
 }
 
 function windowResized() {
