47991e7ea3711118b529a01a4e409726df9ed215
felicechu <81840596+felicechu@users.noreply.github.com>
2021-04-29 18:14:31 -0400

    Complete Adjusting Your Algorithm

---
+++ Felice/Learning to Code 1/Adjusting Your Algorithm.swift
@@ -0,0 +1,22 @@
+func  navigateAroundWall () {
+    if isBlockedRight {
+        if isBlocked {
+            turnLeft ()
+            if isBlocked {
+                turnLeft ()
+            }
+        }
+        moveForward ()
+    }   else {
+        turnRight ()
+        moveForward ()
+    }
+}
+
+while  !isOnClosedSwitch {
+    navigateAroundWall ()
+    if isOnGem {
+        collectGem ()
+    }
+}
+toggleSwitch ()
