d2bda76999fe5b1ebbaacd9c840047eb062d4f1d
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-04-30 16:38:51 -0700

    bee back and forth

--- Jaden/p5js/myscript.js
+++ Jaden/p5js/myscript.js
@@ -1,6 +1,7 @@
-let circleY = 25;
-let circleX = 250;
-let speed = 5;
+let circleY = 250;
+let circleX = 25;
+let speedX = 5;
+let speedY = 5;
 
 function setup() {
     createCanvas(500, 500);
@@ -9,21 +10,72 @@ function setup() {
   
 function draw() {
     background(220);
-    rectangle(circleX,circleY,50, 50);
-    if (circleY < 250 && circleY >= 25 && circleX >= 250 && circleX < 475) {
-        circleY += speed;
-        circleX += speed;
-  
-    } else if (circleX <= 475 && circleX > 250 && circleY >= 250 && circleY < 475) {
-        circleY += speed;
-        circleX -= speed;
-    } else if (circleX <= 250 && circleX > 25 && circleY <= 475 && circleY > 250) {
-        circleY -= speed;
-        circleX -= speed;
-    } else if (circleX >= 25 && circleX < 250 && circleY <= 250 && circleY > 25) {
-        circleY -= speed;
-        circleX += speed;
+    if (circleX > width-26){
+        speedX = -5;
+    }else if (circleX < 26){
+        speedX = 5;
     }
+
+    if (speedX > 0){
+        if (circleX <width/2){
+            speedY = -1;
+        }else{
+            speedY = 1;
+        }    
+    }else{
+        if (circleX <width/2){
+            speedY = 1;
+        }else{
+            speedY = -1;
+        }
+    
+    }
+
+    circleX += speedX;
+    circleY += speedY;
+    circle(circleX, circleY,50);
+}
+//     let x = 20;
+//     for (let i = 1; i < 8; i++) {
+//         circle(x,y,10)
+        
+//         if( y <width/2 ){
+//             x=x+1
+//         }else{
+//             y+=1
+        
+//         }
+//     }
+// }
+
+
+
+
+
+
+
+
+
+
+
+
+    // circle(circleX,circleY,50);
+    // if (circleY < 250 && circleY >= 25 && circleX >= 250 && circleX < 475) {
+    //     circleY += speed;
+    //     circleX += speed;
+    //     circle(circleX,circleY,50)
+  
+    // } else if (circleX <= 475 && circleX > 250 && circleY >= 250 && circleY < 475) {
+    //     circleY += speed;
+    //     circleX -= speed;
+    // } else if (circleX <= 250 && circleX > 25 && circleY <= 475 && circleY > 250) {
+    // //     circleY -= speed;
+    // //     circleX -= speed;
+    // } else if (circleX >= 25 && circleX < 250 && circleY <= 250 && circleY > 25) {
+    //     circleY -= speed;
+    //     circleX += speed;
+    //     circle(circleX, circleY,50)
+    // }
     
 
 
@@ -49,7 +101,7 @@ function draw() {
 //     circleY += speed;
 //     circleX -= speed;
     
- }
+ 
 
 // function draw() {
 //     background(220);
