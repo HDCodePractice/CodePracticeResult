b77cb543af9072d0b4478f43e9ccc79a388ba1c7
colinw0214 <82334488+colinw0214@users.noreply.github.com>
2021-04-30 16:02:25 -0400

    Seeking Seven Gems DOne

---
+++ Colin/Learning Code 2/Seeking Seven Gems.swift
@@ -0,0 +1,12 @@
+var gemCounter=0
+while gemCounter<7{
+    moveForward()
+    if isBlocked{
+        turnLeft()
+        turnLeft()
+    }
+    if isOnGem{
+        collectGem()
+        gemCounter=gemCounter+1
+    }
+}
