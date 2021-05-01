24d4dd98751f3bbd96e4e781f956b680ff194996
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-05-01 07:11:28 -0700

    ball move 4 wall

--- Jaden/p5js/myscript.js
+++ Jaden/p5js/myscript.js
@@ -1,8 +1,8 @@
-let circleY = 250;
-let circleX = 25;
+let circleY = 25;
+let circleX = 250;
 let speedX = 5;
 let speedY = 5;
-
+let speed = 5;
 function setup() {
     createCanvas(500, 500);
     
@@ -10,30 +10,46 @@ function setup() {
   
 function draw() {
     background(220);
-    if (circleX > width-26){
-        speedX = -5;
-    }else if (circleX < 26){
-        speedX = 5;
-    }
+    circle(circleX,circleY,50);
+    if (circleY < 250 && circleY >= 25 && circleX >= 250 && circleX < 475) {
+        circleY += speed;
+        circleX += speed;
 
-    if (speedX > 0){
-        if (circleX <width/2){
-            speedY = -1;
-        }else{
-            speedY = 1;
-        }    
-    }else{
-        if (circleX <width/2){
-            speedY = 1;
-        }else{
-            speedY = -1;
-        }
+    } else if (circleX <= 475 && circleX > 250 && circleY >= 250 && circleY < 475) {
+        circleY += speed;
+        circleX -= speed;
+    } else if (circleX <= 250 && circleX > 25 && circleY <= 475 && circleY > 250) {
+        circleY -= speed;
+        circleX -= speed;
+    } else if (circleX >= 25 && circleX < 250 && circleY <= 250 && circleY > 25) {
+        circleY -= speed;
+        circleX += speed;
+    }
     
-    }
+    // if (circleX > width-26){
+    //     speedX = -5;
+    // }else if (circleX < 26){
+    //     speedX = 5;
+    // }
 
-    circleX += speedX;
-    circleY += speedY;
-    circle(circleX, circleY,50);
+    // if (speedX > 0){
+    //     if (circleX <width/2){
+    //         speedY = -1;
+    //     }else{
+    //         speedY = 1;
+    //     }    
+    // }else{
+    //     if (circleX <width/2){
+    //         speedY = 1;
+    //     }else{
+    //         speedY = -1;
+    //     }
+    
+    // }
+
+    // circleX += speedX;
+    // circleY += speedY;
+    // circle(circleX, circleY,50);
 }
 //     let x = 20;
 //     for (let i = 1; i < 8; i++) {
