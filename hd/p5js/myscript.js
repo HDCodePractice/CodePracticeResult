2e005a82f09817104c4033fdb9ed54b3e9bfc279
Danny Huang <huangdong@gmail.com>
2021-05-01 11:49:14 -0400

    花儿和太阳

--- hd/p5js/myscript.js
+++ hd/p5js/myscript.js
@@ -1,20 +1,66 @@
-let circleY = 25;
-let circleX = 250;
+let size = 300;
+let circleX = 0;
+let circleY = 100;
 let speed = 5;
+let speedY = speed/3;
+let speedX = speed;
+let isNight = false;
+
+function mysun(x,y,size,suncolor){
+
+}
+
+function myflower(x,y,size,color1,color2){
+    
+}
 
 function setup() {
-    createCanvas(500, 500);
+    createCanvas(size, size);
 }
   
 function draw() {
-    background(220);
-    circle(circleX,circleY,50); 
-    if (circleY > height - 30) {
-        speed = -5;
+    background(220); 
+    if (circleX > width) {
+        circleX = 0;
+        circleY = 100;
+        if (isNight){
+            isNight = false;
+        }else{
+            isNight = true;
+        }
+    }else if (circleX < 25){
+        speedX = speed;
     }
-    if(circleY < 30){
-        speed = 5;
+    if (circleX < width/2){
+        circleY -= speedY;
+    }else{
+        circleY += speedY;
     }
-    circleY += speed;
-    circleX += speed;
+    circleX += speedX;
+    if (isNight){
+        fill(250,250,250);
+    }else{
+        fill(241,203,2);
+    }
+    circle(circleX,circleY,50);
+
+    let flowerX = width/2;
+    let flowerY = height-height/5;
+    let flowerWidth = width/5;
+    let flowerHeight = width/5;
+    let flowerXDistance = flowerWidth/2;
+    let flowerYDistance = flowerHeight/2;
+
+    fill(135,40,158)
+    // // 左上角的花瓣
+    ellipse(flowerX-flowerXDistance,flowerY-flowerYDistance,flowerWidth,flowerHeight)
+    // // 左下角的花瓣
+    ellipse(flowerX-flowerXDistance,flowerY+flowerYDistance,flowerWidth,flowerHeight)
+    // // 右下角的花瓣
+    ellipse(flowerX+flowerXDistance,flowerY+flowerYDistance,flowerWidth,flowerHeight)
+    // // 右上角的花瓣
+    ellipse(flowerX+flowerXDistance,flowerY-flowerYDistance,flowerWidth,flowerHeight)
+    // 花蕊
+    fill(253,242,99)
+    ellipse(flowerX,flowerY,flowerWidth,flowerHeight);
 }
