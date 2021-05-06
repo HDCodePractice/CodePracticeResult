fb814563b0be8960fcba0cea83c563b30ea0fbc8
silvestar32 <82227329+silvestar32@users.noreply.github.com>
2021-05-06 16:26:14 -0700

    Complete 3 gems, 4 switches

---
+++ Grace/Learning Code 2/3 Gems 4 Switches.swift
@@ -0,0 +1,18 @@
+var gemCounter = 0
+var switchCounter = 0
+while gemCounter < 3 || switchCounter < 4 {
+    if !isBlocked {
+        moveForward()
+        if isOnGem && gemCounter < 3{
+            collectGem()
+            gemCounter = gemCounter + 1
+        } else if isOnClosedSwitch && switchCounter < 4 {
+            toggleSwitch()
+            switchCounter += 1
+        }
+    } else if !isBlockedRight {
+        turnRight()
+    } else if !isBlockedLeft {
+        turnLeft()
+    }
+}
