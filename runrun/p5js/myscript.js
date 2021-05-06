2224c222809b5216f45180d6f8bdafefb5949821
runrun0128 <77179953+runrun0128@users.noreply.github.com>
2021-05-05 20:29:14 -0400

    小花和太阳

--- runrun/p5js/myscript.js
+++ runrun/p5js/myscript.js
@@ -1,32 +1,76 @@
-let circleY = 25;
-let circleX = 250;
-let speedY = 15;
- let speedX = 15;
+    let size = 300;
+    let circleX = 0;
+    let circleY = 100;
+    let speed = 5;
+    let speedY = speed/3;
+    let speedX = speed;
+    let isNight = false;
+   
 
 function setup() {
-    createCanvas(500, 500);
-    background(220);
+    createCanvas(size, size);
 }
+function mysun(x,y,color1){
+    fill(color1)
+    circle(x,y,50);
+}
+
+function myflower(x,y,size,color1,color2){
+    let flowerX = x
+    let flowerY = y
+    let flowerWidth = size
+    let flowerHeight =size
+    let flowerXDistance = flowerWidth/2;
+    let flowerYDistance = flowerHeight/2;
+    fill(color2)
+    // // 左上角的花瓣
+    ellipse(flowerX-flowerXDistance,flowerY-flowerYDistance,flowerWidth,flowerHeight)
+    // // 左下角的花瓣
+    ellipse(flowerX-flowerXDistance,flowerY+flowerYDistance,flowerWidth,flowerHeight)
+    // // 右下角的花瓣
+    ellipse(flowerX+flowerXDistance,flowerY+flowerYDistance,flowerWidth,flowerHeight)
+    // // 右上角的花瓣
+    ellipse(flowerX+flowerXDistance,flowerY-flowerYDistance,flowerWidth,flowerHeight)
+    // 花蕊
+    fill(color1)
+    ellipse(flowerX,flowerY,flowerWidth,flowerHeight); 
+}
+
+
   
 function draw() {
-    background(220);
-    circle(circleX,circleY,50); 
-    if (circleY > height-30) {
-        speedX = -15;
-        speedY = -15;
-    }else if (circleX > width-30) {
-        speedX = 15;
-        speedY = -15;
+    background(220); 
+    if (circleX > width) {
+        circleX = 0;
+        circleY = 100;
+        if (isNight){
+            isNight = false;
+        }else{
+            isNight = true;
+        }
+    }else if (circleX < 25){
+        speedX = speed;
+    }
+    if (circleX < width/2){
+        circleY -= speedY;
+    }else{
+        circleY += speedY;
+    }
+    circleX += speedX;
+    if (isNight){
+        myflower(width/2,height-height/5,width/5,'rgb(253,242,100)','rgb(135,40,158)');
+        myflower(width/5,height-height/10,width/10,'rgb(0,242,100)','rgb(0,40,158)');
+        myflower(width*4/5,height-height/10,width/10,'rgb(0,242,5)','rgb(0,40,158)');
+        mysun(circleX,circleY,'rgb(250,250,250)');
+
+    }else{
+        myflower(width/2,height-height/5,width/5,'rgb(250,250,100)','rgb(135,40,148)');
+        myflower(width/5,height-height/10,width/10,'rgb(253,240,100)','rgb(0,50,158)');
+        myflower(width*4/5,height-height/10,width/10,'rgb(253,240,5)','rgb(0,50,158)');
+        mysun(circleX,circleY,'rgb(241,203,2)')
+    }
+  
  
-    }else if (circleX < 30){
-        speedX = -15;
-        speedY = 15;
-
-    }else if (circleY < 30) {
-        speedX = 15
-        speedY = 15;
-    } 
-
-    circleY += speedX;
-    circleX += speedY;
+   
+    
 }
