7047da71a3f67d89fb68da0e4cffa0ae88439e50
silvestar32 <82227329+silvestar32@users.noreply.github.com>
2021-05-19 19:40:44 -0700

    complete train your expert

---
+++ Grace/Learning Code 2/Train your expert.swift
@@ -0,0 +1,25 @@
+func move() {
+    expert.moveForward()
+    expert.moveForward()
+}
+func turnAround(){
+    expert.turnLeft()
+    expert.turnLeft()
+}
+for i in 1...3{
+    move()
+    expert.collectGem()
+    expert.turnLockDown()
+    turnAround()
+    move()
+    expert.turnRight()
+}
+move()
+expert.moveForward()
+for i in 1...3{
+    expert.turnLeft()
+    move()
+    expert.collectGem()
+    turnAround()
+    move()
+}
