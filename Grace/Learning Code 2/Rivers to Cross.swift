e134cc6b078c2e6b9b85a8031de20c25da6c5d96
silvestar32 <82227329+silvestar32@users.noreply.github.com>
2021-05-22 13:34:56 -0700

    Update Rivers to Cross.swift

--- Grace/Learning Code 2/Rivers to Cross.swift
+++ Grace/Learning Code 2/Rivers to Cross.swift
@@ -1,21 +1,21 @@
 let expert = Expert()
 world.place(expert, facing: south, atColumn: 1, row: 8)
-func collectGemMoveForward() { 
+func collectGemMove() { 
     expert.collectGem()
     expert.moveForward()
 }
 for i in 1 ... 3 {
-    collectGemMoveForward()
+    collectGemMove()
 }
 expert.turnLockDown()
 expert.turnLeft()
-collectGemMoveForward()
+collectGemMove()
 expert.move(distance: 2)
-collectGemMoveForward()
+collectGemMove()
 expert.turnLockUp()
 expert.turnRight()
-collectGemMoveForward()
-collectGemMoveForward()
+collectGemMove()
+collectGemMove()
 expert.moveForward()
-collectGemMoveForward()
-collectGemMoveForward()
+collectGemMove()
+collectGemMove()
