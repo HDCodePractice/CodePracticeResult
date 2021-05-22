a827b617d47e37cd1490c1a39d5c9fa04def5b8c
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-05-21 21:32:16 -0400

    Improve

--- Sichengthebest/p5js/07.js
+++ Sichengthebest/p5js/07.js
@@ -4,6 +4,8 @@ var beeX;
 var beeY;
 var score = 0;
 let speed = 10;
+let speed2 = 6;
+let scoreadd = 7;
 
 function setup() {
     createCanvas(windowWidth,windowHeight-45);
@@ -52,8 +54,22 @@ function drawBee(beeX,beeY,beeSize){
 
 }
 
+function drawRect(txt,x,y,fillColor,size,textcolor) {
+    textSize(15)
+    fill(fillColor)
+    rect(x,y,2*size,size)
+    fill(textcolor)
+    text(txt,x+5,y+25)
+}
+
 function draw() {
     background(120,226,255);
+    drawRect("Default",250,10,[100,205,200],40,250)
+    drawRect("Easy",350,10,[107,215,105],40,250)
+    drawRect("Medium",450,10,[250,200,0],40,250)
+    drawRect("Hard",550,10,[250,0,0],40,250)
+    drawRect("Hardcore",650,10,[120,9,2],40,250)
+    drawRect("Impossible",750,10,0,40,250)
     if (keyIsPressed){
         if (keyCode === LEFT_ARROW) {
             if (xposition > 25) {
@@ -65,23 +81,54 @@ function draw() {
             } 
         }
     } 
+    if (mouseIsPressed) {
+        if (mouseX > 250 && mouseX < 290 && mouseY > 10 && mouseY < 90) {
+            speed = 10
+            speed2 = 6
+            scoreadd = 5
+        } else if (mouseX > 350 && mouseX < 390 && mouseY > 10 && mouseY < 90) {
+            speed = 12
+            speed2 = 4
+            scoreadd = 3
+        } else if (mouseX > 450 && mouseX < 490 && mouseY > 10 && mouseY < 90) {
+            speed = 10.5
+            speed2 = 5.5
+            scoreadd = 4.5
+        } else if (mouseX > 550 && mouseX < 590 && mouseY > 10 && mouseY < 90) {
+            speed = 9
+            speed2 = 7
+            scoreadd = 6
+        } else if (mouseX > 650 && mouseX < 690 && mouseY > 10 && mouseY < 90) {
+            speed = 7.5
+            speed2 = 8.5
+            scoreadd = 7.5
+        } else if (mouseX > 750 && mouseX < 790 && mouseY > 10 && mouseY < 90) {
+            speed = 5
+            speed2 = 11
+            scoreadd = 10
+        }
+    }
     myflower(xposition,yposition,30);
-    beeY += 6;
+    beeY += speed2;
     if (beeY >= height-60) {
         if (beeX < xposition+60 && beeX > xposition-60) {
-            score += 1;
+            score += scoreadd;
         } else {
-            score -= 1;
+            score -= 3;
         }
         beeY = 20;
         beeX = random(20,width);
     }
     drawBee(beeX,beeY,20);
     fill(220)
-    rect(0,0,100,35)
+    rect(0,0,200,35)
     fill(0,0,0);
     textSize(20)
     text(`Score: ${score}`,5,25);
+    text(`Flower speed: ${speed}\nBee speed:${speed2}`,5,70)
+    textSize(15)
+    text('Use the left-right arrow keys to steer.',5,150)
+    text(`Points added each time you get a bee: ${scoreadd}`,5,170)
 }
 
 function windowResized() {
