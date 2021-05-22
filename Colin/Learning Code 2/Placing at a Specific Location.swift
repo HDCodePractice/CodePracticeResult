2b957e6729374ae90ae8388f887ab8ee3a92069f
colinw0214 <82334488+colinw0214@users.noreply.github.com>
2021-05-22 16:48:38 -0400

    Update Placing at a Specific Location.swift

--- Colin/Learning Code 2/Placing at a Specific Location.swift
+++ Colin/Learning Code 2/Placing at a Specific Location.swift
@@ -1,8 +1,4 @@
 let expert = Expert()
-func turnAround(){
-    expert.turnRight()
-    expert.turnRight()
-}
 world.place(expert, atColumn: 1, row: 6)
 expert.collectGem()
 world.place(expert, atColumn: 1, row: 1)
