404aa4abd3b4a360088f814e1389b7dc8b75e01a
colinw0214 <82334488+colinw0214@users.noreply.github.com>
2021-05-27 16:38:57 -0400

    Update Connect and Solve.swift

--- Colin/Learning Code 2/Connect and Solve.swift
+++ Colin/Learning Code 2/Connect and Solve.swift
@@ -1,13 +1,8 @@
-let block1 = Block()
-let block2 = Block()
-let block3 = Block()
-let block4 = Block()
-let block5 = Block()
-world.place(block1, atColumn: 2, row: 2)
-world.place(block2, atColumn: 2, row: 2)
-world.place(block3, atColumn: 4, row: 2)
-world.place(block4, atColumn: 6, row: 2)
-world.place(block5, atColumn: 6, row: 2)
+world.place(Block(), atColumn: 2, row: 2)
+world.place(Block(), atColumn: 2, row: 2)
+world.place(Block(), atColumn: 4, row: 2)
+world.place(Block(), atColumn: 6, row: 2)
+world.place(Block(), atColumn: 6, row: 2)
 var gemCounter = 0
 while gemCounter < 3{
     moveForward()
