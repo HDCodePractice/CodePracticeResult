41601b1491813e60464ea7868a3f7caaea5c38a7
felicechu <81840596+felicechu@users.noreply.github.com>
2021-05-27 19:18:37 -0400

    Complete Connect and Solve

--- Felice/Learning Code 2/Connect and Solve.swift
+++ Felice/Learning Code 2/Connect and Solve.swift
@@ -1,13 +1,24 @@
-let felice =  Expert ()
-world.place (felice, atColumn : 2 , row : 4 )
-felice.toggleSwitch ()
-world.place (felice, atColumn : 2 , row : 0 )
-felice.collectGem ()
-world.place (felice, atColumn : 4 , row : 4 )
-felice.toggleSwitch ()
-world.place (felice, atColumn : 4 , row : 0 )
-felice.collectGem ()
-world.place (felice, atColumn : 6 , row : 4 )
-felice.toggleSwitch ()
-world.place (felice, atColumn : 6 , row : 0 )
-felice.collectGem ()
+world.place(Block(), atColumn: 2, row: 2)
+world.place(Block(), atColumn: 2, row: 2)
+world.place(Block(), atColumn: 4, row: 2)
+world.place(Block(), atColumn: 6, row: 2)
+world.place(Block(), atColumn: 6, row: 2)
+var gemCounter = 0
+while gemCounter < 3 {
+    if isOnClosedSwitch {
+        toggleSwitch()
+    } else if isOnGem {
+        collectGem()
+        gemCounter += 1
+    } else {
+        if !isBlockedRight {
+            turnRight()
+            moveForward()
+        } else if isBlocked {
+            turnLeft()
+            turnLeft()
+        } else {
+            moveForward()
+        }
+    }
+}
