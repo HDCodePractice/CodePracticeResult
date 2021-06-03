fe5b4a039cafce80e45f3cca092e96083c6b3c77
felicechu <81840596+felicechu@users.noreply.github.com>
2021-06-03 19:13:39 -0400

    Complete Iteration Exploration

--- Felice/Learning Code 2/Iteration Exploration.swift
+++ Felice/Learning Code 2/Iteration Exploration.swift
@@ -1,5 +1,5 @@
 let columns = [ 0 , 1 , 2 , 3 , 4 ]
 for currentColumn in columns {
-    world. place ( Gem (), atColumn : currentColumn, row : 1 )
-    world. place ( Switch (), atColumn : currentColumn, row : 1 )
+    world.place (Gem(), atColumn : currentColumn, row : 1 )
+    world.place (Switch(), atColumn : currentColumn, row : 1 )
 }
