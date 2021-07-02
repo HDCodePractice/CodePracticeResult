6209ccd51e8709284b759776551b74eb01eb7354
Jqazwsxedcrfvtgb <86443690+Jqazwsxedcrfvtgb@users.noreply.github.com>
2021-07-02 14:47:09 -0400

    Create 定义更巧妙的函数.swift

---
+++ Jason/学习编程一/定义更巧妙的函数.swift
@@ -0,0 +1,38 @@
+func collectOrToggle() {
+  if isOnClosedSwitch {
+    toggleSwitch()
+    
+  } else if isOnGem {
+collectGem()
+    
+  }
+
+}
+moveForward()
+moveForward()
+    collectOrToggle()
+moveForward()
+moveForward()
+collectOrToggle()
+turnLeft()
+moveForward()
+moveForward()
+turnLeft()
+moveForward()
+moveForward()
+collectOrToggle()
+moveForward()
+moveForward()
+collectOrToggle()
+turnRight()
+moveForward()
+turnRight()
+moveForward()
+
+
+moveForward()
+
+collectOrToggle()
+moveForward()
+moveForward()
+collectOrToggle()
