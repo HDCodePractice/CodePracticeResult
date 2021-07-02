c41d97d20c713a4c2dae5c22c19c3dc70fcd790b
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-02 16:02:10 -0400

    Create 使用“非”运算符.swift

---
+++ Charles/学习编程一/使用“非”运算符.swift
@@ -0,0 +1,20 @@
+for i in 1 ... 4 {
+    moveForward()
+    if isOnGem
+    {
+        collectGem()
+        
+    } else{turnLeft()
+    
+        moveForward()
+        moveForward()
+        collectGem()
+        turnLeft()
+        turnLeft()
+        moveForward()
+        moveForward()
+        turnLeft()
+    }
+    
+}
+
