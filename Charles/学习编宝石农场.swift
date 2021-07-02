e0a51cb28186b150ef85edeb73f32a9c992c50d1
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-02 15:41:04 -0400

    Create 学习编宝石农场.swift

---
+++ Charles/学习编宝石农场.swift
@@ -0,0 +1,25 @@
+for i in 1 ... 3 {
+    turnRight()
+    moveForward()
+    collectGem()
+    moveForward()
+    collectGem()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    
+}
+
+    
+    
