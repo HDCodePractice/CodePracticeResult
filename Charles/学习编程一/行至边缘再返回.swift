d804b49bb46ddeb94eb8d7f37724466d349d89b3
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-02 15:33:54 -0400

    Create 行至边缘再返回.swift

---
+++ Charles/学习编程一/行至边缘再返回.swift
@@ -0,0 +1,17 @@
+for i in 1 ... 4 {
+  for i in 1 ... 4 {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    turnLeft()
+    
+    
+    
+}
+
+    
+    
