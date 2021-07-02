4c25d3759fac941a0af42da58270fb79fefeac75
Jqazwsxedcrfvtgb <86443690+Jqazwsxedcrfvtgb@users.noreply.github.com>
2021-07-02 14:30:21 -0400

    Create 行至边缘再返回.swift

---
+++ Jason/学习编程一/行至边缘再返回.swift
@@ -0,0 +1,23 @@
+for i in 1 ... 2 {
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    
+}
+turnLeft()
+
+for i in 1 ... 2 {
+    
+    moveForward()
+    moveForward()
+    toggleSwitch()
+    turnLeft()
+    turnLeft()
+    moveForward()
+    moveForward()
+    
+}
