365e9e2da882d6fdde455e37d63049364c4f7517
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-02 17:17:37 -0400

    Create Nesting patterns.swift

---
+++ James/Learning Code 1/Nesting patterns.swift
@@ -0,0 +1,21 @@
+func turnAround() {
+    //#-editable-code
+    turnLeft()
+    turnLeft()
+    //#-end-editable-code
+}
+
+func solveStair() {
+    //#-editable-code
+    moveForward()
+    collectGem()
+    
+    //#-end-editable-code
+}
+//#-editable-code
+solveStair()
+turnAround()
+moveForward()
+solveStair()
+turnAround()
+moveForward()
