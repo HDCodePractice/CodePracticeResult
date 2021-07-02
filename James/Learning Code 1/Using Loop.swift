98afb5cd8418e3b8aa4685425189c825a8315b0e
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-02 17:53:11 -0400

    Update Using Loop.swift

--- James/Learning Code 1/Using Loop.swift
+++ James/Learning Code 1/Using Loop.swift
@@ -1,1 +1,16 @@
 
+for i in 1 ... /*#-editable-code*/5/*#-end-editable-code*/ {
+    //#-editable-code
+    moveForward()
+    moveForward()
+    collectGem()
+    moveForward()
+    
+    
+    //#-end-editable-code
+}
+
+//#-hidden-code
+playgroundEpilogue()
+//#-end-hidden-code
+
