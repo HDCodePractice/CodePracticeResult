d6127e8cf42fd34f69dee0e8b278dd997ca625aa
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-02 16:00:59 -0400

    Create 决策树.swift

---
+++ Charles/学习编程一/决策树.swift
@@ -0,0 +1,60 @@
+func toggleorgem() {
+    moveForward()
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }
+    if isOnGem {
+        collectGem()
+        
+    }
+    turnRight()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnLeft()
+    moveForward()
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }
+    if isOnGem {
+        collectGem()
+        
+    }
+    turnLeft()
+    turnLeft()
+    moveForward()
+    turnRight()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnRight()
+    moveForward()
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }
+    if isOnGem {
+        collectGem()
+        
+    }
+    turnLeft()
+    moveForward()
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }
+    if isOnGem {
+        collectGem()
+        
+    }
+    turnLeft()
+    turnLeft()
+    moveForward()
+    turnLeft()
+    
+}
+toggleorgem()
+moveForward()
+toggleorgem()
