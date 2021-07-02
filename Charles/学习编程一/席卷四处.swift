e0451ef975608208932c9b8d7e63e88d462f2177
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-02 15:46:57 -0400

    Create 席卷四处.swift

---
+++ Charles/学习编程一/席卷四处.swift
@@ -0,0 +1,29 @@
+func gemJump() {
+    moveForward()
+    collectGem()
+    
+
+for i in 1 ... 2 {
+    turnLeft()
+    moveForward()
+    collectGem()
+    turnRight()
+    turnRight()
+    moveForward()
+    
+    
+    
+}
+    turnLeft()
+    moveForward()
+    collectGem()
+    moveForward()
+    
+
+
+
+}
+for i in 1 ... 4 {
+    gemJump()
+    
+}
