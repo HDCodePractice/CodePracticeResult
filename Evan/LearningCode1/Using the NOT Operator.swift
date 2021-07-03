96552ef9625ac1b9a4250cb2a3b6e68b4de63cdc
evanfang44 <75408438+evanfang44@users.noreply.github.com>
2021-07-02 20:31:17 -0500

    Create Using the NOT Operator.swift

---
+++ Evan/LearningCode1/Using the NOT Operator.swift
@@ -0,0 +1,21 @@
+moveForward()
+
+for i in 1 ... 4 {
+    
+    if isOnGem {
+        collectGem()
+    }
+    else if !isOnGem {
+        turnLeft()
+        moveForward()
+        moveForward()
+        collectGem()
+        turnRight()
+        turnRight()
+        moveForward()
+        moveForward()
+        turnLeft()
+    }
+    moveForward()
+    
+}
