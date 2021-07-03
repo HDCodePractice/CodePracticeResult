6e256b8c94f0e317dbc1e4896aa623186d180d20
evanfang44 <75408438+evanfang44@users.noreply.github.com>
2021-07-02 20:16:13 -0500

    Create Defining Smarter Functions.swift

---
+++ Evan/LearningCode1/Defining Smarter Functions.swift
@@ -0,0 +1,27 @@
+func collectOrToggle() {
+  if isOnGem {
+        collectGem()
+    }
+    else if isOnClosedSwitch {
+        toggleSwitch()
+    }
+    moveForward()
+}
+for i in 1 ... 5 {
+    collectOrToggle()
+}
+collectGem()
+turnLeft()
+moveForward()
+moveForward()
+turnLeft()
+
+for i in 1 ... 5 {
+    collectOrToggle()
+}
+turnRight()
+moveForward()
+turnRight()
+for i in 1 ... 5 {
+    collectOrToggle()
+}
