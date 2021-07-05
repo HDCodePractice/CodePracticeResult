3b15f9b1993a7ee67c7bd3d268beac1d3f25a454
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-05 19:11:23 -0400

    Create Boxed in.swift

---
+++ James/Learning Code 1/Boxed in.swift
@@ -0,0 +1,22 @@
+for i in 1 ... 10 {
+    moveForward()
+    
+    if isOnOpenSwitch {
+        
+        moveForward()
+        
+    };turnLeft()
+    
+    if isOnClosedSwitch {
+        toggleSwitch()
+        moveForward()
+        
+    }
+    if isOnGem {
+        collectGem()
+        moveForward()
+        
+    }
+    
+    
+}
