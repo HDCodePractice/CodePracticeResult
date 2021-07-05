eb41c49808f5098032c6c9734841f0e1b2622535
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-05 19:18:58 -0400

    Create Checking This OR That.swift

---
+++ James/Learning Code 1/Checking This OR That.swift
@@ -0,0 +1,11 @@
+for i in 1 ... 12 {
+    if isBlockedLeft || isBlocked {
+        turnRight()
+        moveForward()
+        collectGem()
+    } else {
+        moveForward()
+        collectGem()
+    }
+    
+}
