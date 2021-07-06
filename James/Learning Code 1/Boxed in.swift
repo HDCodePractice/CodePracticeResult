a0516ba459f53f040837983aecfd067f42e24814
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-06 17:33:03 -0400

    Update Boxed in.swift

--- James/Learning Code 1/Boxed in.swift
+++ James/Learning Code 1/Boxed in.swift
@@ -1,22 +1,14 @@
 for i in 1 ... 10 {
     moveForward()
-    
     if isOnOpenSwitch {
-        
         moveForward()
-        
     };turnLeft()
-    
     if isOnClosedSwitch {
         toggleSwitch()
         moveForward()
-        
     }
     if isOnGem {
         collectGem()
-        moveForward()
-        
-    }
-    
-    
+        moveForward() 
+    }   
 }
