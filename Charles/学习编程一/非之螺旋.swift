4c89297a5711bf6a501a260bf2c60725d0343459
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-05 20:45:46 -0400

    Create 非之螺旋.swift

---
+++ Charles/学习编程一/非之螺旋.swift
@@ -0,0 +1,14 @@
+for i in 1 ... 13 {
+    if !isBlocked {
+        moveForward()
+        
+    }else if isBlocked {
+        turnLeft()
+        moveForward()
+        
+    }
+    
+    
+    
+}
+toggleSwitch()
