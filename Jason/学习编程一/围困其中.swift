698ea68427fb7c3d4f219ec23b09b28691a6da32
老房东 <huangdong@gmail.com>
2021-07-06 17:33:27 -0400

    Update 围困其中.swift

--- Jason/学习编程一/围困其中.swift
+++ Jason/学习编程一/围困其中.swift
@@ -1,24 +1,18 @@
 moveForward()
-
 func cort() {
     if isOnClosedSwitch {
         toggleSwitch()
         
     } else if isOnGem {
         collectGem()
-        
     }
-    
-    
-};for i in 1 ... 4 {
+}
+for i in 1 ... 4 {
     moveForward()
     cort()
     turnLeft()
     moveForward()
     cort()
-    
-    
 }
 moveForward()
-
 cort()
