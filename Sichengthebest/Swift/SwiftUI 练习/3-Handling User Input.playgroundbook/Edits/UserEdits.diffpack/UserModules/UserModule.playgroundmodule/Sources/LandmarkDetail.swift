b9e5f31bbd555dcd28dcf4d1f31b18fadafc6e33
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-07-16 14:35:11 -0400

    Add Handling user input

---
+++ Sichengthebest/Swift/SwiftUI 练习/3-Handling User Input.playgroundbook/Edits/UserEdits.diffpack/UserModules/UserModule.playgroundmodule/Sources/LandmarkDetail.swift
@@ -0,0 +1,52 @@
+
+import SwiftUI
+
+struct LandmarkDetail: View {
+    @EnvironmentObject var modelData: ModelData
+    var landmark: Landmark
+    var landmarkIndex: Int {
+        modelData.landmarks.firstIndex(where: { $0.id == landmark.id })!
+    }
+    var body: some View {
+        VStack {
+            ScrollView {
+                MapView(coordinate: landmark.locationCoordinate)
+                    .ignoresSafeArea(edges: .top)
+                    .frame(height: 300)
+                
+                CircleImage(image: landmark.image)
+                    .offset(y: -130)
+                    .padding(.bottom, -130)
+                
+                VStack(alignment: .leading) {
+                    HStack {
+                        Text(landmark.name)
+                            .font(.title)
+                            .foregroundColor(.primary)
+                        FavoriteButton(isSet: $modelData.landmarks[landmarkIndex].isFavorite)
+                    }
+                    HStack {
+                        Text(landmark.park)
+                        Spacer()
+                        Text(landmark.state)
+                    }
+                    .font(.subheadline)
+                    .foregroundColor(.secondary)
+                    
+                    Divider()
+                    
+                    Text("About \(landmark.name)")
+                        .font(.title2)
+                    Text(landmark.description)
+                }
+                .padding()
+            }
+        }
+    }
+}
+
+struct LandmarkDetail_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkDetail(landmark: landmarks[1])
+    }
+}
