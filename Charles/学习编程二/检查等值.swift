c050c83cd5d17b2ada190fadfb759c42312df154
Charleszhang2021 <86215181+Charleszhang2021@users.noreply.github.com>
2021-07-20 17:08:47 -0400

    Update 检查等值.swift

--- Charles/学习编程二/检查等值.swift
+++ Charles/学习编程二/检查等值.swift
@@ -1,1 +1,14 @@
+let switchCounter = numberOfSwitches
+var gemCounter = 0
+while gemCounter<switchCounter {
+    if isBlocked {
+        turnRight()
+    } else{moveForward()
+    }
+    if isOnGem {
+        collectGem()
+        gemCounter=gemCounter+1
+    }
+} 
+
 
