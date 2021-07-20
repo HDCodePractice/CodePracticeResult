e637c957636767dd646fae46e684f180e47c84ba
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-20 17:04:35 -0400

    Create Checking for equal values.swift

---
+++ James/Learning Code 2/Checking for equal values.swift
@@ -0,0 +1,13 @@
+let switchCounter = numberOfSwitches
+var gemCounter = 0
+if gemCounter == switchCounter {
+    moveForward()
+}
+    while gemCounter != switchCounter && {
+        collectGem()
+        moveForward()
+        if isBlocked {
+            turnRight()
+        }
+    }
+    moveForward()
