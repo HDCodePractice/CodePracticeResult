d194cb8ec811c5f4f08cc294133914aac5b2ffff
hendryshao <87082007+hendryshao@users.noreply.github.com>
2021-07-20 12:12:52 -0400

    Create Bump up the value.swift

---
+++ Hendry/学习编程1/Bump up the value.swift
@@ -0,0 +1,16 @@
+var gemCounter = 0
+moveForward()
+collectGem()
+gemCounter = 1
+moveForward()
+collectGem()
+gemCounter = 2
+moveForward()
+collectGem()
+gemCounter = 3
+moveForward()
+collectGem()
+gemCounter = 4
+moveForward()
+collectGem()
+gemCounter = 5
