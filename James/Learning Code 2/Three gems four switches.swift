47c9ca9ebe780f0c6bc8cf36f05a6cd38e1529c3
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-22 17:13:49 -0400

    Update Three gems four switches.swift

--- James/Learning Code 2/Three gems four switches.swift
+++ James/Learning Code 2/Three gems four switches.swift
@@ -1,15 +1,19 @@
 var gemCounter = 0 
 var switchCounter = 0
-for i in 1 ... 8 {
-    while gemCounter < 3 || switchCounter < 4{
+var totalswitch = 4
+var totalgem = 3
+    while switchCounter < totalswitch{
+        if isBlocked && isBlockedLeft {
+            turnRight()
+        } else if isBlocked && isBlockedRight {
+            turnLeft()
+        }
         moveForward()
-        if gemCounter < 3 && isOnGem {
+        if gemCounter < totalgem && isOnGem {
             collectGem()
+            gemCounter = gemCounter  + 1
+        } else if switchCounter < totalswitch && isOnClosedSwitch{
+            toggleSwitch()
+            switchCounter = switchCounter + 1
         }
     }
-    if isBlocked && isBlockedLeft {
-        turnRight()
-    } else if isBlocked && isBlockedRight {
-        turnLeft()
-    }
-}
