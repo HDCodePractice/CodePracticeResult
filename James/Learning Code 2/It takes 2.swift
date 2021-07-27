9d365a614e70c01159ea4c86fa63ee41b425c1a1
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-27 13:39:18 -0400

    Create It takes 2.swift

---
+++ James/Learning Code 2/It takes 2.swift
@@ -0,0 +1,30 @@
+let expert = Expert()
+let charecter = Charecter()
+expert.turnLeft()
+func speedE() {
+    for i in 1 ... 3 {
+        expert.moveForward()
+    }
+}
+speedE()
+expert.turnRight()
+expert.moveForward()
+expert.moveForward()
+expert.turnLeft()
+expert.turnLockDown()
+expert.turnLockDown()
+expert.turnLeft()
+speedE()
+expert.turnLeft()
+speedE()
+speedE()
+expert.turnLeft()
+expert.moveForward()
+expert.moveForward()
+expert.turnRight()
+expert.turnLockUp()
+for i in 1 ... 4 {
+    character.moveForward()
+    character.toggleSwitch()
+    character.collectGem()
+}
