27b8a737c8da4b6620263357e6cedfb5ea735176
James6942 <86499178+James6942@users.noreply.github.com>
2021-07-30 12:55:17 -0400

    Create Moving further forward.swift

---
+++ James/Learning Code 2/Moving further forward.swift
@@ -0,0 +1,21 @@
+let expert = Expert()
+func move(distance: int) {
+for i in 1 ... distance {
+  expert.moveforward
+}
+  move(distance: 6)
+expert.turnRight()
+move(distance: 2)
+expert.turnRight()
+move(distance: 5)
+expert.turnLeft()
+move(distance: 5)
+expert.turnLeft()
+expert.turnLockUp()
+expert.turnLeft()
+move(distance: 3)
+expert.turnRight()
+move(distance: 3)
+expert.turnRight()
+move(distance: 4)
+expert.collectGem()
