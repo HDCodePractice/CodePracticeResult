6aaafd278d88e56420faa7080ca3c10a3bbd6e0d
Jason <86443690+Jqazwsxedcrfvtgb@users.noreply.github.com>
2021-08-03 17:02:11 -0400

    Create 放置在特点位置.swift

---
+++ Jason/学习编程二/放置在特点位置.swift
@@ -0,0 +1,7 @@
+let character = Character()
+world.place(character, atColumn: 6, row: 1)
+character.collectGem()
+world.place(character, atColumn: 1, row: 1)
+character.collectGem()
+world.place(character, atColumn: 1, row: 6)
+character.collectGem()
