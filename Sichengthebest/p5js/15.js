4a17f3771ac0308365cad54bcb24f42e05c465da
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-08-27 20:13:59 -0400

    Update 15.js

--- Sichengthebest/p5js/15.js
+++ Sichengthebest/p5js/15.js
@@ -1,13 +1,15 @@
 
-const cellSize = 20;
-const gridSize = 25;
+const cellSize = 25;
+const gridSize = 20;
 const scoreHeight = 100;
 
-let score = 0;
+let score;
 let snake = [];
 let grid = [];
 let direction = 0; // 0 = right, 1 = left, 2 = down, 3 = up
 let apple = 0;
+let gameOver = false;
+let completed = false;
 
 function setup() {
     createCanvas(cellSize * gridSize + 2, cellSize * gridSize + 2 + scoreHeight);
@@ -19,9 +21,13 @@ function newGame() {
     grid = new Array(gridSize * gridSize).fill('n');
     snake = [int(gridSize/2)*gridSize+3,int(gridSize/2)*gridSize+2,int(gridSize/2)*gridSize+1];
     apple = (int(gridSize/2) * gridSize + int(gridSize*3/4));
+    score = 0;
+    gameOver = false;
+    completed = false;
 }
 
 function drawScore() {
+    background(220)
     fill(0, 220, 0);
     textSize(24);
     text(`Score: ${score}`,gridSize*cellSize / 2-50,scoreHeight / 2);
@@ -54,12 +60,47 @@ function drawGrid() {
 }
 
 function draw() {
-    drawScore();
-    drawGrid();
+    if (gameOver === false) {
+        drawScore();
+        drawGrid();
+        for (let index = 0; index < snake.length; index++) {
+            snake[index] += 1; 
+            if (snake[index]%gridSize >= gridSize) {
+                gameOver = true;
+            }
+        }
+        if (snake.includes(apple)) {
+            score += 1/snake.length;
+        }
+    } else {
+        drawGameOver()
+    }
 }
 
 function drawApple(row, col) {
     fill(0, 102, 153);
     textSize(20);
     text("🍎",col*cellSize,scoreHeight+(row+1)*cellSize-1);
+}
+
+function drawGameOver() {
+    fill(255,0,0)
+    textSize(int(cellSize*gridSize/10));
+    text('GAME OVER\nClick [Enter] to restart',width/2,height/2)
+}
+
+function keyPressed() {
+    if (key === 'Enter') {
+        newGame();
+        setup();
+    }
+    if (keycode === LEFT_ARROW) {
+        direction = 1
+    } else if (keycode === RIGHT_ARROW) {
+        direction = 0
+    } else if (keycode === UP_ARROW) {
+        direction = 3
+    } else if (keycode === DOWN_ARROW) {
+        direction = 2
+    }
 }
