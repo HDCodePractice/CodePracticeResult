95e552868c1f6af3365a3e434ede5bb659e10315
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-08-28 07:07:44 -0700

    Update snake.js

--- Jaden/p5js/snake.js
+++ Jaden/p5js/snake.js
@@ -7,6 +7,45 @@ let snake = [];
 let direction = "r";  // l, r, u, d
 let score;
 let apple = 0;
+let snakeCol = []
+let speed = 7
+let gameOver = false
+let edges = [0
+    ,10
+    ,20
+    ,30
+    ,40
+    ,50
+    ,60
+    ,70
+    ,80
+    ,90
+    ,91
+    ,92
+    ,93
+    ,94
+    ,95
+    ,96
+    ,97
+    ,98
+    ,99
+    ,89
+    ,79
+    ,69
+    ,59
+    ,49
+    ,39
+    ,29
+    ,19
+    ,9
+    ,8
+    ,6
+    ,6
+    ,5
+    ,4
+    ,3
+    ,2
+    ,1]
 
 function colRowToIndex(col, row) {
   return row * gridSize + col;
@@ -26,10 +65,22 @@ function drawSquare(row, col) {
     square(col*cellSize+1+cellSize/5,row*cellSize+1+scoreHeight+cellSize/5, cellSize*3/5)
 }
 
+function keyPressed() {
+    if(key === "ArrowLeft" && direction !== "r"){
+        direction = "l"
+    }else if(key === "ArrowUp" && direction !== "d"){
+        direction = "u"
+    }else if(key === "ArrowDown" && direction !== "u"){
+        direction = "d"
+    }else if(key === "ArrowRight" && direction !== "l"){
+        direction = "r"
+    }
+}
+
 function drawApple(row, col) {
     fill(0, 102, 153);
     textSize(28)
-    text("🍎",col*cellSize+10,scoreHeight+(row+1)*cellSize-10);
+    text("🍎",col*cellSize+2,scoreHeight+(row+1)*cellSize-4);
 }
 
 function newGame(){
@@ -45,7 +96,37 @@ function newGame(){
 function setup() {
     createCanvas(cellSize * gridSize + 2, cellSize * gridSize + 2 + scoreHeight);
     newGame();
-    print(snake);
+    frameRate(speed)
+}
+
+function updateSnake(){
+    if (!gameOver){
+        if (direction === "r"){
+            snake.splice(snake.length-1, 1)
+            snake.splice(0,0,snake[0]+1)
+        }else if (direction === "u"){
+            snake.splice(snake.length-1, 1)
+            snake.splice(0,0,snake[0]-gridSize)
+        }else if (direction === "d"){
+            snake.splice(snake.length-1, 1)
+            snake.splice(0,0,snake[0]+gridSize)
+        }else if (direction === "l"){
+            snake.splice(snake.length-1, 1)
+            snake.splice(0,0,snake[0]-1)
+        }
+        if (snake.some(item => edges.includes(item))){
+            print("die")
+            gameOver = true
+        }
+    }
+}
+
+function mousePressed() {
+    mousecol = Math.ceil(mouseX/cellSize)-1;
+    mouserow = Math.ceil((mouseY-scoreHeight)/cellSize)-1;
+
+    clickindex = ((mouserow)*gridSize)+mousecol
+    print(clickindex)
 }
 
 function draw() {
@@ -66,5 +147,5 @@ function draw() {
             }
         }
     }
-
+    updateSnake();
 }
