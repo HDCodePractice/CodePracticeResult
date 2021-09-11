7d524fba8b7e6dc71bbc932597ca8fe9fe2ee860
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2021-09-11 10:08:33 -0400

    Homework (failed snake speed and apple count)

--- Claire/p5js/object.js
+++ Claire/p5js/object.js
@@ -1,46 +1,124 @@
-let vs = [];
 
-class Word{
-    constructor(w){
-        this.w = w;
-        this.reset();
-    }
-    reset(){
-        this.x = random(50,width-50);
-        this.y = -50;
-        this.color = randomColor();
-    }
-    display(){
-        fill(this.color)
-        text(this.w,this.x,this.y);
-    }
-    randomColor(){
-        this.color = random(["rgb(255,0,0","rgb(0,255,0)","rgb(0,0,255)"])
-    }
-}
+// Tic Tac Toe AI with Minimax Algorithm
+// The Coding Train / Daniel Shiffman
+// https://thecodingtrain.com/CodingChallenges/154-tic-tac-toe-minimax.html
+// https://youtu.be/I64-UTORVfU
+// https://editor.p5js.org/codingtrain/sketches/0zyUhZdJD
 
-function setup() {
-    createCanvas(windowWidth,windowHeight-60);
-    background(220);
-    for (let index = 0; index < 5; index++) {
-        word = new Word("hello",random(50,width-50),100,"rgb(255,20,0)");
-        vs.push(word);
+let board = [
+    ['', '', ''],
+    ['', '', ''],
+    ['', '', '']
+  ];
+  
+  let w; // = width / 3;
+  let h; // = height / 3;
+  
+  let ai = 'X';
+  let human = 'O';
+  let currentPlayer = human;
+  
+  function setup() {
+    createCanvas(400, 400);
+    w = width / 3;
+    h = height / 3;
+    bestMove();
+  }
+  
+  function equals3(a, b, c) {
+    return a == b && b == c && a != '';
+  }
+  
+  function checkWinner() {
+    let winner = null;
+  
+    // horizontal
+    for (let i = 0; i < 3; i++) {
+      if (equals3(board[i][0], board[i][1], board[i][2])) {
+        winner = board[i][0];
+      }
     }
-
-}
-
-function windowResized() {
-    setup();
-}
-
-function draw(){
-    background(220);
-    for (let index = 0; index < vs.length; index++) {
-        vs[index].display();
-        if (vs[index].y>height){
-            vs[index].reset()
-        }else{
-            vs[index].y += 5;
+  
+    // Vertical
+    for (let i = 0; i < 3; i++) {
+      if (equals3(board[0][i], board[1][i], board[2][i])) {
+        winner = board[0][i];
+      }
+    }
+  
+    // Diagonal
+    if (equals3(board[0][0], board[1][1], board[2][2])) {
+      winner = board[0][0];
+    }
+    if (equals3(board[2][0], board[1][1], board[0][2])) {
+      winner = board[2][0];
+    }
+  
+    let openSpots = 0;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (board[i][j] == '') {
+          openSpots++;
         }
+      }
     }
-}
+  
+    if (winner == null && openSpots == 0) {
+      return 'tie';
+    } else {
+      return winner;
+    }
+  }
+  
+  function mousePressed() {
+    if (currentPlayer == human) {
+      // Human make turn
+      let i = floor(mouseX / w);
+      let j = floor(mouseY / h);
+      // If valid turn
+      if (board[i][j] == '') {
+        board[i][j] = human;
+        currentPlayer = ai;
+        bestMove();
+      }
+    }
+  }
+  
+  function draw() {
+    background(255);
+    strokeWeight(4);
+  
+    line(w, 0, w, height);
+    line(w * 2, 0, w * 2, height);
+    line(0, h, width, h);
+    line(0, h * 2, width, h * 2);
+  
+    for (let j = 0; j < 3; j++) {
+      for (let i = 0; i < 3; i++) {
+        let x = w * i + w / 2;
+        let y = h * j + h / 2;
+        let spot = board[i][j];
+        textSize(32);
+        let r = w / 4;
+        if (spot == human) {
+          noFill();
+          ellipse(x, y, r * 2);
+        } else if (spot == ai) {
+          line(x - r, y - r, x + r, y + r);
+          line(x + r, y - r, x - r, y + r);
+        }
+      }
+    }
+  
+    let result = checkWinner();
+    if (result != null) {
+      noLoop();
+      let resultP = createP('');
+      resultP.style('font-size', '32pt');
+      if (result == 'tie') {
+        resultP.html('Tie!');
+      } else {
+        resultP.html(`${result} wins!`);
+      }
+    }
+  }
