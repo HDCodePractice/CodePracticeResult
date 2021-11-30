984dd350c4c1ba5717e901b34e762059b533cc1b
silvestar32 <82227329+silvestar32@users.noreply.github.com>
2021-11-29 17:21:18 -0800

    Finish Nesting Patterns

---
+++ Grace/Learning Code 1/Nesting Patterns.swift
@@ -0,0 +1,15 @@
+func turnAround() {
+  turnLeft()
+  turnLeft()
+}
+func solveStair() {
+  moveForward()
+    collectGem()
+    turnAround()
+    moveForward()
+    turnLeft()
+}
+solveStair()
+solveStair()
+solveStair()
+solveStair()
