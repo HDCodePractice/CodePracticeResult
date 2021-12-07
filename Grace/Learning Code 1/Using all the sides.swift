0bcf30b5f0c73dcb73db5def4b26937bafcb344a
silvestar32 <82227329+silvestar32@users.noreply.github.com>
2021-12-06 17:32:17 -0800

    Finish Using all the sides

---
+++ Grace/Learning Code 1/Using all the sides.swift
@@ -0,0 +1,13 @@
+func gemCollect() {
+    moveForward()
+    collectGem()
+    moveForward()
+    moveForward()
+    moveForward()
+    turnRight()
+}
+gemCollect()
+gemCollect()
+gemCollect()
+moveForward()
+collectGem()
