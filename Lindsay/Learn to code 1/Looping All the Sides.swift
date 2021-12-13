3c9adb5711fcf270aae24a89a9abe5962e5bbf83
lzhou9 <95196831+lzhou9@users.noreply.github.com>
2021-12-12 16:05:30 -0800

    Create Looping All the Sides.swift

---
+++ Lindsay/Learn to code 1/Looping All the Sides.swift
@@ -0,0 +1,17 @@
+for i in 1 ... 4 {
+
+moveForward()
+collectGem()
+moveForward()
+moveForward()
+moveForward()
+turnRight()
+
+}
+
+moveForward()
+collectGem()
+moveForward()
+moveForward()
+moveForward()
+turnRight()
