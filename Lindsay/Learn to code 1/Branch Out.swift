353dde8a3ecf2bd3768857753fe5e4e326fe7e01
lzhou9 <95196831+lzhou9@users.noreply.github.com>
2021-12-19 15:59:55 -0800

    Create Branch Out.swift

---
+++ Lindsay/Learn to code 1/Branch Out.swift
@@ -0,0 +1,15 @@
+for in 1 ... 3 {
+moveForward()
+moveForward()
+turnRight()
+for i in 1 ... 7 {
+moveFoward()
+}
+toggleSwitch()
+turnLeft()
+turnLeft()
+  for i in 1 ... 7 {
+moveFoward()
+}
+turnRight()
+}
