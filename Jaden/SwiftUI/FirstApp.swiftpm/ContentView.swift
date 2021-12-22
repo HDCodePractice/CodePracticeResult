37881eeedd8b68eba08d8e9fa11b20da4d1b38a1
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-12-22 12:31:56 -0800

    Update ContentView.swift

--- Jaden/SwiftUI/FirstApp.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/FirstApp.swiftpm/ContentView.swift
@@ -6,7 +6,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundColor(.accentColor)
-            Text("Hello, world! haghahahshdfh")
+            Text("Hello, world!")
         }
     }
 }
