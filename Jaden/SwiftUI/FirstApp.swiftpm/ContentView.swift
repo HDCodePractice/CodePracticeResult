f1790f71fb677aad94defb0050d939a1ccbd700f
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-12-22 15:56:52 -0800

    work

--- Jaden/SwiftUI/FirstApp.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/FirstApp.swiftpm/ContentView.swift
@@ -1,12 +1,25 @@
 import SwiftUI
 
 struct ContentView: View {
+    
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
-        }
+            Text("Student 001")
+            Image("Diluc")
+            Text("Name: Diluc")
+            Text("School: Synapse")
+            Text("Code: idk what to put here")
+//            Text("Student 002")
+//            Image("Kazu")
+//            Text("Name: Kazuha")
+//            Text("School: Synapse")
+//            Text("Code: idk what to put here")
+//            Text("Student 003")
+//            Image("Xiao")
+//            Text("Name: Xiao")
+//            Text("School: Synapse")
+//            Text("Code: idk what to put here")
+
+       }
     }
 }
