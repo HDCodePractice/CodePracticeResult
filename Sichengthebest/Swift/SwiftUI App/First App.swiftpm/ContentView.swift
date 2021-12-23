d21da45886d083c23b3ba95a3537b325548f39c3
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2021-12-22 19:02:56 -0500

    ok

---
+++ Sichengthebest/Swift/SwiftUI App/First App.swiftpm/ContentView.swift
@@ -0,0 +1,14 @@
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image("profilepic-xmas")
+                .resizable()
+                .scaledToFit()
+            Text("Name: Sichengthebest")
+            Text("School: TMS")
+            Text("ID Code: 234 687 216")
+        }
+    }
+}
