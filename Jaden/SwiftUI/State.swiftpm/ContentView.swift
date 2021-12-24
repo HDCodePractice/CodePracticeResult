644ccc92b8314911aa381765d157246d4e878174
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-12-24 13:23:19 -0800

    huh

--- Jaden/SwiftUI/State.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/State.swiftpm/ContentView.swift
@@ -2,16 +2,77 @@ import SwiftUI
 
 struct ContentView: View {
     
-    @State var tapCount = 100
+    @State var image = ""
+    @State var id = ""
+    @State var code = ""
+    @State var school = ""
     @State var name = ""
     
     var body: some View {
         Form{
             Section{
-                Button("Student 001"){
-                    Text("Student 001")
-                    Image("circ")
+                HStack{
+                    Spacer()
+                    Button("Student 001"){
+                        id = " 001"
+                        image = "circ"
+                        school = " Squid Games"
+                        code = " 67"
+                    }
+                    Spacer()
                 }
+                HStack{
+                    Spacer()
+                    Button("Student 002"){
+                        id = " 002"
+                        image = "tri"
+                        school = " Squid Games"
+                        code = " 456"
+                    }
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Button("Student 003"){
+                        id = " 003"
+                        image = "squa"
+                        school = " Squid Games"
+                        code = " 218"
+                    }
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("Student\(id)")
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Image(image)
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("Name: \(name)")
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    TextField("Enter name:", text: $name)
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("School:\(school)")
+                    Spacer()
+                }
+                HStack{
+                    Spacer()
+                    Text("Code:\(code)")
+                    Spacer()
+                }
+
+
             }header: {
                 HStack{
                     Spacer()
@@ -29,31 +90,7 @@ struct ContentView: View {
                         
                 }.font(.title)
             }
-            
-            Section{
-                Text("Count: \(tapCount)")
-                HStack{
-                    Spacer()
-                    Button("plus one"){
-                        tapCount += 1
-                    }.buttonStyle(.borderedProminent)
-                    Spacer()
-                    Button("plus two"){
-                        tapCount += 2
-                        tapCount += 5
-                    }.buttonStyle(.bordered)
-                    Spacer()
-                }
-               
-            }header: {
-                Text("Parker")
-                    .font(.headline)
-            }
-            
-            Section{
-                Text("name:\(name)")
-                TextField("Enter name:", text: $name)
-            }
+
 
         }
     }
