5c1129ffe4e67b447da5bf9fa88238844124705e
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-12-26 15:50:28 -0800

    Create Random Gems Everywhere.swift

---
+++ Jaden/learn code 2/Random Gems Everywhere.swift
@@ -0,0 +1,14 @@
+let totalGems = randomNumberOfGems
+var gem = 0 
+while gem<totalGems {
+    moveForward()
+    if isOnGem{
+        collectGem()
+        gem+=1
+    }
+    if isBlocked{
+        turnLeft()
+        turnLeft()
+        
+    }
+}
