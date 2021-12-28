dab5435ae57bd491cafabc68966d7de2b0d2f5a3
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-12-28 09:09:45 -0800

    Update ContentView.swift

--- Jaden/SwiftUI/Loop and Picker.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/Loop and Picker.swiftpm/ContentView.swift
@@ -1,89 +1,138 @@
 import SwiftUI
 struct ContentView: View {
-    let actors = ["rock","bean","carrey"]
-    @State var choiceActor = "rock"
     @State var memberNumber = 1
     let unitPrices = [200,300,400]
     @State var unitPrice = 300
     @State var tax: Double = 0.15
-    @FocusState var taxFocused: Bool
+    @FocusState var unitFocused: Bool
+    @State var lb:Double = 1
+    @State var kg:Double = 0.453592
+    var lbkg:Double{
+        return Double(lb)/2.205
+    }
+    var kglb:Double{
+        return Double(kg)*2.205
+    }
+    @State var c:Double = 0
+    @State var f:Double = 32
+    var cf:Double{
+        return Double(c)*9/5+32
+    }
+    var fc:Double{
+        return Double((f)-32)*5/9
+    }
     var totalPrice:Double{
         return Double(unitPrice)*Double(memberNumber)*(1+tax)
     }
     var body: some View {
-        NavigationView{
+//        NavigationView{
             Form{
                 Section{
-                    ForEach(0..<2){
-                        Text("Hello, world!\($0)")
-                    }
-                    ForEach(2..<4){i in
-                        Text("Hello, world!\(i)")
-                    }
-                }header: {
-                    Text("Demo")
-                }
-                Section{
                     HStack{
-                        Spacer()
-                        Image(choiceActor)
-                            .resizable()
-                            .scaledToFit()
-                            .frame(width: 250, height: 250)
-                        Spacer()
+                        Text("Amount of People Attending:")
+                        TextField("People Attending", value:$memberNumber, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($unitFocused)
                     }
-                    Picker("Your choice",selection:$choiceActor){
-                        ForEach(actors,id: \.self){ actor in
-                            Text(actor)
-                        }
-                    }.pickerStyle(.segmented)
-                }header:{
                     HStack{
-                        Image(systemName: "person.fill.questionmark")
-                        Text("Choose Actor")
-                    }
-                }
-                Section{
-                    Picker("Number", selection: $memberNumber){
-                        ForEach(0..<51){
-                            Text("\($0)")
-                        }
-                    }.pickerStyle(.automatic)
-                    HStack{
-                        Text("Unit Price:")
+                        Text("Ticket Price:")
                         Picker("Unit Price",selection:$unitPrice){
                             ForEach(unitPrices,id: \.self){ i in
                                 Text("\(i)")
                             }
                         }.pickerStyle(.segmented)
-
                     }
                     HStack{
-                        Text("Tax:")
+                        Text("Tax Percentage:")
                         TextField("Tax", value:$tax, format: .number)
                             .keyboardType(.decimalPad)
-                            .focused($taxFocused)
+                            .focused($unitFocused)
                     }
                     HStack{
                         Text("Total Price:")
                         Text(totalPrice,format: .currency(code: "CAD"))
                     }
-
                 }header: {
                     HStack{
+                        Spacer()
                         Image(systemName: "dollarsign.circle.fill")
-                        Text("Price")
-                    }
+                        Text("Metric Conversion")
+                        Spacer()
+                    }.font(.largeTitle)
+                    HStack{
+                        Spacer()
+                        Text("Tax")
+                        Spacer()
+                    }.font(.title)
                 }
-            }.navigationTitle("Ticket Price")
+                Section{
+                    HStack{
+                        Text("Celsius:")
+                        TextField("Celsius", value:$c, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($unitFocused)
+                    }
+                    HStack{
+                        Text("Farenheit:")
+                        TextField("Farenheit", value:$f, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($unitFocused)
+                    }
+                    HStack{
+                        Text("Celsius: \(c) to Farenheit:")
+                        Text(cf,format: .currency(code: ""))
+                    }
+                    HStack{
+                        Text("Farenheit: \(f) to Celsius:")
+                        Text(fc,format: .currency(code: ""))
+                    }
+                }header: {
+                    HStack{
+                        Spacer()
+                        Text("Temperature")
+                        Spacer()
+                    }.font(.title)
+                }
+                Section{
+                    HStack{
+                        Text("Pounds:")
+                        TextField("Pounds", value:$lb, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($unitFocused)
+                    }
+                    HStack{
+                        Text("Kilograms:")
+                        TextField("Kilograms", value:$kg, format: .number)
+                            .keyboardType(.decimalPad)
+                            .focused($unitFocused)
+                    }
+                    HStack{
+                        Text("Kilogranms: \(kg) to Pounds:")
+                        Text(kglb,format: .currency(code: ""))
+                    }
+                    HStack{
+                        Text("Pounds: \(lb) to Kilograms:")
+                        Text(lbkg,format: .currency(code: ""))
+                    }
+                }header: {
+                    HStack{
+                        Spacer()
+                        Text("Weight")
+                        Spacer()
+                    }.font(.title)
+                }
+                
+                
+                
+                
                 .toolbar(){
                     ToolbarItemGroup(placement: . keyboard){
                         Button("Done"){
-                            taxFocused = false
+                            unitFocused = false
+
                         }
                     }
                 }
         }
     }
 }
-
