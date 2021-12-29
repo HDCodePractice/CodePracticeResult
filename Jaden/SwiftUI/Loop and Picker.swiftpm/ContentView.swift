524c099fb879d77fa0e2f04b860e32f087bfffc6
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2021-12-29 08:49:40 -0800

    Update ContentView.swift

--- Jaden/SwiftUI/Loop and Picker.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/Loop and Picker.swiftpm/ContentView.swift
@@ -7,6 +7,13 @@ struct ContentView: View {
     @FocusState var unitFocused: Bool
     @State var lb:Double = 1
     @State var kg:Double = 0.453592
+    @State var o:Double = 16
+    var kgo:Double{
+        return Double(kg)*35.274
+    }
+    var lbo:Double{
+        return Double(lb)*16
+    }
     var lbkg:Double{
         return Double(lb)/2.205
     }
@@ -15,6 +22,19 @@ struct ContentView: View {
     }
     @State var c:Double = 0
     @State var f:Double = 32
+    @State var k:Double = 273.15
+    var fk:Double{
+        return Double((f)-32)*5/9+273.15
+    }
+    var kf:Double{
+        return Double((k)-273.15)*9/5+32
+    }
+    var kc:Double{
+        return Double(k) - 273.15
+    }
+    var ck:Double{
+        return Double(c) + 273.15
+    }
     var cf:Double{
         return Double(c)*9/5+32
     }
@@ -25,32 +45,41 @@ struct ContentView: View {
         return Double(unitPrice)*Double(memberNumber)*(1+tax)
     }
     var body: some View {
-//        NavigationView{
             Form{
                 Section{
                     HStack{
-                        Text("Amount of People Attending:")
-                        TextField("People Attending", value:$memberNumber, format: .number)
+                        Text("Celsius:")
+                        TextField("Celsius", value:$c, format: .number)
                             .keyboardType(.decimalPad)
                             .focused($unitFocused)
                     }
                     HStack{
-                        Text("Ticket Price:")
-                        Picker("Unit Price",selection:$unitPrice){
-                            ForEach(unitPrices,id: \.self){ i in
-                                Text("\(i)")
-                            }
-                        }.pickerStyle(.segmented)
+                        Text("From: \(c)")
                     }
                     HStack{
-                        Text("Tax Percentage:")
-                        TextField("Tax", value:$tax, format: .number)
-                            .keyboardType(.decimalPad)
-                            .focused($unitFocused)
+                        
+                        Text("How: \(c) * 9/5 + 32")
                     }
                     HStack{
-                        Text("Total Price:")
-                        Text(totalPrice,format: .currency(code: "CAD"))
+                        Text("To: \(cf)")
+                    }
+                    HStack{
+                        Text("Is: Celsius: \(c) to Farenheit:")
+                        Text(cf,format: .currency(code: ""))
+                    }
+                    HStack{
+                        Text("From: \(c)")
+                    }
+                    HStack{
+                        
+                        Text("How: \(c) + 273.15")
+                    }
+                    HStack{
+                        Text("To: \(ck)")
+                    }
+                    HStack{
+                        Text("Is: Celsius: \(c) to Kelvin:")
+                        Text(ck,format: .currency(code: ""))
                     }
                 }header: {
                     HStack{
@@ -61,17 +90,11 @@ struct ContentView: View {
                     }.font(.largeTitle)
                     HStack{
                         Spacer()
-                        Text("Tax")
+                        Text("Temperature")
                         Spacer()
-                    }.font(.title)
+                    }.font(.headline)
                 }
                 Section{
-                    HStack{
-                        Text("Celsius:")
-                        TextField("Celsius", value:$c, format: .number)
-                            .keyboardType(.decimalPad)
-                            .focused($unitFocused)
-                    }
                     HStack{
                         Text("Farenheit:")
                         TextField("Farenheit", value:$f, format: .number)
@@ -79,19 +102,38 @@ struct ContentView: View {
                             .focused($unitFocused)
                     }
                     HStack{
-                        Text("Celsius: \(c) to Farenheit:")
-                        Text(cf,format: .currency(code: ""))
+                        Text("From: \(f)")
                     }
                     HStack{
-                        Text("Farenheit: \(f) to Celsius:")
+                        Text("How: (\(f) - 32) * 5/9")
+                    }
+                    HStack{
+                        Text("To: \(fc)")
+                    }
+                    HStack{
+                        Text("Is: Farenheit: \(f) to Celsius:")
                         Text(fc,format: .currency(code: ""))
                     }
-                }header: {
+                    HStack{
+                        Text("From: \(f)")
+                    }
+                    HStack{
+                        
+                        Text("How: \(f) * 9/5 + 32")
+                    }
+                    HStack{
+                        Text("To: \(fk)")
+                    }
+                    HStack{
+                        Text("Is: Farenheit: \(c) to Kelvin:")
+                        Text(ck,format: .currency(code: ""))
+                    }
+                }header:{
                     HStack{
                         Spacer()
-                        Text("Temperature")
+                        Text("Temperature Cont")
                         Spacer()
-                    }.font(.title)
+                    }.font(.headline)
                 }
                 Section{
                     HStack{
@@ -101,38 +143,93 @@ struct ContentView: View {
                             .focused($unitFocused)
                     }
                     HStack{
-                        Text("Kilograms:")
-                        TextField("Kilograms", value:$kg, format: .number)
-                            .keyboardType(.decimalPad)
-                            .focused($unitFocused)
+                        Text("From: \(lb)")
                     }
                     HStack{
-                        Text("Kilogranms: \(kg) to Pounds:")
-                        Text(kglb,format: .currency(code: ""))
+                        
+                        Text("How: \(lb) / 2.205")
+                    }
+                    HStack{
+                        Text("To: \(lbkg)")
                     }
                     HStack{
                         Text("Pounds: \(lb) to Kilograms:")
                         Text(lbkg,format: .currency(code: ""))
                     }
-                }header: {
                     HStack{
+                        Text("From: \(lb)")
+                    }
+                    HStack{
+                        
+                        Text("How: \(lb) * 16")
+                    }
+                    HStack{
+                        Text("To: \(lbo)")
+                    }
+                    HStack{
+                        Text("Pounds: \(lb) to Ounces:")
+                        Text(lbo,format: .currency(code: ""))
+                    }
+                }header: {
+                        HStack{
                         Spacer()
                         Text("Weight")
                         Spacer()
-                    }.font(.title)
+                    }.font(.headline)
                 }
-                
-                
-                
-                
-                .toolbar(){
-                    ToolbarItemGroup(placement: . keyboard){
-                        Button("Done"){
-                            unitFocused = false
-
+                    Section{
+                        HStack{
+                            Text("Kilograms:")
+                            TextField("Kilograms", value:$kg, format: .number)
+                                .keyboardType(.decimalPad)
+                                .focused($unitFocused)
                         }
+                        HStack{
+                            Text("From: \(kg)")
+                        }
+                        HStack{
+                            
+                            Text("How: \(kg) * 2.205")
+                        }
+                        HStack{
+                            Text("To: \(kglb)")
+                        }
+                        HStack{
+                            Text("Kilograms: \(kg) to Pounds:")
+                            Text(kglb,format: .currency(code: ""))
+                        }
+                        HStack{
+                            Text("From: \(kg)")
+                        }
+                        HStack{
+                            
+                            Text("How: \(kg) * 35.274")
+                        }
+                        HStack{
+                            Text("To: \(kgo)")
+                        }
+                        HStack{
+                            Text("Kilograms: \(kg) to Ounces:")
+                            Text(kgo,format: .currency(code: ""))
+                        }
+                        
+                    }header: {
+                        HStack{
+                            Spacer()
+                            Text("Weight Cont")
+                            Spacer()
+                        }.font(.headline)
+                    }
+                    
+                
+        }
+            .toolbar(){
+                ToolbarItemGroup(placement: . keyboard){
+                    Button("Done"){
+                        unitFocused = false
                     }
                 }
-        }
+            }
     }
 }
+
