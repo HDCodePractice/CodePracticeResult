c202089736b96c5069a24468f0b8b5ad5d1343c8
老房东 <huangdong@gmail.com>
2022-01-01 12:04:25 -0500

    加入了直接使用的键盘，但是还没有完善所有的程序

--- hd/SwiftUI/Metric Conversion.swiftpm/ContentView.swift
+++ hd/SwiftUI/Metric Conversion.swiftpm/ContentView.swift
@@ -1,6 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    let fromsExpanded = [
+        ["Kilogram","Pound","Gram","Ounce"],
+        ["Kilometre","Mile","Yard","Metre"],
+        ["Square Kilometre","Square Mile","Acre"]
+    ]
     let froms = [
         ["kg","lb","g","oz"],
         ["km","mi","yd","m"],
@@ -18,60 +24,107 @@ struct ContentView: View {
     ]
     
     var body: some View {
-        Form{
-            ForEach(0..<froms.count){
-                MetricConversionUnitView(
-                    froms: froms[$0],
-                    exchange: exchanges[$0],
-                    unitName: unitNames[$0]
-                )
+        NavigationView{
+            ZStack{
+                LinearGradient(colors: [.purple,.blue], startPoint: .top, endPoint: .bottom)
+                
+                VStack(spacing:20){
+                    ForEach(unitNames,id: \.self){ unitName in
+                        NavigationLink(){
+                            ConversionView(
+                                expanded: fromsExpanded[0],
+                                froms: froms[0],
+                                exchange: exchanges[0],
+                                unitName: unitNames[0]
+                            )
+                        }label: {
+                            ZStack{
+                                Text(unitName)
+                                    .font(.title)
+                                    .padding()
+                            }
+                            .frame(width:300)
+                            .foregroundColor(.white)
+                            .background(.gray)
+                            .cornerRadius(10)
+                            .shadow(radius: 5)
+                        }
+                    }
+                }
             }
+            .ignoresSafeArea()
+            .navigationTitle("Metric Conversion")
         }
     }
 }
 
-struct MetricConversionUnitView: View {
-    let froms : [String]
-    let exchange : [Double]
-    let unitName : String
+struct ConversionView: View {
+    let expanded: [String]
+    let froms: [String]
+    let exchange: [Double]
+    let unitName: String
     @State var fromSelect = 0
     @State var toSelect = 0
-    @State var fromUnit = 1.0
-    var toUnit: Double{
-        let from = fromUnit / exchange[fromSelect]
-        let toUnit = from * exchange[toSelect]
-        return toUnit
-    }
+    @State var andSelect = 0
+    @State var fromUnit = "1"
+    @State var andUnit = "1"
+    let inputOrder = [["1","2","3"],["4","5","6"],["7","8","9"],["C","0","."]]
     
     var body: some View {
-        Section{
-            HStack{
-                Text("From")
-                Picker("", selection: $fromSelect){
-                    ForEach(0..<froms.count){
-                        Text(froms[$0])
-                    }
-                }
-                .pickerStyle(.segmented)
-            }
-            HStack{
-                Text("How")
-                TextField("Input from unit", value: $fromUnit, format: .number)
-            }
-            HStack{
-                Text("To")
-                Picker("", selection: $toSelect){
-                    ForEach(0..<froms.count){
-                        Text(froms[$0])
-                    }
-                }
-                .pickerStyle(.segmented)
-            }
-            HStack{
-                Text("Is \(toUnit)")
-            }
-        }header: {
+        VStack(spacing:20){
             Text(unitName)
+                .font(.title)
+            HStack(){
+                Text("Input")
+                Spacer()
+                Text(fromUnit)
+            }
+            .padding()
+            Spacer()
+            VStack{
+                ForEach(inputOrder,id:\.self){ row in
+                    HStack{
+                        ForEach(row,id:\.self){ item in
+                            Button{
+                                if item == "C" {
+                                    fromUnit = "0"
+                                }else{
+                                    fromUnit += item
+                                }
+                            }label: {
+                                ButtonView(item: item)
+                            }
+                        }
+                    }
+                }
+            }
+            .padding()
         }
+    }
+}
+
+struct ConversionView_Previews: PreviewProvider {
+    static var previews: some View {
+        ConversionView(
+            expanded: ["Kilogram","Pound","Gram","Ounce"],
+            froms: ["kg","lb","g","oz"],
+            exchange: [1.0,2.20462262,1000.0,35.27],
+            unitName: "Mass")
+    }
+}
+
+
+struct ButtonView: View {
+    var item : String
+    var body: some View {
+        ZStack{
+            Color.blue
+            Text("\(item)")
+                .font(.title)
+                .foregroundColor(.white)
+        }
+        .frame(width:.infinity)
+        .cornerRadius(5)
+        .shadow(radius: 5)
     }
 }
