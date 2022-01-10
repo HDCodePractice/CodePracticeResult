f4979277ff8d9ac7972cdf8662a4a8d7b603366d
lzhou9 <95196831+lzhou9@users.noreply.github.com>
2022-01-09 17:50:09 -0800

    Create Four by Four.swift

---
+++ Lindsay/Four by Four.swift
@@ -0,0 +1,32 @@
+func maybeSwitch() {
+    if isOnClosedSwitch {
+        toggleSwitch()
+        
+    }else {
+        moveForward()
+    }
+    
+}
+    for i in 1 ... 3 {
+        moveForward()
+    }
+maybeSwitch()
+turnRight()
+for i in 1 ... 2 {
+    moveForward()
+}
+maybeSwitch()
+turnRight()
+
+
+
+for i in 1 ... 3 {
+    moveForward()
+    
+}
+
+maybeSwitch()
+turnRight()
+for i in 1 ... 3 {
+    moveForward()
+}
