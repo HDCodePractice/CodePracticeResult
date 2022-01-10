8264fba7cd763f25cfad537fb5a04394b7ac0e5c
lzhou9 <95196831+lzhou9@users.noreply.github.com>
2022-01-09 17:32:29 -0800

    Create Choosing the Correct Tool.swift

---
+++ Lindsay/Learn to code 1/Choosing the Correct Tool.swift
@@ -0,0 +1,20 @@
+func turnAndCollectGem() {
+    for i in 1 ... 2 {
+        moveForward()
+        turnLeft()
+        moveForward()
+        collectGem()
+        turnRight()
+    }
+    }
+turnAndCollectGem()
+moveForward()
+turnLeft()
+moveForward()
+collectGem()
+turnRight()
+turnAndCollectGem()
+moveForward()
+turnLeft()
+moveForward()
+collectGem()
