7d063498eda7a2a065a916f96e685a2f83498e2a
lzhou9 <95196831+lzhou9@users.noreply.github.com>
2022-01-09 16:15:29 -0800

    Create Running Code While... .swift

---
+++ Lindsay/Running Code While... .swift
@@ -0,0 +1,12 @@
+while isOnClosedSwitch {
+toggleSwitch()
+moveForward()
+moveForward()
+if isOnClosedSwitch {
+toggleSwitch()
+moveForward()
+}else if isOnOpenSwitch{
+ moveForward()
+    }
+    
+}
