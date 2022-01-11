5dfa8b9d1cda17810aa55556c07fb07d11109800
lzhou9 <95196831+lzhou9@users.noreply.github.com>
2022-01-10 17:16:40 -0800

    Update Creating Smarter While Loops.swift

--- Lindsay/Learn to code 1/Creating Smarter While Loops.swift
+++ Lindsay/Learn to code 1/Creating Smarter While Loops.swift
@@ -1,18 +1,12 @@
 moveForward()
 
 while isOnClosedSwitch  {
-    func openSwitch() {
         if isOnClosedSwitch {
             toggleSwitch()
             moveForward()
-        }else {
-            moveForward()
+    
+          
         }
         
-    }
-    
-    for i in 1 ... 10 {
-        openSwitch()
-    }    
     
 }
