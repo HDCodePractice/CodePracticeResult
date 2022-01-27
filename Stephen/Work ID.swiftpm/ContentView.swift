cf393d80a3e46942390519d57ea9b0cc90655d67
stephenzhu01 <82696093+stephenzhu01@users.noreply.github.com>
2022-01-27 23:01:05 +0800

    improve layout

--- Stephen/Work ID.swiftpm/ContentView.swift
+++ Stephen/Work ID.swiftpm/ContentView.swift
@@ -2,33 +2,44 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
+        HStack{
             Image("logo")
                 .resizable()
                 .padding(/*@START_MENU_TOKEN@*/.horizontal, 10.0/*@END_MENU_TOKEN@*/)
+                .frame(width: /*@START_MENU_TOKEN@*/80.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/220.0/*@END_MENU_TOKEN@*/)
                 .scaledToFit()
-            Image("avatar")
-                .resizable()
-                .frame(width: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/250.0/*@END_MENU_TOKEN@*/)
-                .scaledToFit()
-            Text("Name: Stephen")
-            HStack{
-                Image(systemName: "phone.circle")
+            VStack {
+                Text("S&V Information")
+                    .font(.title)
+                    .fontWeight(.bold)
+                    .multilineTextAlignment(.leading)
+                Image("avatar")
+                    .resizable()
+                    .frame(width: /*@START_MENU_TOKEN@*/170.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/250.0/*@END_MENU_TOKEN@*/)
+                    .scaledToFit()
+                HStack{
+                    Image(systemName:"person.circle")
                         .imageScale(.large)
                         .foregroundColor(.accentColor)
-                Text("35792468")
+                    Text("Stephen")
+                }
+                HStack{
+                    Image(systemName: "phone.circle")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("35792468")
+                }
+                HStack{
+                    Image(systemName: "envelope.circle")
+                        .imageScale(.large)
+                        .foregroundColor(.accentColor)
+                    Text("stephen@hdsv.com")
+                }
+                Image("code")
+                    .resizable()
+                    .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
+                    .scaledToFit()
             }
-            HStack{
-                Image(systemName: "envelope.circle")
-                    .imageScale(.large)
-                    .foregroundColor(.accentColor)
-                Text("stephen@hdsv.com")
-            }
-            Image("code")
-                .resizable()
-                .frame(width: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/, height: /*@START_MENU_TOKEN@*/100.0/*@END_MENU_TOKEN@*/)
-                .scaledToFit()
-
         }
     }
 }
