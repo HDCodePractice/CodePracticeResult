aa0b628e11c1d777cf5f45cf48566dc0d31b0821
ParkerXChen <parkerxchen@gmail.com>
2022-02-05 10:06:55 -0500

    *Tried to add difficulty levels but its really scuff*

--- Parker/Swift UI/IDK.swiftpm/ContentView.swift
+++ Parker/Swift UI/IDK.swiftpm/ContentView.swift
@@ -1,53 +1,120 @@
 import SwiftUI
-
 struct ContentView: View {
-    @State var game = Game()
-    @State var answer : [String]?
-    
+    let difficulties = ["Easy"]
     var body: some View {
-        VStack{
-            if let answer = answer{
-                HStack{
-                    VStack{
-                        Text("Click on the flag of:")
-                            .font(.title)
-                            .foregroundColor(.blue)
-                            .padding()
-                    
-                    Text(answer[0])
-                        .font(.title)
-                        .foregroundColor(.green)
-                        .padding()
-//                    Text(answer[1])
-                    }}
-                Text("")
-                ForEach(game.flags,id:\.self){ flag in
-                    Text(flag)
-                        .font(Font.system(size:60, design: .default))
-                        .onTapGesture {
-                            print("Clicked on a flag")
-                        }
-                }
-                
-            }else{
-                Text("Nothing!")
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .padding()
-            }
-            
-            Text("Start")
-                .fontWeight(.bold)
-                        .font(.title)
-                        .foregroundColor(.blue)
-                        .padding()
-                        .overlay(
-                            RoundedRectangle(cornerRadius: 20)
-                                .stroke(Color.black, lineWidth: 5)
+        NavigationView {
+            VStack {
+                ForEach(difficulties,id: \.self){ difficulty in
+                    NavigationLink() {
+                        GameView(
+                            Difficulty: difficulty
                         )
-                .onTapGesture {
-                    answer = game.newGame()
+                    } label: {
+                        ZStack {
+                            Text("Start")
+                                .font(.title)
+                                .padding()
+                        }
+                        .frame(width:300)
+                        .foregroundColor(.black)
+                        .background(.purple)
+                        .cornerRadius(10)
+                        .shadow(radius: 5)
+                    }
                 }
+            }
+            .navigationTitle("Guess the Flag")
+        }
+    }
+}
+
+struct GameView: View {
+    @State var game = Game()
+    @State var response = ""
+    @State var chosenResponse = ""
+    @State var continueButton = false
+    let Difficulty: String
+    var body: some View {
+        VStack {
+            if game.GameStart {
+                Text("Select the flag of \(game.Question)?")
+                    .font(.system(.body, design: .rounded))
+                answers
+                if continueButton == true {
+                    continueButtonDisplay
+                }
+            } else {
+                startButton
+            }
+        }
+    }
+    var answers: some View {
+        ForEach(game.Answers,id:\.self) { flag in
+            HStack {
+                Button() {
+                    if flag == game.CorrectAnswer {
+                        response = "checkmark.circle.fill"
+                    } else {
+                        response = "x.circle.fill"
+                    }
+                    continueButton = true
+                    chosenResponse = flag
+
+                } label: {
+                    Text(flag)
+                        .font(.system(size: 40))
+                }
+                if response == "x.circle.fill" && flag == chosenResponse {
+                    Image(systemName: response)
+                        .foregroundColor(.red)
+                        .font(.system(size: 20))
+                }
+                if (response == "checkmark.circle.fill" && flag == chosenResponse) || (flag == game.CorrectAnswer && response != "") {
+                    Image(systemName: "checkmark.circle.fill")
+                        .foregroundColor(.green)
+                        .font(.system(size: 20))
+                }
+            }
+        }
+    }
+
+    var continueButtonDisplay: some View {
+        VStack {
+            Button() {
+                game.Difficulty = Difficulty
+                game.newGame()
+                response = ""
+                continueButton = false
+            } label: {
+                ZStack {
+                    Color.accentColor
+                    Text("Next")
+                        .foregroundColor(.black)
+                }
+                    .frame(width: 140, height: 50)
+                    .cornerRadius(5)
+                    .shadow(radius: 5)
+            }
+        }
+    }
+
+    var startButton: some View {
+        VStack {
+            Button() {
+                game.Difficulty = Difficulty
+                game.newGame()
+                response = ""
+                continueButton = false
+            } label: {
+                ZStack {
+                    Color.accentColor
+                    Text("Start")
+                        .foregroundColor(.white)
+                }
+                    .frame(width: 140, height: 50)
+                    .cornerRadius(5)
+                    .shadow(radius: 5)
+            }
         }
     }
 }
