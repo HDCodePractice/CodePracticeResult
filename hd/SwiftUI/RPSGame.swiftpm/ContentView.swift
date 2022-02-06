87643374ad979292efa1b64d27cff0332f20b62d
bernieharvard <82739875+bernieharvard@users.noreply.github.com>
2022-02-06 12:51:19 -0500

    2022-2-6 classwork

--- hd/SwiftUI/RPSGame.swiftpm/ContentView.swift
+++ hd/SwiftUI/RPSGame.swiftpm/ContentView.swift
@@ -1,12 +1,74 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var user : String = "No Choose"
-    @State var computer : String = "No Choose"
-    @State var result : String = "Please Choose"
+    @State var game = Game()
     
-    func winOrLoose() {
-        let all = ["rock","paper","scissors"]
+    var body: some View {
+        if game.gameStarting{
+            gameStarting
+        }else{
+            gameEnding
+        }
+    }
+    var gameEnding : some View{
+        VStack{
+            Image(game.computer)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .padding()
+            Text(game.result)
+                .font(.largeTitle)
+            Image(game.user)
+                .resizable()
+                .scaledToFit()
+                .cornerRadius(30)
+                .padding()
+                .onTapGesture {
+                    game.restart()
+                }
+        }
+    }
+    var gameStarting: some View{
+        VStack {
+            Text(game.computer)
+                .font(.largeTitle)
+            Image(systemName: "desktopcomputer")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.accentColor)
+                .frame(width: 100, height: 100)
+                .padding()
+            Text(game.result)
+                .font(.largeTitle)
+            HStack{
+                ForEach(game.all,id: \.self) { i in
+                    Image(i)
+                        .resizable()
+                        .scaledToFit()
+                        .cornerRadius(30)
+                        .onTapGesture {
+                            game.winOrLoose(userInput: i)
+                        }
+                }
+                
+            }
+            .padding()
+            Text(game.user)
+                .font(.largeTitle)
+        }
+    }
+}
+
+struct Game{
+    var user : String = "No Choose"
+    var computer : String = "No Choose"
+    var result : String = "Please Choose"
+    let all = ["rock","paper","scissors"]
+    var gameStarting : Bool = true
+    
+    mutating func winOrLoose(userInput: String) {
+        user = userInput
         if let c = all.randomElement() {
             computer = c
             if computer == user {
@@ -18,57 +80,19 @@ struct ContentView: View {
                 result = "You Won"
             }
             if (user=="scissors"&&computer=="rock") ||
-                       (user=="paper"&&computer=="scissors") ||
+                (user=="paper"&&computer=="scissors") ||
                 (user=="rock"&&computer=="paper") {
                 result = "You Lost"
             }
-            
         }else{
             computer = "No Choose"
         }
+        gameStarting = false
     }
-    
-    var body: some View {
-        VStack {
-            Text(computer)
-                .font(.largeTitle)
-            Image(systemName: "desktopcomputer")
-                .resizable()
-                .scaledToFit()
-                .foregroundColor(.accentColor)
-                .frame(width: 100, height: 100)
-                .padding()
-            Text(result)
-                .font(.largeTitle)
-            HStack{
-                Image("rock")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(30)
-                    .onTapGesture {
-                        user = "rock"
-                        winOrLoose()
-                    }
-                Image("paper")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(30)
-                    .onTapGesture {
-                        user = "paper"
-                        winOrLoose()
-                    }
-                Image("scissors")
-                    .resizable()
-                    .scaledToFit()
-                    .cornerRadius(30)
-                    .onTapGesture {
-                        user = "scissors"
-                        winOrLoose()
-                    }
-            }
-            .padding()
-            Text(user)
-                .font(.largeTitle)
-        }
+    mutating func restart(){
+        gameStarting = true
+        user = "No Choose"
+        computer = "No Choose"
+        result = "Please Choose"
     }
 }
--- hd/SwiftUI/RPSGame.swiftpm/Game.swift
+++ hd/SwiftUI/RPSGame.swiftpm/Game.swift
@@ -1,42 +1,42 @@
+////
+////  File.swift
+////  Test
+////
+////  Created by 老房东 on 2022-01-26.
+////
 //
-//  File.swift
-//  Test
+//import Foundation
 //
-//  Created by 老房东 on 2022-01-26.
-//
-
-import Foundation
-
-struct Game{
-    var computer : String = "rock"
-    var player: String = "rock"
-    var won : Int = 0
-    var lost : Int = 0
-    var tied : Int = 0
-    var starting : Bool = true
-    
-    var result : String {
-        if computer == player {
-            return "Tied"
-        }
-        
-        if (player=="rock"&&computer=="scissors") ||
-            (player=="scissors"&&computer=="paper") ||
-            (player=="paper"&&computer=="rock") {
-            return "Won"
-        }
-        
-        if (player=="scissors"&&computer=="rock") ||
-            (player=="paper"&&computer=="scissors") ||
-            (player=="rock"&&computer=="paper") {
-            return "Lost"
-        }
-        return "Choose One"
-    }
-    
-    mutating func genComputer(){
-        let outcome = ["rock","scissors","paper"]
-        computer = outcome.randomElement() ?? "rock"
-    }
-    
-}
+//struct Game{
+//    var computer : String = "rock"
+//    var player: String = "rock"
+//    var won : Int = 0
+//    var lost : Int = 0
+//    var tied : Int = 0
+//    var starting : Bool = true
+//    
+//    var result : String {
+//        if computer == player {
+//            return "Tied"
+//        }
+//        
+//        if (player=="rock"&&computer=="scissors") ||
+//            (player=="scissors"&&computer=="paper") ||
+//            (player=="paper"&&computer=="rock") {
+//            return "Won"
+//        }
+//        
+//        if (player=="scissors"&&computer=="rock") ||
+//            (player=="paper"&&computer=="scissors") ||
+//            (player=="rock"&&computer=="paper") {
+//            return "Lost"
+//        }
+//        return "Choose One"
+//    }
+//    
+//    mutating func genComputer(){
+//        let outcome = ["rock","scissors","paper"]
+//        computer = outcome.randomElement() ?? "rock"
+//    }
+//    
+//}
