63068f8df87af44dc3fa55e5a3100a73608c0dea
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2022-02-12 08:22:48 -0800

    work

--- Jaden/SwiftUI/Flag Game.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/Flag Game.swiftpm/ContentView.swift
@@ -1,80 +1,170 @@
 import SwiftUI
 
 struct ContentView: View {
+    let difficulties = ["Start"]
+    var body: some View {
+        GameView(Difficulty: "Start")
+//        NavigationView {
+//            ZStack {
+//                VStack {
+//                    ForEach(difficulties,id: \.self){ difficulty in
+//                        NavigationLink() {
+//                            GameView(
+//                                Difficulty: difficulty
+//                            )
+//                        } label: {
+//                            ZStack {
+//                                Text(difficulty)
+//                                    .font(.title)
+//                                    .padding()
+//                            }
+//                            .frame(width:200)
+//                            .foregroundColor(.white)
+//                            .background(.blue)
+//                            .cornerRadius(5)
+//
+//                        }
+//                    }
+//                }
+//            }
+//            .ignoresSafeArea()
+//            .navigationTitle("Flag Game")
+//        }
+    }
+}
+
+
+struct GameView: View {
     @State var game = Game()
-    @State var answer: [String]?
-    private var gamestart: some View {
-        VStack{
-            HStack{
-                Text(game.question)
-                //                    Text(answer[1])
-            }
-            Text("")
-            ForEach(game.answers,id:\.self){flag in
-                Text(flag)
-                    .font(.largeTitle)
-                    .onTapGesture {
-                        game.toggleAnswer(tAnswer: flag)
+    @State var started = false
+    let Difficulty: String
+    var body: some View {
+        ZStack {
+            VStack{
+                if game.Results {
+                    Results
+                } else {
+                    if game.gameStart {
+                        gamestart
+                    } else {
+                        gameEnd
+                        VStack {
+                            if started == true {
+                                Button() {
+                                    game.current += 1
+                                    game.newGame()
+                                } label: {
+                                    ZStack {
+                                        Color.blue
+                                        Text("Continue")
+                                            .foregroundColor(.white)
+                                            .font(.title)
+                                    }
+                                    .frame(width: 140, height: 50)
+                                    .cornerRadius(20)
+
+                                }
+                            } else {
+                                Button() {
+                                    game.Difficulty = Difficulty
+                                    started = true
+                                    game.newGame()
+                                } label: {
+                                    ZStack{
+                                        Color.blue
+                                        Text("Start")
+                                            .foregroundColor(.white)
+                                            .font(.largeTitle)
+                                    
+                                            
+                                    }
+                                    .frame(width: 200, height: 100)
+                                    .cornerRadius(15)
+                                }
+                            }
+                        }
                     }
-                
-                
+                }
             }
         }
+        .ignoresSafeArea()
     }
-    var gameEnd: some View{
+    
+    var gamestart: some View {
         VStack{
-            HStack{
-                Text(game.question)
-            }
+            Text("\(game.current+1)/\(game.total)")
+                .font(.title)
+            Text(game.question)
+                .font(.largeTitle)
+
             Text("")
             ForEach(game.answers,id:\.self){ flag in
                 HStack{
                     Text(flag)
-                        .font(.largeTitle)
-                    if flag == game.answer{
-                        Image(systemName: flag == game.correct ?  "checkmark.circle.fill" : "x.circle.fill")
-                            .foregroundColor(flag == game.correct ? .green : .red)
-                        
-                        
-                            
-
-                    }
-                    if flag == game.correct && flag != game.answer{
-                        Image(systemName: flag == game.correct ?  "checkmark.circle.fill" : "x.circle.fill")
-                            .foregroundColor(flag == game.correct ? .green : .red)
-                    }
+                        .font(.system(size: 60))
+                        .onTapGesture{
+                            if flag == game.correct {
+                                game.score += 1
+                            }
+                            game.toggleAnswer(tAnswer: flag)
+                        }
                 }
             }
         }
     }
-    var body: some View {
-        VStack{
-            if game.gameStart{
-                gamestart
-            }else{
-                gameEnd
-            }
-            Text("Question Number: \(game.questionNumber)/10")
-            Text(game.play)
-            if game.stopper{
-                Text(game.scoreText)
+    
+    var Results: some View {
+        VStack {
+            Text("You got a score of:")
+            Text("\(game.score)/\(game.total)")
+                .font(.largeTitle)
+            Button(){
+                started = false
                 
+            } label: {
+                ZStack{
+                    Color.blue
+                    Text("Play Again")
+                        .foregroundColor(.white)
+                        .font(.largeTitle)
+                
+                        
+                }
+                .frame(width: 200, height: 100)
+                .cornerRadius(15)
+                }
             }
+            
 
-            Text(game.button)
-                .onTapGesture{
-                    game.newGame()
-                    game.tenQuestions()
-                    if game.stopper{
-                        game.tenQuestions()
-                        game.questionNumber = 10
-                        if game.play == "stop"{
-                            game.charger = 1
-                            game.score = 0
+        }
+    }
+    
+    var gameEnd: some View{
+        VStack{
+            if started == true {
+                Text("\(game.current+1)/\(game.total)")
+                    .font(.system(size: 25, design: .rounded))
+                    .foregroundColor(.white)
+                Text(game.question)
+                    .font(.system(size: 40, design: .rounded))
+                    .bold()
+                    .foregroundColor(.white)
+                Text("")
+                ForEach(game.answers,id:\.self){ flag in
+                    HStack{
+                        Text(flag)
+                            .font(.system(size: 60))
+                        if flag == game.answer{
+                            Image(systemName: flag == game.correct ? "checkmark.circle.fill" : "x.circle.fill")
+                                .foregroundColor(flag == game.correct ? .green : .red)
+                        }
+                        if flag == game.correct && flag != game.answer {
+                            Image(systemName: flag == game.correct ? "checkmark.circle.fill" : "x.circle.fill")
+                                .foregroundColor(flag == game.correct ? .green : .red)
                         }
                     }
                 }
-
+            }
         }
     }
 }
--- Jaden/SwiftUI/Flag Game.swiftpm/Game.swift
+++ Jaden/SwiftUI/Flag Game.swiftpm/Game.swift
@@ -1,116 +1,61 @@
-//
-//  Game.swift
-//  Flag Game
-//
-//  Created by Jaden Cheung on 1/29/22.
-//
-
 import Foundation
-import SwiftUI
+
 struct Game{
-    var flags = ["🇨🇦","🇺🇸","🇨🇳","🇻🇨","🇬🇧","🇷🇺","🇯🇵","🇬🇸","🇮🇹", "🇫🇷","🇩🇪","🇦🇷","🇵🇹","🇪🇸"]
-    var countries = ["Canada ","United States ","China ","St. Vincent & Grenadines ","United Kingdom ","Russia ","Japan ","South Georgia & South Sandwich Islands ","Italy ","France ","Germany ","Argentina ","Portugal ","Spain "]
+    var Difficulty: String = "Easy"
+    let EasyFlags = ["🇨🇦","🇺🇸","🇨🇳","🇬🇧","🇷🇺","🇯🇵","🇦🇷","🇦🇺","🇧🇷","🇧🇪","🇬🇷","🇮🇳","🇿🇦","🇪🇸","🇵🇹","🇫🇷"]
+    var EasyCountries = ["Canada","United States","China","United Kingdom","Russia","Japan","Argentina","Australia","Brazil","Belgium","Greece","India","South Africa","Spain","Portugal","France"]
     var answers : [String] = []
     var question : String = ""
     var gameStart : Bool = false
     var correct : String = ""
     var answer : String = ""
     var score : Int = 0
-    var questionNumber : Int = 0
-    var button : String = "Play"
-    var play : String = ""
-    var stopper : Bool = false
-    var scoreText : String = ""
-    var charger : Int = 0
     var exams : [String] = []
     var current : Int = 0
     var total : Int = 10
-    var source : [Int] = []
-    var flog : [String] = []
+    var Results: Bool = false
     
-    mutating func newGame(){
-        exams = Array(countries.shuffled().prefix(10))
-        total = exams.count
-        
-        for i in 0..<countries.count {
-            source.append(i)
-        }
-        let currentIndex = countries.firstIndex(of: exams[current]) ?? 0
-        source.remove(at: currentIndex)
-        source = Array(source.shuffled().prefix(3))
-        source.append(currentIndex)
-        source.shuffle()
-        print(source)
-
-        // source = [0,1,2,3,4,5,6,7,8,9.....countries.cont]
-        // exam[current] -> source firstIndex(of: "maxime") -> currentIndex(3)
-        // source.remove(currentIndex) [0,1,2,3,4,5,6,7,8]
-        // source.shuffled().prefix(3) - [6,2,1]
-        // source.append(currentIndex -> [6,2,1,3]
-        // source.shuffle() -> [1,3,6,2]
-        
-        
-        
-        
-        
-        let rCountries = countries[currentIndex]
-        flog.append(countries[source[0]])
-        flog.append(countries[source[1]])
-        flog.append(countries[source[2]])
-        flog.append(countries[source[3]])
-        countries.remove(at: currentIndex)
-        
-        print(rCountries)
-        questionNumber += 1
-        charger += 1
-        if charger == 0{
-            play = "start"
-        }
-        if charger >= 1{
-            play = "cont"
-        }
-        if charger == 10{
-            play = "stop"
-            stopper = true
-            
-            
-        }
-        
-        if let q = Optional(rCountries) {
-            question = String(q)
-            if let index = countries.firstIndex(of:String(q)){
-                correct = flags[currentIndex]
+    mutating func execute(Countries: [String], Flags: [String]) {
+        if current < total {
+            var xe = Countries
+            if let index = Countries.firstIndex(of: exams[current]) {
+                xe.remove(at: index)
             }
-            answers = []
-            for c in rCountries.shuffled(){
-                if let index = countries.firstIndex(of: String(c)){
-                    answers.append(flags[currentIndex])
+            
+            var rCountries = Array(xe.shuffled().prefix(3))
+            rCountries.append(exams[current])
+            if let q = rCountries.last {
+                question = q
+                if let index = Countries.firstIndex(of: q) {
+                    correct = Flags[index]
                 }
+                answers = []
+                for c in rCountries.shuffled(){
+                    if let index = Countries.firstIndex(of: c){
+                        answers.append(Flags[index])
+                    }
+                }
+                gameStart = true
             }
-            gameStart = true
+        } else {
+            Results = true
         }
-//        flog.remove(at: countries[source[0]])
-//        flog.remove(at:  countries[source[1]])
-//        flog.remove(at:  countries[source[2]])
-//        flog.remove(at:  countries[source[3]])
     }
+    
+    mutating func newGame() {
+        if current == 0 {
+            if Difficulty == "Start" {
+                exams = Array(EasyCountries.shuffled().prefix(10))
+            }
+            total = exams.count
+        }
+        if Difficulty == "Start" {
+            execute(Countries: EasyCountries, Flags: EasyFlags)
+        }
+    }
+    
     mutating func toggleAnswer(tAnswer: String){
         answer = tAnswer
         gameStart = false
-        if tAnswer == correct{
-            score += 1
-        }
-    }
-    mutating func tenQuestions(){
-        if play == "start"{
-            button = "Play"
-        }else if play == "cont"{
-            button = "Next"
-            scoreText = ""
-        }else if play == "stop"{
-            button = "Play Again"
-            scoreText = "Score: \(score)/10"
-        }
     }
 }
-
