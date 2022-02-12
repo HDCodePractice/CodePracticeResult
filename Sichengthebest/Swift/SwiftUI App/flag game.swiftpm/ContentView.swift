eae4414e90b6053d458ff75b24f55b89455b5a13
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2022-02-12 10:02:51 -0500

    ghg

--- Sichengthebest/Swift/SwiftUI App/flag game.swiftpm/ContentView.swift
+++ Sichengthebest/Swift/SwiftUI App/flag game.swiftpm/ContentView.swift
@@ -3,32 +3,59 @@ import SwiftUI
 struct ContentView: View {
     @State var game = Game()
     
+    var gamestart: some View {
+        VStack{
+            HStack{
+                Text(game.question)
+            }
+            Text("")
+            ForEach(game.answers,id:\.self){ flag in
+                HStack{
+                    Text(flag)
+                        .font(.largeTitle)
+                        .onTapGesture{
+                            game.toggleAnswer(tAnswer: flag)
+                        }
+                }
+            }
+        }
+    }
+    
+    var gameEnd: some View{
+        VStack{
+            HStack{
+                Text(game.question)
+            }
+            Text("")
+            ForEach(game.answers,id:\.self){ flag in
+                HStack{
+                    Text(flag)
+                        .font(.largeTitle)
+                    if flag == game.answer{
+                        Image(systemName: flag == game.correct ? "checkmark.circle.fill" : "x.circle.fill")
+                            .foregroundColor(flag == game.correct ? .green : .red)
+                    }
+                    if flag == game.correct && flag != game.answer {
+                        Image(systemName: flag == game.correct ? "checkmark.circle.fill" : "x.circle.fill")
+                            .foregroundColor(flag == game.correct ? .green : .red)
+                    }
+                }
+            }
+        }
+    }
+    
     var body: some View {
         VStack{
             if game.gameStart {
-                HStack{
-                    Text(game.question)
-                }
-                Text("")
-                ForEach(game.answers,id:\.self){ flag in
-                    HStack{
-                        Text(flag)
-                            .font(.largeTitle)
-                        Image(systemName: "checkmark.circle.fill")
-                            .foregroundColor(.green)
-                        Image(systemName: "x.circle.fill")
-                            .foregroundColor(.red)
-                    }
-                }
-                
+                gamestart
             }else{
-                Text("A! Nothing!")
+                gameEnd
+                Text("Next")
+                    .onTapGesture {
+                        game.newGame()
+                    }
             }
             
-            Text("Start")
-                .onTapGesture {
-                    game.newGame()
-                }
         }
     }
 }
