e5fa827b25a33cf9732fa82ac3c5bc585fdf57ce
sunnypo <siyuan0202@gmail.com>
2022-02-12 00:24:13 -0500

    fix interface, add tap number and % =/-

--- sunnypo/App20220123/caculator.swiftpm/ContentView.swift
+++ sunnypo/App20220123/caculator.swiftpm/ContentView.swift
@@ -6,14 +6,23 @@ struct ContentView: View {
     let row2 = ["4", "5", "6", "-"]
     let row3 = ["1", "2", "3", "+"]
     let row4 = ["0", "", ".", "="]
-    @State var number : Int = 0
+    @State var number : Double = 0
+    func tapIn(tapedContent : String) {
+        if Int(tapedContent) != nil {
+            number = number*10 + Double(tapedContent)!
+        }else if tapedContent == "+/-"{
+            number = number * -1
+        }else if tapedContent == "%"{
+            number = number / 100
+        }
+    }
     var body: some View {
         VStack(spacing: 0){
             Rectangle().fill(Color.white).frame(width: .infinity)
             Rectangle().fill(Color.white).frame(width: .infinity)
             ZStack(alignment:.trailing){
                 Rectangle().fill(Color.white).frame(width: .infinity)
-                Text("\(number)").font(.title).foregroundColor(Color.black)
+                Text("\(number)").font(.title).foregroundColor(Color.black).padding(/*@START_MENU_TOKEN@*/.all, 10.0/*@END_MENU_TOKEN@*/)
             }
             HStack(spacing: 0){
                 ForEach(function,id: \.self)
@@ -23,8 +32,12 @@ struct ContentView: View {
                                        Text("\(i)")
                                            .font(.title).foregroundColor(Color.white)
                                     }
+                                   .onTapGesture{
+                                       tapIn(tapedContent: i)
+                                   }
                                }
             }
+
             
             HStack(spacing: 0){
                 ForEach(row1,id:\.self){ n in
@@ -33,6 +46,9 @@ struct ContentView: View {
                         Text("\(n)")
                             .font(.title).foregroundColor(Color.white)
                      }
+                    .onTapGesture{
+                        tapIn(tapedContent: n)
+                    }
                 }
             }
             
@@ -43,6 +59,9 @@ struct ContentView: View {
                         Text("\(n)")
                             .font(.title).foregroundColor(Color.white)
                      }
+                    .onTapGesture{
+                        tapIn(tapedContent: n)
+                    }
                 }
             }
             
@@ -53,16 +72,22 @@ struct ContentView: View {
                         Text("\(n)")
                             .font(.title).foregroundColor(Color.white)
                      }
+                    .onTapGesture{
+                        tapIn(tapedContent: n)
+                    }
                 }
             }
             
             HStack(spacing: 0){
-                ForEach(row3,id:\.self){ n in
+                ForEach(row4,id:\.self){ n in
                     ZStack {
                         Circle().padding(/*@START_MENU_TOKEN@*/.all, 4.0/*@END_MENU_TOKEN@*/).foregroundColor(Int(n) == nil ? Color.gray : Color.pink)
                         Text("\(n)")
                             .font(.title).foregroundColor(Color.white)
                      }
+                    .onTapGesture{
+                        tapIn(tapedContent: n)
+                    }
                 }
             }
             
