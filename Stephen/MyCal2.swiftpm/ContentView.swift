5c18034bca297e17b74d7f52ee898ec00d60e503
stephenzhu01 <82696093+stephenzhu01@users.noreply.github.com>
2022-02-14 00:24:55 +0800

    class

---
+++ Stephen/MyCal2.swiftpm/ContentView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var vm = CalculaterModel()
+    
+    var body: some View {
+        VStack {
+            ForEach(vm.keyboard, id:\.self){row in
+                HStack(spacing:10){
+                    ForEach(row, id:\.self){button in
+                        Button(buttonTitle: button.title, background: button.background, width: button.width, height: button.height)
+                    }
+                }
+            }
+        }
+    }
+}
