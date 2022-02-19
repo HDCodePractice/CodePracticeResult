0f5741d973209c05a8ffbc29c18bed55aed72dbe
hendryshao <87082007+hendryshao@users.noreply.github.com>
2022-02-19 11:23:37 -0500

    calcul

--- Hendry/SwiftUI/calculator.swiftpm/ContentView.swift
+++ Hendry/SwiftUI/calculator.swiftpm/ContentView.swift
@@ -1,97 +1,56 @@
 import SwiftUI
 
 struct ContentView: View {
+    let calc = [["(",")","mc","m+","m-","mr","AC","+/-","%","÷"],["2nd","x2","x3","xy","2x","10x","7","8","9","×"],["1/x","2√x","3√x","y√x","In","log10","4","5","6","-"],["x!","sin","cos","tan","e","EE","1","2","3","+"],["Rad","sinh","cosh","tanh","∏","Rand","0",".","="]]
     var body: some View {
-        VStack {
-            HStack{
-                HStack{
-                Text("AC")
-                    .frame(width: 25.0)
-                    .background(.brown)
-                    .font(.title)
+        ZStack {
+            Color(.black)
+            VStack(spacing:-29) {
+                    ForEach(calc, id: \.self) { column in
+                        HStack (spacing: 3) {
+                            ForEach(column, id: \.self) { row in
+                                Button() {
+
+                                } label: {
+                                    if row != "0" {
+                                        ZStack {
+                                            if ["=","+","-","×","÷"].contains(row) {
+                                                Color.init(red: 155.0, green: 0.0, blue: 20.0)
+                                            } else if ["AC","+/-","%"] .contains(row) {
+                                                Color.init(red: 00.0, green: 50.0, blue: 100.0)
+                                            } else if ["1","2","3","4","5","6","7","8","9","."].contains(row) {
+                                                Color.red
+                                            }else{
+                                                Color.green
+                                            }
+                                            Text("\(row)")
+                                                .font(.title)
+                                        }
+                                        .frame(width: 70, height: 70)
+                                        .clipShape(RoundedRectangle(cornerRadius: 40))
+                                        .shadow(radius: 5)
+                                    } else if row == "0" {
+                                        ZStack {
+                                            Color.red
+                                            Text("\(row)")
+                                                .font(.title)
+                                        }
+                                        .frame(width: 146, height: 70)
+                                        .clipShape(RoundedRectangle(cornerRadius: 40))
+                                        .shadow(radius: 5)
+                                    }
+                                }
+                            }
+                        }.padding()
+                    }
                 }
-                Text("+/-")
-                    .frame(width: 30.0)
-                    .background(.brown)
-                    .font(.title)
-                
-                Text("%")
-                    .frame(width: 30.0)
-                    .background(.brown)
-                    .font(.title)
-                Text("/")
-                    .frame(width: 30.0)
-                    .background(.blue)
-                    .font(.title)
-            }
-            HStack{
-                Text("7")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("8")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("9")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("*")
-                    .frame(width: 30.0)
-                    .background(.blue)
-                    .font(.title)
-            }
-            HStack{
-                Text("4")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("5")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("6")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("-")
-                    .frame(width: 30.0)
-                    .background(.blue)
-                    .font(.title)
-            }
-            HStack{
-                Text("1")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("2")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("3")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("+")
-                    .frame(width: 30.0)
-                    .background(.blue)
-                    .font(.title)
-            }
-            HStack{
-                Text("0")
-                    .frame(width: 68.0)
-                    .background(.gray)
-                    .font(.title)
-                Text(".")
-                    .frame(width: 30.0)
-                    .background(.gray)
-                    .font(.title)
-                Text("=")
-                    .frame(width: 30.0)
-                    .background(.blue)
-                    .font(.title)
-            }
         }
+        .ignoresSafeArea()
+    }
+}
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.portraitUpsideDown)
     }
 }
