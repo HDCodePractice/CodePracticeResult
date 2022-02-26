c684be39c3b5a166bc3a36b7d30cfe3f8b94e0d1
James6942 <86499178+James6942@users.noreply.github.com>
2022-02-26 10:02:32 -0500

    Keyboard

--- James/Swift UI/Keyboard.swiftpm/ContentView.swift
+++ James/Swift UI/Keyboard.swiftpm/ContentView.swift
@@ -1,12 +1,12 @@
 import SwiftUI
 
 struct ContentView: View {
-    let calc = [["(",")","mc","m+","m-","mr","AC","+/-","%","÷"],["2nd","x2","x3","xy","2x","10x","7","8","9","×"],["1/x","2√x","3√x","y√x","In","log10","4","5","6","-"],["x!","sin","cos","tan","e","EE","1","2","3","+"],["Rad","sinh","cosh","tanh","∏","Rand","0",".","="]]
+    let key = [["q","w","e","r","t","y","u","i","o","p","delete"],["CAP","a","s","d","f","g","h","j","k","l","return"],["z", "x","c","v","b","n","m",",","."]]
     var body: some View {
         ZStack {
             Color(.black)
             VStack(spacing:-29) {
-                    ForEach(calc, id: \.self) { column in
+                    ForEach(key, id: \.self) { column in
                         HStack (spacing: 3) {
                             ForEach(column, id: \.self) { row in
                                 Button() {
@@ -14,15 +14,6 @@ struct ContentView: View {
                                 } label: {
                                     if row != "0" {
                                         ZStack {
-                                            if ["=","+","-","×","÷"].contains(row) {
-                                                Color.init(red: 155.0, green: 0.0, blue: 20.0)
-                                            } else if ["AC","+/-","%"] .contains(row) {
-                                                Color.init(red: 00.0, green: 50.0, blue: 100.0)
-                                            } else if ["1","2","3","4","5","6","7","8","9","."].contains(row) {
-                                                Color.red
-                                            }else{
-                                                Color.green
-                                            }
                                             Text("\(row)")
                                                 .font(.title)
                                         }
