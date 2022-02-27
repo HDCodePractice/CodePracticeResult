bc61369a66d1d6e8bde905f5050aa56109299e27
老房东 <huangdong@gmail.com>
2022-02-27 09:21:14 -0500

    小朋友的计算器尝试

---
+++ hd/SwiftUI/CalculaterNew.swiftpm/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  Calculater
+//
+//  Created by 老房东 on 2022-02-26.
+//
+
+import Foundation
+
+struct ViewModel{
+    let grid = [
+        ["AC","+/-","%","÷"],
+        ["7","8","9","x"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".","="]
+    ]
+}
