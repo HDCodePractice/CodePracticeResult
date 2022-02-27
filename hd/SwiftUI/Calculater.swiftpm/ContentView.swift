9476283289aea1cff5b7e5e78407f75daeefaae2
老房东 <huangdong@gmail.com>
2022-02-27 11:28:07 -0500

    fix一点

--- hd/SwiftUI/Calculater.swiftpm/ContentView.swift
+++ hd/SwiftUI/Calculater.swiftpm/ContentView.swift
@@ -34,3 +34,11 @@ struct ContentView: View {
         }
     }
 }
+
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
