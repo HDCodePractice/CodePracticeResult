abbbc18f2d8d0a33b7b1f0f2ecf17d926773003a
bernieharvard <82739875+bernieharvard@users.noreply.github.com>
2022-02-28 21:37:28 -0500

    2022-2-20 classwork part 2

--- Bernie/Calculater.swiftpm/ContentView.swift
+++ Bernie/Calculater.swiftpm/ContentView.swift
@@ -34,3 +34,10 @@ struct ContentView: View {
         }
     }
 }
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+.previewInterfaceOrientation(.landscapeLeft)
+    }
+}
