cc29b5cc7647630ac95355563b7e1cd3a06a71f7
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2022-03-05 10:11:26 -0500

    Homework (3/5/2022)
    - finished 3 things
    - missing one

---
+++ Claire/Swift UI/Calculater New.swiftpm/ViewModel.swift
@@ -0,0 +1,18 @@
+//
+//  ViewModel.swift
+//  Calculater New
+//
+//  Created by Qianji Yao on 2022/2/26.
+//
+
+import Foundation
+
+struct ViewModel{
+    let grid = [
+        ["AC","+/-","%","÷"],
+        ["7","8","9","x"],
+        ["4","5","6","-"],
+        ["1","2","3","+"],
+        ["0",".","="]
+    ]
+}
