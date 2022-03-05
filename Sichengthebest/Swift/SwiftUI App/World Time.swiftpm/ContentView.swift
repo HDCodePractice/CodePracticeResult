27e6d0f899fe770d21a7f42d7b50cfc1e38d2a41
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2022-03-05 10:18:31 -0500

    add non-moving world clock

--- Sichengthebest/Swift/SwiftUI App/World Time.swiftpm/ContentView.swift
+++ Sichengthebest/Swift/SwiftUI App/World Time.swiftpm/ContentView.swift
@@ -3,19 +3,19 @@ import SwiftUI
 struct ContentView: View {
     @State var vm = ViewModel()
     var body: some View {
-        ScrollView {
-            VStack(alignment: .leading){
-                Text("World Clock")
-                    .font(.largeTitle)
-                    .bold()
+        VStack(alignment: .leading){
+            Text("World Clock")
+                .font(.largeTitle)
+                .bold()
+            ScrollView {
                 Divider()
                 ForEach(vm.items){ city in
                     ClockView(title: city.name, timeDifference: city.timeDifference,time:city.time,ampm:city.ampm)
                     Divider()
                 }
             }
-            .padding()
-            Spacer()
         }
+        .padding()
+        Spacer()
     }
 }
