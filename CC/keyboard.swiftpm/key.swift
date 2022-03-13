2282d38d95a828f57f9331949618a4ab29ee76c0
haiyanw5837 <98291864+haiyanw5837@users.noreply.github.com>
2022-03-13 08:55:53 -0400

    keyboard

---
+++ CC/keyboard.swiftpm/key.swift
@@ -0,0 +1,36 @@
+//
+//  File.swift
+//  keyboard
+//
+//  Created by cc on 2022-03-06.
+//
+
+import SwiftUI
+
+enum key:Hashable{
+    enum symbol:String{
+        case at = "@"
+        case hash = "#"
+        case dollar = "$"
+        case and = "&"
+        case asterisk = "*"
+        case leftparenthese = "("
+        case rightparenthese = ")"
+        case singlequote = "'"
+        case quote = "\""
+        case percent = "%"
+        case minus = "-"
+        case plus = "+"
+        case equal = "="
+        case divide = "/"
+        case semicolon = ";"
+        case colon = ":"
+        case exclamation = "!"
+        case comma = ","
+        case period = "."
+        case question = "?"
+        case change = "123"
+        
+        
+    }
+}
