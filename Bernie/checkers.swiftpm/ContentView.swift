44f996b6b356a6a2554880454a9fecb75c1afb5b
bernieharvard <82739875+bernieharvard@users.noreply.github.com>
2022-03-20 15:02:56 -0400

    2022-3-20 classwork

---
+++ Bernie/checkers.swiftpm/ContentView.swift
@@ -0,0 +1,58 @@
+import SwiftUI
+
+struct ContentView: View {
+    @State var vm = ViewModel()
+    var body: some View {
+        VStack(spacing:0){
+            ForEach (vm.grid, id:\.self){row in
+                HStack(spacing:0){
+                    ForEach (row){item in
+                        GridItemView(item: item)
+//                        ZStack{
+//                            Rectangle()
+//                                .fill(item.background.color)
+//                            if !item.chess.isHidden{
+//                                Image(systemName: item.chess.rawValue)
+//                                    .resizable()
+//                                    .scaledToFit()
+//                                    .foregroundColor(.gray)
+//                                    .padding(2)
+//                            }
+                        }
+//                        .frame(width: 30, height: 30)
+                    }
+                }
+            }
+//        }.border(.black,width:1.0).padding()
+//        .scaleEffect()
+//        .scaledToFit()
+        
+    }
+    
+}
+
+struct GridItemView : View{
+    @State var item : GridItem
+    var body: some View{
+        ZStack{
+            Rectangle()
+                .fill(item.background.color)
+            Image(systemName:item.chess.rawValue)
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.red)
+                .padding(2)
+        }
+        .onTapGesture{
+            item.chess = item.chess.nextChess
+    }
+        .frame(width: 30, height: 30)
+    }
+}
+
+//struct ViewModel{
+//    var gridSize = 8
+//    var gridBackGround : [[GridBackGround]]
+//    var chessGrid:[[ChessItem]]
+//}
+
