92eec538eddb07a430fd93131a81324a696d17b0
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2022-04-01 16:16:41 -0700

    work

---
+++ Jaden/SwiftUI/Connect4.swiftpm/ContentView.swift
@@ -0,0 +1,43 @@
+import SwiftUI
+
+struct ContentView : View {
+    @State var vm = ViewModel()
+    var body: some View{
+        ZStack{
+            Color.gray
+            VStack{
+//
+//                // green
+//                Image(systemName: "circle.fill")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundColor(.green)
+//                    .onTapGesture {
+//                        vm.printGrid()
+//                    }
+//                // black
+//                Image(systemName: "circle.fill")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundColor(.black)
+//                // greenWin
+//                Image(systemName: "star.circle.fill")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundColor(.green)
+//                // blackWin
+//                Image(systemName: "star.circle.fill")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundColor(.black)
+//                // nothing
+//                Image(systemName: "circle.fill")
+//                    .resizable()
+//                    .scaledToFit()
+//                    .foregroundColor(.gray)
+                
+            }
+        }.ignoresSafeArea()
+
+    }
+}
---
+++ Jaden/SwiftUI/checkers.swiftpm/DataModel.swift
@@ -0,0 +1,56 @@
+import Foundation
+import SwiftUI
+
+struct GridItem: Identifiable,Hashable{
+    let id = UUID()
+    var background : GridBackGround
+    var checker : CheckerItem
+}
+
+enum GridBackGround{
+    case white
+    case black
+}
+
+enum CheckerItem:String{
+    case white = "circle"
+    case black = "circle.fill"
+    case whiteKing = "crown"
+    case blackKing = "crown.fill"
+    case nothing = ""
+}
+
+extension CheckerItem{
+    var isHidden:Bool{
+        if self == .nothing{
+            return true
+        }
+        return false
+    }
+    
+    var nextchecker: CheckerItem{
+        switch self {
+        case .white:
+            return  .whiteKing
+        case .whiteKing:
+            return .black
+        case .black:
+            return .blackKing
+        case .blackKing:
+            return .nothing
+        default:
+            return  .white
+        }
+    }
+}
+
+extension GridBackGround{
+    var color: Color{
+        switch self {
+        case .white:
+            return .white
+        case .black:
+            return .blue
+        }
+    }
+}
