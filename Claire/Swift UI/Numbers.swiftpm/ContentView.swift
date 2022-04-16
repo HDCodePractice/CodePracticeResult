e0dabb4b7c590d99d6d7bf8863f38971edc8adb9
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2022-04-16 12:09:03 -0400

    Updated WWDC Numbers

--- Claire/Swift UI/Numbers.swiftpm/ContentView.swift
+++ Claire/Swift UI/Numbers.swiftpm/ContentView.swift
@@ -1,7 +1,17 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var vm = ViewModel()
     var body: some View {
-        
+        LinearGradient(colors: [.yellow,.red], startPoint: .top, endPoint: .bottom)
+        ForEach(vm.gridHeight, id:\.self){ height in
+            ForEach(vm.gridWidth, id:\.self){ width in
+                Rectangle()
+                    .frame(width: 20, height: 20)
+                    .fill(Color.white)
+                    .cornerRadius(20)
+                    .shadow(radius: 5)
+            }
+        }
     }
 }
