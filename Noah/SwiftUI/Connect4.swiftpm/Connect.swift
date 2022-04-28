ab98b173e35af6acb3ae386fe7f4615d6126121a
Noah <59535872+SSnipro@users.noreply.github.com>
2022-04-27 20:33:57 -0400

    buh

--- Noah/SwiftUI/Connect4.swiftpm/Connect.swift
+++ Noah/SwiftUI/Connect4.swiftpm/Connect.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct GridItem: Identifiable, Hashable {
+struct GridItem: Identifiable,Hashable {
     var id = UUID()
     var connect: Connect
 }
@@ -11,8 +11,10 @@ enum Connect {
     case greenWin
     case blackWin
     case nothing
-    
-    var color: Color{
+}
+
+extension Connect {
+    var color:Color{
         switch self {
         case . green, . greenWin:
             return .green
@@ -31,4 +33,86 @@ enum Connect {
             return "star.fill"
         }
     }
+}
+
+func checkHorizontal(row: Int, col: Int, target: Int) -> Bool {
+    var index = 0
+    if grid[row][col] == target {
+        for i in 1 ... 3 {
+            if col <= 4 {
+                if grid[row][col+i] == target {
+                    index += 1
+                }
+            }
+        }
+    }
+    
+    if index == 3 {
+        return true
+    }
+    return false
+}
+
+func checkVertical(row: Int, col: Int, target: Int) -> Bool {
+    var index = 0
+    if grid[row][col] == target {
+        for i in 1 ... 3 {
+            if row >= 3 {
+                if grid[row-i][col] == target {
+                    index += 1
+                }
+            }
+        }
+    }
+    
+    if index == 3 {
+        return true
+    }
+    return false
+}
+
+func checkDiagonal(row: Int, col: Int, target: Int) -> Bool {
+    var index = 0
+    var index2 = 0
+    if grid[row][col] == target {
+        for i in 1 ... 3 {
+            if row >= 3 && col <= 4 {
+                if grid[row-i][col+i] == target {
+                    index += 1
+                }
+                
+            }
+            if row >= 3 && col >= 3 {
+                if grid[row-i][col-i] == target {
+                    index2 += 1
+                }
+            }
+        }
+    }
+    if index == 3 || index2 == 3 {
+        return true
+    }
+    return false
+}
+
+func checkOneWon() -> Bool {
+    for eachRow in 0 ... 6 {
+        for each in 0 ... 7 {
+            if checkHorizontal(row: eachRow, col: each, target: 1) || checkVertical(row: eachRow, col: each, target: 1) || checkDiagonal(row: eachRow, col: each, target: 1) {
+                return true
+            }
+        }
+    }
+    return false
+}
+
+func checkTwoWon() -> Bool {
+    for eachRow in 0 ... 6 {
+        for each in 0 ... 7 {
+            if checkHorizontal(row: eachRow, col: each, target: 2) || checkVertical(row: eachRow, col: each, target: 2) || checkDiagonal(row: eachRow, col: each, target: 2) {
+                return true
+            }
+        }
+    }
+    return false
 }
--- Noah/SwiftUI/Connect4.swiftpm/ViewModel.swift
+++ Noah/SwiftUI/Connect4.swiftpm/ViewModel.swift
@@ -1,7 +1,7 @@
 import Foundation
 import SwiftUI
 
-struct ViewModel {
+struct ViewModel{
     var gridWidth = 8
     var gridHeight = 7
     var gridBackground  = Color.red
@@ -12,9 +12,16 @@ struct ViewModel {
         for _ in 1 ... gridWidth  {
             var col : [GridItem] = []
             for _ in 1 ... gridHeight {
-                col.append(GridItem(connect: .green))
+                col.append(GridItem(connect:.nothing))
             }
             grid.append(col)
         }
+    }
+    func tapColumn(column: [GridItem]) -> [GridItem] {
+        var rColumn = column
+        for i in 0..<rColumn.count {
+            rColumn[i] = GridItem(connect: .green)
+        }
+        return rColumn
     }
 }
