7f1ed0441b79f986af400d6b07c54d2a78628ab4
bernieharvard <82739875+bernieharvard@users.noreply.github.com>
2022-06-26 11:11:20 -0400

    课堂作业更新

--- Bernie/cc.swiftpm/ViewModel.swift
+++ Bernie/cc.swiftpm/ViewModel.swift
@@ -9,5 +9,17 @@ import Foundation
 import SwiftUI
 
 struct ViewModel{
+    var row = 9
+    var column = 8
+    var background = Color.yellow
+    var grid:[[GridItem]] = []
     
+    init(){
+        for _ in 0..<row{
+            var col:[GridItem] = []
+            for _ in 0..<column{
+                col.append(GridItme(cc: .nothing))
+            }
+        }
+    }
 }
