9cd751d49a69df45e294948b41d17cbe19695aca
老房东 <huangdong@gmail.com>
2022-07-10 11:23:53 -0400

    加入BlackJack的上课内容

--- hd/SwiftUI/BlackJack.playgroundbook/Edits/UserEdits.diffpack/UserModules/MyFiles.playgroundmodule/Sources/SendCards.swift
+++ hd/SwiftUI/BlackJack.playgroundbook/Edits/UserEdits.diffpack/UserModules/MyFiles.playgroundmodule/Sources/SendCards.swift
@@ -9,6 +9,10 @@ public func startGame(playerNumber: Int=2, suitsNumber: Int=3){
     }
 }
 
+/**
+ player: 发牌给第几个player，从0开始
+ number: 发几张牌
+ **/
 public func sendCards(player: Int,number: Int=1){
     if cards.isEmpty{
         shuffleCards()
