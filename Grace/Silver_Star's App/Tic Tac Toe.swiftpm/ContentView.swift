9a8054a3039d0332a657a035a61b90d082d135cd
silvestar32 <82227329+silvestar32@users.noreply.github.com>
2022-07-25 16:23:54 -0700

    Tic tak toe

--- Grace/Silver_Star's App/Tic Tac Toe.swiftpm/ContentView.swift
+++ Grace/Silver_Star's App/Tic Tac Toe.swiftpm/ContentView.swift
@@ -1,12 +1,53 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var grid22 = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                    Rectangle()
+                        .foregroundColor(.teal)
+                Rectangle()
+                    .foregroundColor(.teal)
+                Rectangle()
+                    .foregroundColor(.teal)
+            }
+            
+            HStack{
+                Rectangle()
+                    .foregroundColor(.teal)
+                ZStack{
+                    Rectangle()
+                        .foregroundColor(.teal)
+                    if grid22 == 1 {
+                        CirclePiece()
+                    }else if grid22 == 2{
+                        CrossPiece()
+                    }
+                    
+                    
+                }
+                Rectangle()
+                    .foregroundColor(.teal)
+                
+            }
+            .onTapGesture {
+                if grid22 == 0{
+                    grid22=1
+                }else if grid22==1{
+                    grid22=2
+                }else {
+                    grid22=0
+                }
+            }
+            HStack{
+                Rectangle()
+                    .foregroundColor(.teal)
+                Rectangle()
+                    .foregroundColor(.teal)
+                Rectangle()
+                    .foregroundColor(.teal)
+            }
         }
     }
 }
