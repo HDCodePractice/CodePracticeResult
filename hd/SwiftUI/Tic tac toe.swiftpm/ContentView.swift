25836b0ba868a2e1165523642b6f7b98422e805e
老房东 <huangdong@gmail.com>
2022-07-29 09:36:50 -0400

    小小班的小练习

--- hd/SwiftUI/Tic tac toe.swiftpm/ContentView.swift
+++ hd/SwiftUI/Tic tac toe.swiftpm/ContentView.swift
@@ -1,12 +1,49 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var grid22 = 0
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HStack{
+                Rectangle()
+                    .foregroundColor(.red)
+                Rectangle()
+                    .foregroundColor(.red)
+                Rectangle()
+                    .foregroundColor(.red)
+            }
+            HStack{
+                Rectangle()
+                    .foregroundColor(.red)
+                ZStack{
+                    Rectangle()
+                        .foregroundColor(.red)
+                    if grid22 == 1 {
+                        CirclePiece()
+                    }else if grid22 == 2{
+                        CrossPiece()
+                    }
+                }
+                .onTapGesture {
+                    if grid22 == 0{
+                        grid22=1
+                    }else if grid22==1{
+                        grid22=2
+                    }else{
+                        grid22=0
+                    }
+                }
+                Rectangle()
+                    .foregroundColor(.red)
+            }
+            HStack{
+                Rectangle()
+                    .foregroundColor(.red)
+                Rectangle()
+                    .foregroundColor(.red)
+                Rectangle()
+                    .foregroundColor(.red)
+            }
         }
     }
 }
