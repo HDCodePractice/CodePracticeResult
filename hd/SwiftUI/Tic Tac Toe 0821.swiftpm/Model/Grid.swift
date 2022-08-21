b29a39936c68ea0c574444ee2e2fadf5b2b4680b
老房东 <huangdong@gmail.com>
2022-08-21 19:53:18 -0400

    加入一个全新的Tic Tac Toe

---
+++ hd/SwiftUI/Tic Tac Toe 0821.swiftpm/Model/Grid.swift
@@ -0,0 +1,23 @@
+import SwiftUI
+
+struct Grid: Hashable, Identifiable{
+    let id = UUID()
+    var chess : Chess
+    var x : Int
+    var y : Int
+    
+    func isEmpty()->Bool{
+        if chess == .null{
+            return true
+        }
+        return false
+    }
+    
+    mutating func changeChess(newChess: Chess)->Bool{
+        if chess == .null{
+            chess = newChess
+            return true 
+        }
+        return false
+    }
+}
