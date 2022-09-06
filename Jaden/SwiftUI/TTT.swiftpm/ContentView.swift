90b70d5da087aa8df320a484727a7bd695db6a70
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2022-09-06 16:30:47 -0700

    work

--- Jaden/SwiftUI/TTT.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/TTT.swiftpm/ContentView.swift
@@ -2,13 +2,60 @@ import SwiftUI
 
 struct ContentView: View {
     @State var board = [[0,0,0],[0,0,0],[0,0,0]]
+    @State var ending = ""
     @State var isFirst = true
+    func checkWin(){
+        for i in 1...2{
+            if board.contains([i,i,i]){
+                if i == 1{
+                    ending = "Player O Wins!"
+                }else{
+                    ending = "Player X Wins!"
+                }
+            }else if board[0][0] == i && board[1][0] == i && board[2][0] == i{
+                if i == 1{
+                    ending = "Player O Wins!"
+                }else{
+                    ending = "Player X Wins!"
+                }
+            }else if board[0][1] == i && board[1][1] == i && board[2][1] == i{
+                if i == 1{
+                    ending = "Player O Wins!"
+                }else{
+                    ending = "Player X Wins!"
+                }
+            }else if board[0][2] == i && board[1][2] == i && board[2][2] == i{
+                if i == 1{
+                    ending = "Player O Wins!"
+                }else{
+                    ending = "Player X Wins!"
+                }
+            }else if board[0][0] == i && board[1][1] == i && board[2][2] == i{
+                if i == 1{
+                    ending = "Player O Wins!"
+                }else{
+                    ending = "Player X Wins!"
+                }
+            }else if board[0][2] == i && board[1][1] == i && board[2][0] == i{
+                if i == 1{
+                    ending = "Player O Wins!"
+                }else{
+                    ending = "Player X Wins!"
+                }
+            }else if !board[0].contains(0) && !board[1].contains(0) && !board[2].contains(0) {
+                ending = "It's a tie"
+            }
+        }
+        
+    }
     var body: some View {
         VStack{
             HStack{
                 Text("Player:")
                 Image(systemName: isFirst ? "circle" : "pencil.slash")
                     .foregroundColor(.accentColor)
+                
+                Text("\(ending)")
             }
             .font(.largeTitle)
             ZStack{
@@ -36,16 +83,20 @@ struct ContentView: View {
                                         .scaleEffect(board[row][column]==2 ? 1 : 0)
                                 }
                                 .onTapGesture {
-                                    withAnimation(.linear(duration: 2)) { 
-                                        if board[row][column]==0{
-                                            if isFirst{
-                                                board[row][column]=1
-                                            }else{
-                                                board[row][column]=2
+                                    withAnimation() {
+                                        if ending == ""{
+                                            if board[row][column]==0{
+                                                if isFirst{
+                                                    board[row][column]=1
+                                                }else{
+                                                    board[row][column]=2
+                                                }
+                                                isFirst.toggle()
                                             }
-                                            isFirst.toggle()
                                         }
+                                        
                                     }
+                                    checkWin()
                                 }
                             }
                         }
@@ -53,9 +104,10 @@ struct ContentView: View {
                 }
             }
             Button("Restart Game"){
-                withAnimation(.linear(duration: 2)) { 
+                withAnimation(.easeInOut(duration: 2)) { 
                     board = [[0,0,0],[0,0,0],[0,0,0]]
                     isFirst = true
+                    ending = ""
                 }
             }
         }
