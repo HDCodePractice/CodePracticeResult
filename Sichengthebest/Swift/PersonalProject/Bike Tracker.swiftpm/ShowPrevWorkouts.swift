4e37f574de000cd9adef53faa263d07446afed2e
Sichengthebest <59479191+Sichengthebest@users.noreply.github.com>
2022-09-28 19:32:53 -0400

    make showing old workouts better

--- Sichengthebest/Swift/PersonalProject/Bike Tracker.swiftpm/ShowPrevWorkouts.swift
+++ Sichengthebest/Swift/PersonalProject/Bike Tracker.swiftpm/ShowPrevWorkouts.swift
@@ -7,47 +7,53 @@ struct PreviousWorkoutsView: View {
         VStack {
             Text("Your previous workouts:")
                 .font(.system(size:30))
-            ForEach(0..<workouts.count,id:\.self) { index in
-                // Display each workout as a separate window
-                NavigationLink {
-                    VStack {
-                        Text("\(workouts[index].date,style: .date)")
-                            .foregroundColor(.yellow)
-                            .font(.system(size: 25))
-                        Text("Time: \(Stopwatch(progressTime: workouts[index].time))")
-                            .font(.system(size: 20))
-                        Text("Speed: \(String(format:"%.1f", workouts[index].speed)) kph")
-                            .font(.system(size: 20))
-                        Text("Distance: \(String(format: "%.2f",workouts[index].distance)) km")
-                            .font(.system(size: 20))
-                        MapView(lineCoordinates: workouts[index].coordinates,beforePauses: workouts[index].beforePauses, region: MKCoordinateRegion(
-                            center: workouts[index].coordinates[0], span: MKCoordinateSpan(
-                                latitudeDelta: 0.05, longitudeDelta: 0.05
-                            )
-                        ), ended: true)
-                        RectButtonView(text:"Delete this workout",color:.red)
-                            .frame(height:20)
-                            .onTapGesture {
-                                workouts.remove(at: index)
-                            }
-                            .padding()
-                    }
-                } label: {
-                    // Rectangular text displaying each workout
-                    ZStack {
-                        RoundedRectangle(cornerRadius: 10)
-                            .frame(height: 40)
-                            .foregroundColor(.blue)
-                        HStack {
+            Divider()
+            if workouts.count >= 1 {
+                ForEach(0..<workouts.count,id:\.self) { index in
+                    // Display each workout as a separate window
+                    NavigationLink {
+                        VStack {
                             Text("\(workouts[index].date,style: .date)")
-                                .foregroundColor(.black)
+                                .foregroundColor(.yellow)
+                                .font(.system(size: 25))
+                            Text("Time: \(Stopwatch(progressTime: workouts[index].time))")
                                 .font(.system(size: 20))
-                            Text("\(String(format: "%.2f",workouts[index].distance)) km")
-                                .foregroundColor(.black)
-                                .font(.system(size: 18))
+                            Text("Speed: \(String(format:"%.1f", workouts[index].speed)) kph")
+                                .font(.system(size: 20))
+                            Text("Distance: \(String(format: "%.2f",workouts[index].distance)) km")
+                                .font(.system(size: 20))
+                            MapView(lineCoordinates: workouts[index].coordinates,beforePauses: workouts[index].beforePauses, region: MKCoordinateRegion(
+                                center: workouts[index].coordinates[0], span: MKCoordinateSpan(
+                                    latitudeDelta: 0.05, longitudeDelta: 0.05
+                                )
+                            ), ended: true)
+                            RectButtonView(text:"Delete this workout",color:.red)
+                                .frame(height:20)
+                                .onTapGesture {
+                                    workouts.remove(at: index)
+                                }
+                                .padding()
+                        }
+                    } label: {
+                        // Rectangular text displaying each workout
+                        ZStack {
+                            RoundedRectangle(cornerRadius: 10)
+                                .frame(height: 40)
+                                .foregroundColor(.blue)
+                            HStack {
+                                Text("\(workouts[index].date,style: .date)")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 20))
+                                Text("\(String(format: "%.2f",workouts[index].distance)) km")
+                                    .foregroundColor(.black)
+                                    .font(.system(size: 18))
+                            }
                         }
                     }
+                    
                 }
+            } else {
+                Text("You have no workouts to display. Go to \"Start a workout\" to start a new workout.")
             }
         }
     }
