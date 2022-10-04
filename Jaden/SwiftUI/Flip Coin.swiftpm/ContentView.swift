a973948dd9442c343cd216cb19cced9b6f05f36c
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2022-10-03 21:02:30 -0700

    work

--- Jaden/SwiftUI/Flip Coin.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/Flip Coin.swiftpm/ContentView.swift
@@ -2,13 +2,31 @@ import SwiftUI
 
 struct ContentView: View {
     @State var coin = ["1","2","3","4","5","6"].randomElement()!
+    @State var coin1 = ["1","2","3","4","5","6"].randomElement()!
+    @State var coin2 = ["1","2","3","4","5","6"].randomElement()!
     @State var degress: Double = 0
     @State var isRunning = false
-    @State var diceNumber : [String] = ["0","0","0"]
+    @State var diceNumber : [String] = ["0"]
+    @State var diceNumber1 : [String] = ["0"]
+    @State var diceNumber2 : [String] = ["0"]
     var body: some View {
         VStack {
             VStack{
                 Image(coin)
+                    .resizable()
+                    .scaledToFit()
+                    .mask { 
+                        Rectangle()
+                    }
+                    .rotation3DEffect(Angle(degrees: degress), axis: (x: 0.5, y: 0.6 , z: 0.5))
+                Image(coin1)
+                    .resizable()
+                    .scaledToFit()
+                    .mask { 
+                        Rectangle()
+                    }
+                    .rotation3DEffect(Angle(degrees: degress), axis: (x: 0.5, y: 0.6 , z: 0.5))
+                Image(coin2)
                     .resizable()
                     .scaledToFit()
                     .mask { 
@@ -22,33 +40,51 @@ struct ContentView: View {
             }else{
                 Text("\(coin)")
                     .font(.title)
-                HStack{
-                    Text("Numbers Rolled: \(diceNumber[0])")
-//                    Text("\(diceNumber[1])")
-//                    Text("\(diceNumber[2])")
-                }
-                
+                Text("Numbers Rolled: \(diceNumber[0]) \(diceNumber1[0]) \(diceNumber2[0])")
             }
             Button("Start"){
                 diceNumber = []
+                
                 degress = 0
                 isRunning = true
-                for i in 1...3{
-                    withAnimation(.easeIn(duration: 2.0)) { 
-                        degress = 360
-                        coin = ["1","2","3","4","5","6"].randomElement()!
-                    }
-                    withAnimation(.easeInOut(duration: 0.1).delay(0.5)) {
-                        coin = ["1","2","3","4","5","6"].randomElement()!
-                        
-                        isRunning = false
-                    }
-                    withAnimation(.easeInOut(duration: 0.5).delay(1.0)){
-                        print(coin)
-                        diceNumber.append(coin)
-                    }
+                withAnimation(.easeIn(duration: 2.0)) { 
+                    degress = 360
+                    coin = ["1","2","3","4","5","6"].randomElement()!
+                    print(coin,"1")
                 }
-                
+                withAnimation(.easeInOut(duration: 0.1).delay(2.0)) {
+                    coin = ["1","2","3","4","5","6"].randomElement()!
+                    print(coin,"2")
+                    diceNumber.append(coin)
+                    print(diceNumber[0])
+                    isRunning = false
+                }
+                withAnimation(.easeIn(duration: 2.0).delay(3.0)) { 
+                    degress = 360
+                    coin1 = ["1","2","3","4","5","6"].randomElement()!
+                    print(coin1,"3")
+                }
+                withAnimation(.easeInOut(duration: 0.1).delay(2.0)) {
+                    coin1 = ["1","2","3","4","5","6"].randomElement()!
+                    print(coin1,"4")
+                    diceNumber1 = []
+                    diceNumber1.append(coin1)
+                    print(diceNumber1[0])
+                    isRunning = false
+                }
+                withAnimation(.easeIn(duration: 2.0).delay(3.0)) { 
+                    degress = 360
+                    coin2 = ["1","2","3","4","5","6"].randomElement()!
+                    print(coin2,"5")
+                }
+                withAnimation(.easeInOut(duration: 0.1).delay(2.0)) {
+                    coin2 = ["1","2","3","4","5","6"].randomElement()!
+                    print(coin,"6")
+                    diceNumber2 = []
+                    diceNumber2.append(coin2)
+                    print(diceNumber2[0])
+                    isRunning = false
+                }
             }.buttonStyle(.borderedProminent)
         }
         .padding()
