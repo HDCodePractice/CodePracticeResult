8225ad203d96ed58de19e7b5e3883c398efb09ae
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2022-10-31 21:08:54 -0700

    work

--- Jaden/SwiftUI/Poker.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/Poker.swiftpm/ContentView.swift
@@ -25,7 +25,9 @@ struct ContentView: View {
                 let numbers = [0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35,36,37,38,39,40,41,42,43,44,45,46,47,48,49,50,51]
                 let x = numbers.shuffled()
                 let p1 = x[0] + x[1] + x[2] + x[3]
+                print(p1)
                 let p2 = x[4] + x[5] + x[6] + x[7]
+                print(p2)
                 for i in 0...7{ players.append(x[i])}
                 if p1 > p2{ state = "Player 1 Won" }else if p2 > p1{ state = "Player 2 Won"}else{ state = "It's A Tie"}
             }.buttonStyle(.borderedProminent)
--- Jaden/SwiftUI/Teacher’s Version Poker.swiftpm/Card.swift
+++ Jaden/SwiftUI/Teacher’s Version Poker.swiftpm/Card.swift
@@ -1,51 +1,49 @@
 import SwiftUI
 
 struct Card: View {
-    let testNumber : String
-    let testSuit : String
+    let number : String
+    let suit: String
+    
     var body: some View {
-        VStack{
+        ZStack{
+            RoundedRectangle(cornerRadius: 20)
+                .fill(.white)
+            Text(suit)
+                .font(.system(size: 50))
             ZStack(alignment: .topLeading){
-                RoundedRectangle(cornerRadius: 20)
-                    .fill(.white)
-                    .border(.black)
+                Color.clear
                 VStack{
-                    Text(testNumber)
-                        .frame(width: 100, height: 100)
-                        .font(.system(size: 100))
-                    Text(testSuit)
-                        .frame(width: 100, height: 100)
-                        .font(.system(size: 100))
+                    Text(number)
+                        .foregroundColor(.black)
+                    Text(suit)
                 }
-                .padding()
-                ZStack(alignment: .center){
-                    Color.clear
-                    Text(testSuit)
-                        .font(.system(size: 200))
-                }
-                ZStack(alignment: .bottomTrailing){
-                    Color.clear
-                    VStack{
-                        Text(testSuit)
-                            .frame(width: 100, height: 100)
-                            .font(.system(size: 100))
-                            .rotationEffect(.degrees(180))
-                        Text(testNumber)
-                            .frame(width: 100, height: 100)
-                            .font(.system(size: 100))
-                            .rotationEffect(.degrees(180))
-                    }
-                    .padding()
-                }
-                
             }
+            .font(.system(size: 20))
+            .padding()
+            ZStack(alignment: .bottomTrailing){
+                Color.clear
+                VStack{
+                    Text(suit)
+                        .rotationEffect(.degrees(180))
+                    Text(number)
+                        .foregroundColor(.black)
+                        .rotationEffect(.degrees(180))
+                }
+            }
+            .font(.system(size: 20))
+            .padding()
         }
+        .overlay(
+            RoundedRectangle(cornerRadius: 20)
+                .stroke()
+        )
+        .padding()
     }
 }
 
 struct Card_Previews: PreviewProvider {
     static var previews: some View {
-        Card(testNumber: "10", testSuit: "♣️")
+        Card(number: "10", suit: "♣️")
+        Card(number: "A", suit: "♥️")
     }
 }
-
