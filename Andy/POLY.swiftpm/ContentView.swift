7fed64a8ac2ca23ac1b2c19eb6c983501689abfc
Andeeezzzz <110422896+Andeeezzzz@users.noreply.github.com>
2022-11-22 19:34:50 -0500

    aaaaa monopoly hard lol
    why tf is this error so fkng annoying AAAA

--- Andy/POLY.swiftpm/ContentView.swift
+++ Andy/POLY.swiftpm/ContentView.swift
@@ -1,10 +1,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var color : [[Color]] = [[.red,.yellow],[.orange,.green],[.orange,.green],[.clear,.clear],[.orange,.green],[.clear,.clear],[.purple,.clear],[.purple,.blue],[.clear,.clear],[.purple,.blue],[.brown,.cyan]]
     var body: some View {
-        VStack {
-            ForEach(1...11,id\.self){ _ in
-                Rectangle()
+        ZStack{
+            VStack(spacing:3){
+                ForEach(1...11,id:\.self){ _ in
+                    HStack(spacing:3){
+                        ForEach(1...11,id:\.self){ _ in
+                            Rectangle()
+                        }
+                    }
+                }
             }
+            VStack(spacing:3){
+                ForEach(0..<11,id:\.self){ i in
+                    HStack(spacing:3){
+                        Rectangle()
+                            .fill(color[i][0])
+                            .frame(width: 270)
+                        Rectangle()
+                            .fill(color[i][1])
+                            .frame(width: 260)
+                            
+                    }
+                }
+                
+            }.fra
+            
+            Rectangle()
+                .padding(80)
         }
     }
+}
