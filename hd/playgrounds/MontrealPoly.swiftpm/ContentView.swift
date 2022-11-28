5eb0d859d846f59b3b3756050344692b07e5142e
老房东 <huangdong@gmail.com>
2022-11-28 11:36:02 -0500

    继续

--- hd/playgrounds/MontrealPoly.swiftpm/ContentView.swift
+++ hd/playgrounds/MontrealPoly.swiftpm/ContentView.swift
@@ -1,50 +1,133 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var corners : [String] = ["Go","Visiting Jail","Free Parking","Go To Jail"]
+    
+    @State var colors : [[Color]] = [
+        [.clear,.blue,.blue,.clear,.blue,.green,.clear,.red,.clear,.clear,.clear],
+        [.clear,.red,.blue,.clear,.blue,.green,.clear,.red,.clear,.clear,.clear],
+        [.clear,.red,.blue,.clear,.blue,.green,.clear,.red,.clear,.clear,.clear],
+        [.clear,.red,.blue,.clear,.blue,.green,.clear,.red,.clear,.clear,.clear]
+    ]
+    
     @State var lands = [
-        ["","Jaden","Jaden","Jaden","4","Claire","6","Claire","8","Claire",""],
+        ["","Jaden","Jaden","","Jaden","","6","Claire","8","Claire",""],
         ["","1","2","3","4","5","6","7","8","9",""],
         ["","1","2","3","4","5","6","7","8","9",""],
         ["","1","2","3","4","5","6","7","8","9",""]
     ]
+    
+    @State var player : Player = Player()
+    
+    @State var dice : Int = 1
+    @State var turn : Bool = false
+    
     var body: some View {
-        VStack(spacing: 0) {
-            ForEach(0..<11){ y in
-                HStack(spacing: 0) {
-                    ForEach(0..<11){ x in
-                        ZStack{
-                            Rectangle()
-                                .fill(isClear(x: x, y: y) ? .white : .cyan)
-                                .overlay{
-                                    if !isClear(x: x, y: y){
-                                        Rectangle()
-                                            .stroke(.black,lineWidth: 2)
+        ZStack{
+            VStack(spacing: 0) {
+                ForEach(0..<11){ y in
+                    HStack(spacing: 0) {
+                        ForEach(0..<11){ x in
+                            ZStack{
+                                Rectangle()
+                                    .fill(isClear(x: x, y: y) ? .white : .cyan)
+                                    .overlay{
+                                        if !isClear(x: x, y: y){
+                                            Rectangle()
+                                                .stroke(.black,lineWidth: 2)
+                                        }
+                                    }
+                                VStack{
+                                    Text(getText(x: x, y: y))
+//                                    Text("\(x),\(y)")
+//                                        .foregroundColor(.black)
+                                }
+                                VStack{
+                                    HStack{
+                                        Circle()
+                                            .fill(player.getPlayerColor(x: x, y: y, player: 0))
+                                            .frame(width: 20)
+                                        Circle()
+                                            .fill(player.getPlayerColor(x: x, y: y, player: 1))
+                                            .frame(width: 20)
+                                        
+                                    }
+                                    HStack{
+                                        Circle()
+                                            .fill(player.getPlayerColor(x: x, y: y, player: 2))
+                                            .frame(width: 20)
+                                        
+                                        Circle()
+                                            .fill(player.getPlayerColor(x: x, y: y, player: 3))
+                                            .frame(width: 20)
                                     }
                                 }
-                            Text(getText(x: x, y: y))
-                            if !isClear(x: x, y: y){
-                                VStack{
-                                    Rectangle()
-                                        .fill(.red)
-                                    Rectangle()
-                                        .fill(.clear)
-                                    Rectangle()
-                                        .fill(.clear)
-                                    Rectangle()
-                                        .fill(.clear)
-                                    Rectangle()
-                                        .fill(.clear)
+                                if !isClear(x: x, y: y){
+                                    VStack{
+                                        Rectangle()
+                                            .fill(getColor(x: x, y: y))
+                                        Rectangle()
+                                            .fill(.clear)
+                                        Rectangle()
+                                            .fill(.clear)
+                                        Rectangle()
+                                            .fill(.clear)
+                                        Rectangle()
+                                            .fill(.clear)
+                                    }
                                 }
                             }
                         }
                     }
                 }
             }
+            VStack{
+                Dice(dice: $dice, turn: $turn)
+                    .onTapGesture {
+                        withAnimation(.easeInOut(duration: 0.5)){
+                            dice = Int.random(in: 1...6)
+                            turn.toggle()
+                            player.movePlayer(step: dice)
+                        }
+                    }
+                Text("\(dice)")
+                    .foregroundColor(.black)
+            }
         }
         .padding()
     }
     
+
+    
+    func getColor(x: Int,y:Int)->Color{
+        if y==0{
+            return colors[0][x]
+        }
+        if x==0 {
+            return colors[1][y]
+        }
+        if x==10{
+            return colors[2][y]
+        }
+        if y==10{
+            return colors[3][x]
+        }
+        return .clear
+    }
+    
     func getText(x: Int,y:Int)->String{
+        if x==10 && y==10{
+            return corners[0]
+        }
+        if x==0 && y==10{
+            return corners[1]
+        }
+        if x==0 && y==0{
+            return corners[2]
+        }
+        if x==10 && y==0{
+            return corners[3]
+        }
         if y==0{
             return lands[0][x]
         }
