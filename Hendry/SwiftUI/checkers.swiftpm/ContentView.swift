ac8d75c8d7b7ab4f5ea9150a13d2658fbcaba8a6
hendryshao <87082007+hendryshao@users.noreply.github.com>
2022-12-13 19:31:07 -0500

    homework 12-13

--- Hendry/SwiftUI/checkers.swiftpm/ContentView.swift
+++ Hendry/SwiftUI/checkers.swiftpm/ContentView.swift
@@ -1,45 +1,27 @@
 import SwiftUI
 
 struct ContentView: View {
-    @State var board : [[Int]] = 
-    [[0,1,0,1,0,1,0,1],
-     [1,0,1,0,1,0,1,0],
-     [0,1,0,1,0,1,0,1],
-     [1,0,1,0,1,0,1,0],
-     [0,1,0,1,0,1,0,1],
-     [1,0,1,0,1,0,1,0],
-     [0,1,0,1,0,1,0,1],
-     [1,0,1,0,1,0,1,0]]
+    @State var board = Board()
     var body: some View {
-        VStack(spacing:0){
-            ForEach(0..<board.count){row in
+        VStack(spacing:0) {
+            ForEach(0..<board.height,id:\.self){ x in
                 HStack(spacing:0){
-                    ForEach(0..<board.count){column in
+                    ForEach(0..<board.width,id:\.self){ y in
                         ZStack{
-                            if board[row][column] == 0{
-                                Rectangle()
-                                    .fill(.white)
-                            }
-                            if row <= 2 && board[row][column] == 0{
-                                Circle()
-                                    .fill(.cyan)
-                                
-                            }
-                        }
-                        ZStack{
-                            if board[row][column] == 1{
-                                Rectangle()
-                                    .fill(.black)
-                            }
-                            if row >= 5 && board[row][column] == 1{
-                                Circle()
-                                    .fill(.white)
-                                
-                            }
+                            Rectangle()
+                                .fill(board.grids[x][y].color)
+                            Circle()
+                                .fill(board.grids[x][y].token)
+                                .padding(3)
+                                .onTapGesture {
+                                    board.select(grid: board.grids[x][y])
+                                }
                         }
                     }
                 }
             }
         }
+        .padding()
     }
 }
+
