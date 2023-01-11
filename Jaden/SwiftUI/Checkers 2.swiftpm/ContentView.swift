aec12cceeb330fe395871c320838fb3776a3648e
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2023-01-10 16:33:07 -0800

    work

--- Jaden/SwiftUI/Checkers 2.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/Checkers 2.swiftpm/ContentView.swift
@@ -3,18 +3,19 @@ struct ContentView: View {
     @State var board = Board()
     var body: some View {
         VStack(spacing:0) {
-            ForEach(0..<board.height, id:\.self){y in
+            ForEach(0..<board.height, id:\.self){x in
                 HStack{
-                    ForEach(0..<board.width, id:\.self){x in
+                    ForEach(0..<board.width, id:\.self){y in
                         ZStack{
                             Rectangle()
                                 .fill(board.grids[x][y].color)
                             Circle()
                                 .fill(board.grids[x][y].token)
                                 .padding()
-                                .onTapGesture {
-                                    board.select(grid: board.grids[x][y])
-                                }
+                              
+                        }
+                        .onTapGesture {
+                            board.select(grid: board.grids[x][y])
                         }
                        
                     }
