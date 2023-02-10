60255e7e654955f40b024da8913d6a60bc5ee39a
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2023-02-10 10:56:21 -0800

    work

---
+++ Jaden/SwiftUI/Dog Guide.swiftpm/TestView.swift
@@ -0,0 +1,17 @@
+import SwiftUI
+
+struct Questions: View {
+    var body: some View {
+        var questions = [""]
+        var questionChoices = [[""]]
+        var responses = [""]
+        var dog = ""
+    }
+}
+
+struct Questions_Previews: PreviewProvider {
+    static var previews: some View {
+        Questions()
+    }
+}
+
