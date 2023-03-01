f52eb26d35756c43cf04b4ed07ab1e337c539b79
Claireyao123 <63210864+Claireyao123@users.noreply.github.com>
2023-02-28 21:03:35 -0500

    Space 4

---
+++ Claire/Swift UI/Space.swiftpm/Planet View/VenusView.swift
@@ -0,0 +1,13 @@
+import SwiftUI
+
+struct Venus: View {
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct Venus_Preview: PreviewProvider {
+    static var previews: some View {
+        Venus()
+    }
+}
