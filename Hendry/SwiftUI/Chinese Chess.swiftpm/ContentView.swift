d0196ac6f4047ad10cbf755a425cacc026b2f0f2
hendryshao <87082007+hendryshao@users.noreply.github.com>
2023-03-10 21:33:05 -0500

    board

--- Hendry/SwiftUI/Chinese Chess.swiftpm/ContentView.swift
+++ Hendry/SwiftUI/Chinese Chess.swiftpm/ContentView.swift
@@ -1,33 +1,28 @@
 import SwiftUI
-
 struct ContentView: View {
     let num1 = ["1","2","3","4","5","6","7","8","9"]
     let num2 = ["9","8","7","6","5","4","3","2","1"]
-    
-    @State var crossgrid = CrossGrid()
+
+    var borardGrid = BoardGrid()
+
     var body: some View {
         ZStack{
-            
+
             VStack(spacing:0){
-                ForEach(0..<8){row in
+                ForEach(0..<10){row in
                     HStack(spacing:0){
-                        ForEach(0..<8){column in
-                            if row == 0{
-                                topGrid()
-                            }else{
-                                CrossGrid()
-                            }
-                            
-                            
+                        ForEach(0..<9){column in
+                            let grid = borardGrid.grids[row][column]
+                            Grid(grid: grid)
                         }
                     }
                 }
-                
+
             }
-            
+
             chuhe()
         }
         .padding(10)
     }
-    
+
 }
