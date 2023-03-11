fc4c40193fd6d204234f29277058e39f9adeabd1
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2023-03-10 18:13:08 -0800

    work

--- Jaden/SwiftUI/Dog Guide 2.swiftpm/TestView.swift
+++ Jaden/SwiftUI/Dog Guide 2.swiftpm/TestView.swift
@@ -1,6 +1,6 @@
 import SwiftUI
 
-struct dogs{
+struct Dogs{
     var names = ["labrador retriever",
                  "german shepherd",
                  "golden retriever",
@@ -35,10 +35,20 @@ struct dogs{
 struct Questions: View {
     let questions = ["What is your lifestyle like?","What size dog are you looking for?","Do you have any previous experience with dog ownership?","Do you have any other pets or children in the household?","Do you prefer a high energy or low energy dog?","How much room do you have for a dog?","Are you willing to train a dog, or do you prefer a dog that is easy to train?","What is your budget for pet care?","Do you have any specific breed preferences or requirements?"]
     let questionChoices = [["Active","Outdoor","Indoor","Lazy"],["Small","Medium","Large"],["Yes","No"],["Children","Pets","None"],["High Energy","Low Energy"],["Lots of room","Small Room", "Some room"],["I can train a dog","I want one easy to train"],["$10,000","$15,000","$20,000","$40,000"],["Hypoallergenic","Quiet","Protective","Low-Shedding"]]
-    var dogscore = [[[4, 3, 4, 2, 3, 3, 3, 4, 3, 2, 2, 1, 4, 2, 1, 1, 2, 1, 1, 4],[4,4,4,2,3,2,1,4,3,3,1,1,3,2,2,2,3,1,1,4],[2, 2, 2, 4, 2, 4, 3, 2, 3, 3, 4, 1, 1, 4, 3, 1, 1, 3, 4, 1],[1,2,1,3,2,2,2,1,2,3,3,4,1,3,4,4,3,4,4,1]],[[2, 2, 2, 3, 3, 3, 3, 1, 4, 3, 4, 1, 1, 4, 4, 2, 1, 1, 4, 2],[4,4,4,3,3,3,3,3,2,2,2,1,4,1,1,2,1,2,1,4,],[3,3,3,2,2,2,2,4,1,2,1,4,4,1,1,3,4,4,1,3,]],[[4,4,4,3,3,4,2,3,2,3,2,2,4,1,1,2,1,1,1,3,],[1,1,1,2,2,1,3,2,3,2,3,3,1,4,4,3,4,4,4,2,]],[[4, 4, 3, 3, 4, 3, 2, 2, 2, 3, 4, 4, 2, 1, 2, 3, 1, 1, 1],[4,2,4,4,4,4,1,2,3,3,3,2,3,1,2,2,1,1,1,3],[4,4,4,3,1,3,1,3,3,3,2,1,4,1,2,3,2,1,2,4]],[[3,3,4,4,3,3,4,4,2,2,1,2,4,1,1,2,2,1,1,4],[3,1,3,3,2,3,2,1,4,4,4,2,2,4,3,1,1,1,1,4,2]],[[4,4,4,3,2,2,3,4,1,2,1,4,3,1,1,3,2,2,1,3],[1,1,1,3,3,3,2,1,4,3,4,1,2,4,4,2,3,2,3,2],[4,4,4,3,2,2,3,4,1,2,1,4,3,1,1,3,2,2,1,3]],[[4,4,4,2,2,4,3,2,3,3,2,2,4,1,1,3,2,3,2,4],[4,4,4,3,3,4,3,3,3,3,3,2,4,2,2,3,3,3,2,4]],[[4,4,4,3,3,3,3,3,2,2,2,2,4,1,1,1,1,1,1,1,4],[4,4,4,3,3,3,3,3,2,2,2,2,4,2,1,1,1,1,1,1,4],[4,4,4,3,3,3,3,3,2,2,2,2,4,2,1,1,1,1,1,1,4],[3,3,4,4,2,2,4,4,2,2,3,4,1,2,3,1,1,1,1,3,1]],[[2,3,3,1,2,4,4,3,2,2,4,1,2,1,4,1,1,3,3,4,3],[3,2,3,4,3,1,1,2,2,3,4,4,2,1,2,1,1,4,3],[3,4,3,4,3,2,1,2,3,2,4,2,1,2,4,1,1,3],[1,1,1,3,3,4,4,2,2,2,4,2,4,4,4,2,3,4,4,2]]]
+    var dogscore = [[[4, 3, 4, 2, 3, 3, 3, 4, 3, 2, 2, 1, 4, 2, 1, 1, 2, 1, 1, 4],[4,4,4,2,3,2,1,4,3,3,1,1,3,2,2,2,3,1,1,4],[2, 2, 2, 4, 2, 4, 3, 2, 3, 3, 4, 1, 1, 4, 3, 1, 1, 3, 4, 1],[1,2,1,3,2,2,2,1,2,3,3,4,1,3,4,4,3,4,4,1]],[[2, 2, 2, 3, 3, 3, 3, 1, 4, 3, 4, 1, 1, 4, 4, 2, 1, 1, 4, 2], [4,4,4,3,3,3,3,3,2,2,2,1,4,1,1,2,1,2,1,4], [3,3,3,2,2,2,2,4,1,2,1,4,4,1,1,3,4,4,1,3,]], [[4,4,4,3,3,4,2,3,2,3,2,2,4,1,1,2,1,1,1,3],[1,1,1,2,2,1,3,2,3,2,3,3,1,4,4,3,4,4,4,2]],[[4, 4, 3, 3, 4, 3, 2, 2, 2, 3, 4, 4, 2, 1, 2, 3, 1, 1, 1, 1], [4,2,4,4,4,4,1,2,3,3,3,2,3,1,2,2,1,1,1,3], [4,4,4,3,1,3,1,3,3,3,2,1,4,1,2,3,2,1,2,4]], [[3,3,4,4,3,3,4,4,2,2,1,2,4,1,1,2,2,1,1,4], [3,1,3,3,2,3,2,1,4,4,4,2,2,4,3,1,1,1,1,4,2]], [[4,4,4,3,2,2,3,4,1,2,1,4,3,1,1,3,2,2,1,3],[1,1,1,3,3,3,2,1,4,3,4,1,2,4,4,2,3,2,3,2],[4,4,4,3,2,2,3,4,1,2,1,4,3,1,1,3,2,2,1,3]],[[4,4,4,2,2,4,3,2,3,3,2,2,4,1,1,3,2,3,2,4],[4,4,4,3,3,4,3,3,3,3,3,2,4,2,2,3,3,3,2,4]],[[4,4,4,3,3,3,3,3,2,2,2,2,4,1,1,1,1,1,1,1,4],[4,4,4,3,3,3,3,3,2,2,2,2,4,2,1,1,1,1,1,1,4],[4,4,4,3,3,3,3,3,2,2,2,2,4,2,1,1,1,1,1,1,4],[3,3,4,4,2,2,4,4,2,2,3,4,1,2,3,1,1,1,1,3,1]],[[2,3,3,1,2,4,4,3,2,2,4,1,2,1,4,1,1,3,3,4,3],[4,3,2,3,4,3,1,1,2,2,3,4,4,2,1,2,1,1,4,3],[4,3,4,3,4,3,2,1,2,3,2,4,2,1,2,4,1,1,3,1],[1,1,1,3,3,4,4,2,2,2,4,2,4,4,4,2,3,4,4,2]]]
     @State var responses : [String] = []
     @State var dog = ""
     @State var a = 0
+    @State var dogs = Dogs()
+    @State var largest = 0
+    @State var dogSpot = 0
+    func addScore(questionNumber: Int, choiceNumber : Int){
+        
+        for i in 0..<dogs.names.count {
+            dogs.scores[i] += dogscore[questionNumber][choiceNumber][i]
+        }
+        print(dogs.scores)
+    }
 
     var body: some View {
         VStack{
@@ -56,6 +66,8 @@ struct Questions: View {
             }else{
                 ZStack{
                     Color(red: 0, green: 0.8, blue: 0.6)
+                  
+                    
                     Text("The dog best fit for you is \(dog)")
                         .font(.system(size: 40, design: .rounded))
                         .bold()
@@ -81,15 +93,27 @@ struct Questions: View {
                     .cornerRadius(20)
                     .onTapGesture {
                         withAnimation{
-                            responses.append(questionChoices[a][i])
-                            print(responses)
                             
+                            responses.append(questionChoices[a][i])
+                            addScore(questionNumber: a, choiceNumber: i)
                             
                             if a < 8{
                                 a += 1
                             }else{
-                                a = 9 
+                                a = 9
+                                for i in dogs.scores{
+                                    if i > largest{
+                                        largest = i
+                                    }
+                                }
+                                for i in 0..<dogs.scores.count{
+                                    if dogs.scores[i] == largest{
+                                        dogSpot = i
+                                    }
+                                }
+                                dog = dogs.names[dogSpot]
                             }
+                           
                         }
                         
                         
