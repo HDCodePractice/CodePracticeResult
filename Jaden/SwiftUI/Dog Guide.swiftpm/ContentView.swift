c2c7d392bd6048dcd18e79960cfccc48608ca3e0
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2023-03-28 18:10:00 -0700

    work

--- Jaden/SwiftUI/Dog Guide.swiftpm/ContentView.swift
+++ Jaden/SwiftUI/Dog Guide.swiftpm/ContentView.swift
@@ -3,10 +3,7 @@ import SwiftUI
 struct ContentView: View {
     var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+            HomeView()
         }
     }
 }
---
+++ Jaden/SwiftUI/Dog Guide.swiftpm/Support.swift
@@ -0,0 +1,21 @@
+//
+//  Support.swift
+//  Dog Guide
+//
+//  Created by Jaden Cheung on 3/28/23.
+//
+
+import SwiftUI
+
+struct Support: View {
+    var showingText = false
+    var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
+    }
+}
+
+struct Support_Previews: PreviewProvider {
+    static var previews: some View {
+        Support()
+    }
+}
