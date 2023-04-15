1696133e8fa26f67b4ac5d65e091a0f2709c90d6
NinjagoJaden1 <68250411+NinjagoJaden1@users.noreply.github.com>
2023-04-14 17:41:25 -0700

    work

--- Jaden/SwiftUI/Dog Guide 2.swiftpm/HomeView.swift
+++ Jaden/SwiftUI/Dog Guide 2.swiftpm/HomeView.swift
@@ -4,9 +4,10 @@ struct HomeView: View {
     @State private var showingTest = false
     @State private var showingGuide = false
     @State private var showingGame = false
+        @State private var showingTTT = false
     var body: some View {
         VStack{
-            if showingTest == false && showingGuide == false && showingGame == false{
+            if showingTest == false && showingGuide == false && showingGame == false && showingTTT == false{
                 Button() {
                     showingTest = true
                     
@@ -55,6 +56,22 @@ struct HomeView: View {
                     .padding()
                     
                 }
+                Image("aussiepuppy")
+                Button() {
+                    showingTTT = true
+                } label: {
+                    ZStack {
+                        Color(red: 0, green: 0.8, blue: 0.6)
+                        Text("Tic Tac Toe")
+                            .font(.system(size: 40, design: .rounded))
+                            .bold()
+                            .foregroundColor(.black)
+                            .font(.title)
+                    }
+                    .cornerRadius(20)
+                    .padding()
+                    
+                }
             }
             if showingTest == true{
                 TestView()
@@ -65,7 +82,9 @@ struct HomeView: View {
             if showingGame == true{
                 GameView()
             }
-            
+            if showingTTT == true{
+                TicTacToeView()
+            }
         }
         
     }
