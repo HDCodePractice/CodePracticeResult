8ab0d444b694ae913b1b9f9013369247def3d80e
老房东 <huangdong@gmail.com>
2023-04-26 10:01:15 -0400

    pipe is ready

--- hd/unity/Flappy Copter/Assets/Scripts/PipeMovement.cs
+++ hd/unity/Flappy Copter/Assets/Scripts/PipeMovement.cs
@@ -6,10 +6,17 @@ public class PipeMovement : MonoBehaviour
 {
     [SerializeField]
     private float speed = 5f;
+    [SerializeField]
+    private float deadZone = -45;
+
     // Update is called once per frame
     void Update()
     {
         transform.position = transform.position
             + Vector3.left * speed * Time.deltaTime;
+        if (transform.position.x < deadZone)
+        {
+            Destroy(gameObject);
+        }
     }
 }
--- hd/unity/Flappy Copter/Assets/Scripts/PipeSpawn.cs
+++ hd/unity/Flappy Copter/Assets/Scripts/PipeSpawn.cs
@@ -8,13 +8,16 @@ public class PipeSpawn : MonoBehaviour
     private float spawnRate = 2.0f;
     [SerializeField]
     private GameObject pip;
+    [SerializeField]
+    private float heightOffSet = 1.8f;
+
     private float timer = 0f;
 
 
     // Start is called before the first frame update
     void Start()
     {
-
+        spawnPip();
     }
 
     // Update is called once per frame
@@ -26,8 +29,20 @@ public class PipeSpawn : MonoBehaviour
         }
         else
         {
-            Instantiate(pip);
+            spawnPip();
             timer = 0;
         }
+    }
+
+    void spawnPip()
+    {
+        float lowestPoint = transform.position.y - heightOffSet;
+        float highestPoint = transform.position.y + heightOffSet;
+        Vector3 position = new Vector3(
+            transform.position.x,
+            Random.Range(lowestPoint, highestPoint),
+            0);
+
+        Instantiate(pip, position, transform.rotation);
     }
 }
