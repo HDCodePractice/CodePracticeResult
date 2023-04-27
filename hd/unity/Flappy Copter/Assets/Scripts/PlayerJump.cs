152395cadf2988725e5f882f69b8a428bd6a5475
老房东 <huangdong@gmail.com>
2023-04-27 10:39:11 -0400

    优化Jump和音效

--- hd/unity/Flappy Copter/Assets/Scripts/PlayerJump.cs
+++ hd/unity/Flappy Copter/Assets/Scripts/PlayerJump.cs
@@ -6,6 +6,8 @@ public class PlayerJump : MonoBehaviour
 {
     [SerializeField]
     private float flapStrength = 5f;
+    [SerializeField]
+    private AudioSource jumpSoundEffect;
     private Rigidbody2D rb;
 
     // Start is called before the first frame update
@@ -17,9 +19,10 @@ public class PlayerJump : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButton("Jump"))
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.Mouse0))
         {
             rb.velocity = Vector2.up * flapStrength;
+            jumpSoundEffect.Play();
         }
     }
 }
